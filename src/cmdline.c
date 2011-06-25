ccb32de5717a9826cf0cb1b2d79510c24da18fd0
xaizek <xaizek@gmail.com>
2011-06-25 21:33:25 +0300

    Use only executables for :!./ completion.

--- src/cmdline.c
+++ src/cmdline.c
@@ -1171,7 +1171,7 @@ line_completion(struct line_stats *stat)
 			raw_name = exec_completion(stat->complete_continue ? NULL : last_word);
 		else
 			raw_name = filename_completion(stat->complete_continue ? NULL : last_word,
-					(id == COM_CD) ? 1 : 0);
+					(id == COM_CD) ? 1 : (id == COM_EXECUTE ? 2 : 0));
 
 		stat->complete_continue = 1;
 
@@ -1487,6 +1487,11 @@ exec_completion(char *str)
 /* On the first call to this function,
  * the string to be parsed should be specified in str.
  * In each subsequent call that should parse the same string, str should be NULL
+ *
+ * type:
+ *  - 0 - all files and directories
+ *  - 1 - only directories
+ *  - 2 - only executable files
  */
 static char *
 filename_completion(char *str, int type)
@@ -1578,6 +1583,11 @@ filename_completion(char *str, int type)
 			if(d->d_type == DT_LNK && is_dir(d->d_name))
 				continue;
 			if(access(d->d_name, X_OK) != 0)
+				continue;
+		}
+		else if(type == 3)
+		{
+			if(d->d_type == DT_DIR)
 				continue;
 		}
 		found = 1;
