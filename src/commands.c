2aaf240957026577012cf5d80a6aa8411e1fc60a
xaizek <xaizek@gmail.com>
2011-07-11 21:18:33 +0300

    Fix problem with :com and user defined commands.
    If user defined command begins with :com.

--- src/commands.c
+++ src/commands.c
@@ -1282,7 +1282,9 @@ parse_command(FileView *view, char *command, cmd_params *cmd)
 	else /* Prevent eating '!' after command name. by not doing cmd->pos++ */
 	{
 		cmd->cmd_name[cmd->pos - pre_cmdname_len] = '\0';
-		if(is_user_command(cmd->cmd_name) > - 1)
+		if(cmd->cmd_name[cmd->pos - pre_cmdname_len] == '!' &&
+				command_is_reserved(cmd->cmd_name) < 0 &&
+				is_user_command(cmd->cmd_name) > - 1)
 		{
 			cmd->cmd_name[cmd->pos - pre_cmdname_len] = '!';
 			cmd->cmd_name[cmd->pos - pre_cmdname_len + 1] = '\0';
