083bba57698bf9cade63302ab447688aa5ffef11
xaizek <xaizek@gmail.com>
2011-07-17 22:05:29 +0300

    Replace /home/login by ~ in the view title.

--- src/filelist.c
+++ src/filelist.c
@@ -481,6 +481,7 @@ find_file_pos_in_list(FileView *view, const char *file)
 static void
 update_view_title(FileView *view)
 {
+	char buf[PATH_MAX];
 	size_t len;
 
 	if(curr_stats.vifm_started != 2)
@@ -488,12 +489,19 @@ update_view_title(FileView *view)
 
 	werase(view->title);
 
-	len = get_utf8_string_length(view->curr_dir);
+	len = strlen(cfg.home_dir) - 1;
+	if(strncmp(view->curr_dir, cfg.home_dir, len) == 0 &&
+			(view->curr_dir[len] == '\0' || view->curr_dir[len] == '/'))
+		strncat(strcpy(buf, "~"), view->curr_dir + len, sizeof(buf));
+	else
+		strncpy(buf, view->curr_dir, sizeof(buf));
+
+	len = get_utf8_string_length(buf);
 	if(len + 1 > view->window_width && curr_view == view)
 	{ /* Truncate long directory names */
 		char *ptr;
 
-		ptr = view->curr_dir;
+		ptr = buf;
 		while(len > view->window_width - 2)
 		{
 			len--;
@@ -503,7 +511,7 @@ update_view_title(FileView *view)
 		wprintw(view->title, "...%s", ptr);
 	}
 	else
-		wprintw(view->title, "%s", view->curr_dir);
+		wprintw(view->title, "%s", buf);
 
 	wnoutrefresh(view->title);
 }
