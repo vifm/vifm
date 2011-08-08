e42d1f3dc632caf3cbd78776a4c6a017ec33694f
xaizek <xaizek@gmail.com>
2011-08-08 16:56:20 +0300

    Redraw list after :file.

--- src/menus.c
+++ src/menus.c
@@ -627,6 +627,8 @@ execute_filetype_cb(FileView *view, menu_info *m)
 	run_using_prog(view, prog_str, 0, background);
 
 	clean_selected_files(view);
+	draw_dir_list(view, view->top_line);
+	moveto_list_pos(view, view->list_pos);
 }
 
 void
