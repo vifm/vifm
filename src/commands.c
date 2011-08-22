de26ae519e2028a7101cb7d93d572931c21e38d2
xaizek <xaizek@gmail.com>
2011-08-22 20:48:51 +0300

    :copy!
    I forgot it.

--- src/commands.c
+++ src/commands.c
@@ -206,7 +206,7 @@ static const struct cmd_add commands[] = {
 		.handler = colorscheme_cmd, .qmark = 0,      .expand = 0, .cust_sep = 0,         .min_args = 0, .max_args = 1,       .select = 0, },
   { .name = "command",          .abbr = "com",   .emark = 1,  .id = -1,              .range = 0,    .bg = 0, .quote = 0, .regexp = 0,
     .handler = command_cmd,     .qmark = 0,      .expand = 0, .cust_sep = 0,         .min_args = 0, .max_args = NOT_DEF, .select = 0, },
-	{ .name = "copy",             .abbr = "co",    .emark = 0,  .id = -1,              .range = 1,    .bg = 0, .quote = 1, .regexp = 0,
+	{ .name = "copy",             .abbr = "co",    .emark = 1,  .id = -1,              .range = 1,    .bg = 0, .quote = 1, .regexp = 0,
 		.handler = copy_cmd,        .qmark = 0,      .expand = 0, .cust_sep = 0,         .min_args = 0, .max_args = NOT_DEF, .select = 1, },
 	{ .name = "cunmap",           .abbr = "cu",    .emark = 0,  .id = -1,              .range = 0,    .bg = 0, .quote = 0, .regexp = 0,
 		.handler = cunmap_cmd,      .qmark = 0,      .expand = 0, .cust_sep = 0,         .min_args = 1, .max_args = 1,       .select = 0, },
@@ -2267,7 +2267,19 @@ command_cmd(const struct cmd_info *cmd_info)
 static int
 copy_cmd(const struct cmd_info *cmd_info)
 {
-	return cpmv_files(curr_view, cmd_info->argv, cmd_info->argc, 0, 0) != 0;
+	if(cmd_info->emark)
+	{
+		if(cmd_info->argc > 0)
+		{
+			status_bar_message("No arguments are allowed if you use \"!\"");
+			return 1;
+		}
+		return cpmv_files(curr_view, NULL, -1, 0, 0) != 0;
+	}
+	else
+	{
+		return cpmv_files(curr_view, cmd_info->argv, cmd_info->argc, 0, 0) != 0;
+	}
 }
 
 static int
