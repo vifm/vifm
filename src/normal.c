7579767488ea97bed5a34a4e174706f2860e948f
xaizek <xaizek@gmail.com>
2011-09-21 14:36:58 +0300

    Update copyrights
    Mainly for consistency, and also because I've modified all files.

--- src/normal.c
+++ src/normal.c
@@ -1,10 +1,11 @@
 /* vifm
  * Copyright (C) 2001 Ken Steen.
+ * Copyright (C) 2011 xaizek.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * (at your option) any later version.
  * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
