vifm/vifm

commit 4a4fc16959d73d9ff231d0fc3278f9d488bf1971

Correct window update on startup.  Less blinking.

src/signals.c
@@ -59,8 +59,6 @@ received_sigwinch(void)
 	{
 		if(curr_stats.vifm_started >= 3)
 			curr_stats.pending_redraw = 1;
-		else
-			curr_stats.startup_redraw_pending = 1;
 	}
 	else
 	{

src/filelist.c
@@ -2239,7 +2239,8 @@ load_dir_list(FileView *view, int reload)
 #endif
 		status_bar_message("Reading directory...");
 
-	update_all_windows();
+	if(curr_stats.vifm_started < 2)
+		update_all_windows();
 
 	/* this is needed for lstat() below */
 	if(my_chdir(view->curr_dir) != 0 && !is_unc_root(view->curr_dir))

src/log.c
@@ -99,6 +99,18 @@ log_serror(const char *file, const char *func, int line, int no)
 	fprintf(log, "               %s\n", strerror(no));
 }
 
+#ifdef _WIN32
+void
+log_werror(const char *file, const char *func, int line, int no)
+{
+	if(verbosity <= 0)
+		return;
+
+	log_prefix(file, func, line);
+	fprintf(log, "               Windows error code: %d\n", no);
+}
+#endif
+
 void
 log_msg(const char *msg, ...)
 {

src/log.h
@@ -37,12 +37,18 @@
         log_prefix(__FILE__, __FUNCTION__, __LINE__); \
         log_msg((msg), ## args); \
     }
+
 #define LOG_SERROR(no) log_error(__FILE__, __FUNCTION__, __LINE__, (no))
 #define LOG_SERROR_MSG(no, msg, args...) \
     { \
         log_serror(__FILE__, __FUNCTION__, __LINE__, (no)); \
         log_msg((msg), ## args); \
     }
+
+#ifdef _WIN32
+#define LOG_WERROR(no) log_werror(__FILE__, __FUNCTION__, __LINE__, (no))
+void log_werror(const char *file, const char *func, int line, int no);
+#endif
 
 void init_logger(int verbosity_level);
 void reinit_logger(void);

src/main_loop.c
@@ -68,7 +68,7 @@ update_win_console(void)
 static int
 read_char(WINDOW *win, wint_t *c, int timeout)
 {
-	static const int T = 50;
+	static const int T = 150;
 
 	int i;
 	int result;

src/ops.c
@@ -146,7 +146,10 @@ op_removesl(void *data, const char *src, const char *dst)
 	}
 	else
 	{
-		return (DeleteFile(src) == 0);
+		int ok = DeleteFile(src);
+		if(!ok)
+			LOG_WERROR(GetLastError());
+		return !ok;
 	}
 #endif
 }

src/status.c
@@ -31,7 +31,6 @@ void
 init_status(void)
 {
 	curr_stats.need_redraw = 0;
-	curr_stats.startup_redraw_pending = 0;
 	curr_stats.last_char = 0;
 	curr_stats.is_console = 0;
 	curr_stats.search = 0;

src/status.h
@@ -44,7 +44,6 @@ typedef enum
 typedef struct
 {
 	int need_redraw;
-	int startup_redraw_pending;
 	int last_char;
 	int is_console;
 	int search;

commit 0a1437dc1fd4de87c3493c4ec6e9bfb71bd74899

Fix redrawing on startup on Linux
Was broken after 4a4fc16.

src/signals.c
@@ -57,8 +57,7 @@ received_sigwinch(void)
 
 	if(!isendwin())
 	{
-		if(curr_stats.load_stage >= 3)
-			curr_stats.pending_redraw = 1;
+		curr_stats.pending_redraw = 1;
 	}
 	else
 	{