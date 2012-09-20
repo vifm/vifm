782349d1d4f378ee856288ecd8c994dd321a0d55
xaizek <xaizek@lavabit.com>
2012-09-20 21:04:18 +0200

    Add engine/var.c unit

---
+++ src/engine/var.c
@@ -0,0 +1,90 @@
+/* vifm
+ * Copyright (C) 2012 xaizek.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include <assert.h> /* assert() */
+#include <stddef.h> /* size_t */
+#include <stdio.h> /* sprintf() snprintf() */
+#include <stdlib.h> /* free() */
+#include <string.h> /* strdup() */
+
+#include "var.h"
+
+var_t
+var_false(void)
+{
+	static const var_t false_var = { VT_INT, 0 };
+	return false_var;
+}
+
+var_t
+var_new(VarType type, const var_val_t value)
+{
+	var_t new_var = { type, value };
+	if(type == VT_STRING)
+	{
+		new_var.value.string = strdup(value.string);
+	}
+	return new_var;
+}
+
+char *
+var_to_string(const var_t var)
+{
+	switch(var.type)
+	{
+		case VT_STRING:
+			return strdup(var.value.string);
+		case VT_INT:
+			{
+				size_t len = snprintf(NULL, 0, "%d", var.value.integer);
+				char *str = malloc(len + 1);
+				(void)sprintf(str, "%d", var.value.integer);
+				return str;
+			}
+
+		default:
+			assert(0 && "Var -> String function: unhandled variable type");
+	}
+}
+
+int
+var_to_boolean(const var_t var)
+{
+	switch(var.type)
+	{
+		case VT_STRING:
+			return var.value.string[0] != '\0';
+		case VT_INT:
+			return var.value.integer != 0;
+
+		default:
+			assert(0 && "Var -> Boolean function: unhandled variable type");
+	}
+}
+
+void
+var_free(const var_t var)
+{
+	if(var.type == VT_STRING)
+	{
+		free(var.value.string);
+	}
+}
+
+/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
+/* vim: set cinoptions+=t0 : */
