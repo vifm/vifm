f633e80b6aa3af6e8d3e7f7ee2e349f79677ac07
xaizek <xaizek@lavabit.com>
2012-09-24 13:46:42 +0200

    Add utils/int_stack.c

---
+++ src/utils/int_stack.c
@@ -0,0 +1,86 @@
+/* vifm
+ * Copyright (C) 2012 xaizek.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include <assert.h> /* assert() */
+#include <stddef.h> /* size_t */
+#include <stdlib.h> /* realloc() */
+
+#include "int_stack.h"
+
+static int ensure_available(int_stack_t *const stack);
+
+int
+int_stack_is_empty(const int_stack_t *const stack)
+{
+	return stack->top == 0;
+}
+
+int
+int_stack_get_top(const int_stack_t *const stack)
+{
+	assert(!int_stack_is_empty(stack));
+
+	return stack->data[stack->top - 1];
+}
+
+void
+int_stack_set_top(const int_stack_t *const stack, const int val)
+{
+	assert(!int_stack_is_empty(stack));
+
+	stack->data[stack->top - 1] = val;
+}
+
+int
+int_stack_push(int_stack_t *const stack, const int val)
+{
+	if(ensure_available(stack) == 0)
+	{
+		stack->data[stack->top++] = val;
+		return 0;
+	}
+	return 1;
+}
+
+/* Ensures that there is a place for one more element in the stack.  Returns
+ * non-zero if not enough memory. */
+static int
+ensure_available(int_stack_t *const stack)
+{
+	if(stack->top == stack->len)
+	{
+		int *const ptr = realloc(stack->data, sizeof(int)*(stack->len + 1));
+		if(ptr != NULL)
+		{
+			stack->data = ptr;
+			stack->len++;
+		}
+	}
+	return (stack->top < stack->len) ? 0 : 1;
+}
+
+void
+int_stack_pop(int_stack_t *const stack)
+{
+	assert(!int_stack_is_empty(stack));
+
+	stack->top--;
+}
+
+/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
+/* vim: set cinoptions+=t0 : */
---
+++ src/utils/int_stack.h
@@ -0,0 +1,55 @@
+/* vifm
+ * Copyright (C) 2012 xaizek.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#ifndef __INT_STACK_H__
+#define __INT_STACK_H__
+
+#include <stddef.h> /* NULL, size_t */
+
+/* Integer stack structure. */
+typedef struct {
+	int *data; /* Pointer to stack data. */
+	size_t len; /* Number of available elements. */
+	size_t top; /* Number of used elements. */
+} int_stack_t;
+
+#define INT_STACK_INITIALIZER { NULL, 0U, 0U };
+
+
+/* Checks whether stack is empty.  Returns non-zero if it is. */
+int int_stack_is_empty(const int_stack_t *const stack);
+
+/* Returns element at the top of the stack.  The stack have to contain at least
+ * one element. */
+int int_stack_get_top(const int_stack_t *const stack);
+
+/* Set element at the top of the stack.  The stack have to contain at least one
+ * element. */
+void int_stack_set_top(const int_stack_t *const stack, const int val);
+
+/* Puts the val at the top of the stack.  Returns non-zero in case of error. */
+int int_stack_push(int_stack_t *const stack, const int val);
+
+/* Removes element from the top of the stack.  The stack have to contain at
+ * least one element */
+void int_stack_pop(int_stack_t *const stack);
+
+#endif /* __INT_STACK_H__ */
+
+/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
+/* vim: set cinoptions+=t0 : */
