6cbeb6f30118fd093ffa1141b6e3809930e76146
xaizek <xaizek@lavabit.com>
2012-09-29 21:21:08 +0200

    Reduce code duplication in tests a bit

--- tests/keys/selectors.c
+++ tests/keys/selectors.c
@@ -5,32 +5,27 @@
 extern int last_command_count;
 extern int last_selector_count;
 
+/* This should be a macro to see what test have failed. */
+#define check(wait, full, sel_count, cmd_count) \
+	{ \
+		assert_int_equal(KEYS_WAIT, execute_keys(wait)); \
+		assert_int_equal(0, execute_keys(full)); \
+		assert_int_equal((sel_count), last_selector_count); \
+		assert_int_equal((cmd_count), last_command_count); \
+	}
+
 static void
 test_no_number_ok(void)
 {
-	assert_int_equal(KEYS_WAIT, execute_keys(L"d"));
-	assert_int_equal(0, execute_keys(L"dk"));
-	assert_int_equal(NO_COUNT_GIVEN, last_selector_count);
-	assert_int_equal(NO_COUNT_GIVEN, last_command_count);
+	check(L"d", L"dk", NO_COUNT_GIVEN, NO_COUNT_GIVEN);
 }
 
 static void
 test_with_number_ok(void)
 {
-	assert_int_equal(KEYS_WAIT, execute_keys(L"d1"));
-	assert_int_equal(0, execute_keys(L"d1k"));
-	assert_int_equal(1*1, last_selector_count);
-	assert_int_equal(NO_COUNT_GIVEN, last_command_count);
-
-	assert_int_equal(KEYS_WAIT, execute_keys(L"d12"));
-	assert_int_equal(0, execute_keys(L"d12k"));
-	assert_int_equal(1*12, last_selector_count);
-	assert_int_equal(NO_COUNT_GIVEN, last_command_count);
-
-	assert_int_equal(KEYS_WAIT, execute_keys(L"d123"));
-	assert_int_equal(0, execute_keys(L"d123k"));
-	assert_int_equal(1*123, last_selector_count);
-	assert_int_equal(NO_COUNT_GIVEN, last_command_count);
+	check(L"d1",   L"d1k",   1*1,   NO_COUNT_GIVEN);
+	check(L"d12",  L"d12k",  1*12,  NO_COUNT_GIVEN);
+	check(L"d123", L"d123k", 1*123, NO_COUNT_GIVEN);
 }
 
 static void
@@ -47,20 +42,9 @@ test_with_zero_number_fail(void)
 static void
 test_with_number_before_and_in_the_middle_ok(void)
 {
-	assert_int_equal(KEYS_WAIT, execute_keys(L"2d1"));
-	assert_int_equal(0, execute_keys(L"2d1k"));
-	assert_int_equal(2*1, last_selector_count);
-	assert_int_equal(NO_COUNT_GIVEN, last_command_count);
-
-	assert_int_equal(KEYS_WAIT, execute_keys(L"1d12"));
-	assert_int_equal(0, execute_keys(L"3d12k"));
-	assert_int_equal(3*12, last_selector_count);
-	assert_int_equal(NO_COUNT_GIVEN, last_command_count);
-
-	assert_int_equal(KEYS_WAIT, execute_keys(L"2d123"));
-	assert_int_equal(0, execute_keys(L"2d123k"));
-	assert_int_equal(2*123, last_selector_count);
-	assert_int_equal(NO_COUNT_GIVEN, last_command_count);
+	check(L"2d1",   L"2d1k",   2*1,   NO_COUNT_GIVEN);
+	check(L"3d12",  L"3d12k",  3*12,  NO_COUNT_GIVEN);
+	check(L"2d123", L"2d123k", 2*123, NO_COUNT_GIVEN);
 }
 
 void
