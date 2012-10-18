vifm/vifm

commit 60fa899e98353c9c6eec4dbf8a53f82823ff52ec

Fix small issue with sorting files with numbers
strverscmp() on *nix sorts 0 and 0x wrong, it places 0 after 0x,
so need to skip all leading zero characters manually.

src/sort.c
@@ -94,13 +94,19 @@ compare_file_names(const char *s, const char *t, int ignore_case)
 	}
 
 	if(!cfg.sort_numbers)
+	{
 		return strcmp(s, t);
+	}
 	else
+	{
 #if defined(_WIN32) || defined(__APPLE__) || defined(__CYGWIN__)
 		return vercmp(s, t);
 #else
+		s = skip_all(s, '0');
+		t = skip_all(t, '0');
 		return strverscmp(s, t);
 #endif
+	}
 }
 
 #if defined(_WIN32) || defined(__APPLE__) || defined(__CYGWIN__)

src/utils/str.c
@@ -280,5 +280,15 @@ is_null_or_empty(const char string[])
 	return string == NULL || string[0] == '\0';
 }
 
+char *
+skip_all(const char string[], char ch)
+{
+	while(*string == ch)
+	{
+		string++;
+	}
+	return (char *)string;
+}
+
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
 /* vim: set cinoptions+=t0 : */

src/utils/str.h
@@ -73,6 +73,8 @@ const char * extract_part(const char str[], char separator, char part_buf[]);
 char * escape_chars(const char string[], const char chars[]);
 /* Returns non-zero if the string is NULL or empty. */
 int is_null_or_empty(const char string[]);
+/* Returns pointer to first character in the string not equal to the ch. */
+char * skip_all(const char string[], char ch);
 
 #endif
 

commit b5aaf3fdc209fd845490240435a555865bd510c8

Fix bug #35 at SourceForge
Was introduced in 60fa899e98.

TODO
@@ -23,6 +23,8 @@ Basic things that need to be done.
         only, and maybe update size of all parent cached directories. And maybe
         show somehow in the TUI calculation process.
     Make ga and gA work in visual mode.
+    Better remove confirmetion messages (should contain list of files).
+    Better failed operation messages (list of errors, not just one first error).
 
 Documentation.
     Better documentation.
@@ -36,6 +38,12 @@ Windows.
     Fix freezing (maybe already fixed).
     Use UTF8 internally.
     Make 'fastrun' understand file extensions on Windows (to ignore them).
+    Maybe read first bytes of files and threat files as executable if first
+        two bytes equal to '$!'.
+    Remove '     :     ' from default Windows statusbar (it's left from
+        user:group on *nix).
+    Make 'slowfs' option available on Windows and provide a support for builtin
+        file system time 'winnetwork', which will mean network resources.
 
 Vi(m) specific features that need to be added.
     Fix '' to go back to all jumps G gg links (in one directory?).
@@ -67,6 +75,8 @@ Vi(m) specific features that need to be added.
     Make d2d, y2y and D2D work as in Vim.
     Quickfix list support (to navigate through file list produced by :find or
         :locate with :cn, :cp and alike).
+    Maybe add 'titleold' option.
+    Maybe add "%p" and "%P" macros to 'rulerformat' option.
 
 Possible things to add.
     Use system wide mimetypes file. /etc/mailcap?
@@ -91,8 +101,13 @@ Possible things to add.
     $_ env var and Alt-T combination as in bash shell.
     Rename existing file option on name conflicts.
     Support of command line arguments like ~/some.ssh/server/path (for FUSE).
+<<<<<<< HEAD
     Think about running vim on :help command in kind of sandbox, so the only
         help files available were those provided by vifm.
+=======
+    Maybe provide a way to skip non-existent directory errors on <c-o>/<c-i>.
+    Something to control the frequency of directory change changes.
+>>>>>>> Fix bug #35 at SourceForge
 
 Other (questionable) things.
     Use mimetypes in FILETYPE.

src/sort.c
@@ -102,9 +102,16 @@ strnumcmp(const char s[], const char t[])
 #if defined(_WIN32) || defined(__APPLE__) || defined(__CYGWIN__)
 		return vercmp(s, t);
 #else
-		s = skip_all(s, '0');
-		t = skip_all(t, '0');
-		return strverscmp(s, t);
+		const char *new_s = skip_all(s, '0');
+		const char *new_t = skip_all(t, '0');
+		if(new_s[0] == '\0' || new_t == '\0')
+		{
+			return strverscmp(new_s, new_t);
+		}
+		else
+		{
+			return strverscmp(s, t);
+		}
 #endif
 }
 

tests/misc/sort_tests.c
@@ -192,6 +192,16 @@ test_versort_zero_and_zerox(void)
 	assert_true(strnumcmp(s, t) < 0);
 }
 
+static void
+test_versort_zerox_and_one(void)
+{
+	const char *s, *t;
+
+	s = "00_";
+	t = "01";
+	assert_true(strnumcmp(s, t) < 0);
+}
+
 void
 sort_tests(void)
 {
@@ -207,6 +217,7 @@ sort_tests(void)
 	run_test(test_versort_numbers_only);
 	run_test(test_versort_numbers_only_and_letters_only);
 	run_test(test_versort_zero_and_zerox);
+	run_test(test_versort_zerox_and_one);
 
 	test_fixture_end();
 }