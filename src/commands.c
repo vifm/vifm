a63b06a0af1d2a569f7b222947c767709d4f16fb
xaizek <xaizek@openmailbox.org>
2013-10-02 23:38:20 +0300

    Reload original local options after winrun
    Otherwise pane could use options from another pane.
    
    Thanks to filterfalse.

--- src/commands.c
+++ src/commands.c
@@ -3658,17 +3658,19 @@ static int
 winrun(FileView *view, const char cmd[])
 {
 	int result;
-	FileView *tmp_curr = curr_view;
-	FileView *tmp_other = other_view;
+	FileView *const tmp_curr = curr_view;
+	FileView *const tmp_other = other_view;
 
 	curr_view = view;
 	other_view = (view == tmp_curr) ? tmp_other : tmp_curr;
-
 	load_local_options(curr_view);
+
 	result = exec_commands(cmd, curr_view, GET_COMMAND);
 
 	curr_view = tmp_curr;
 	other_view = tmp_other;
+	load_local_options(curr_view);
+
 	return result;
 }
 
