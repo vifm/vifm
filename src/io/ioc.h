0f26450404584b131d080085ac5399f4540bb18b
xaizek <xaizek@openmailbox.org>
2013-11-19 21:49:06 +0200

    Add signatures for new io functions

---
+++ src/io/ioc.h
@@ -0,0 +1,70 @@
+/* vifm
+ * Copyright (C) 2013 xaizek.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#ifndef VIFM__IO__IOC_H__
+#define VIFM__IO__IOC_H__
+
+#include <sys/types.h> /* gid_t mode_t uid_t */
+
+#include "ioe.h"
+
+/* ioc - I/O common - Input/Output common */
+
+typedef struct
+{
+	io_err_cb errors_cb;
+	io_errlst_t errors;
+}
+io_result_t;
+
+typedef struct
+{
+	union
+	{
+		const char *path;
+		const char *src;
+	}
+	arg1;
+
+	union
+	{
+		const char *dst;
+		const char *target;
+	}
+	arg2;
+
+	union
+	{
+		int parent;
+		int overwrite;
+		mode_t mode;
+#ifndef _WIN32
+		uid_t uid;
+		gid_t gid;
+#endif
+	}
+	arg3;
+
+	io_result_t result;
+}
+io_args_t;
+
+#endif /* VIFM__IO__IOC_H__ */
+
+/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
+/* vim: set cinoptions+=t0 : */
---
+++ src/io/iop.h
@@ -0,0 +1,68 @@
+/* vifm
+ * Copyright (C) 2013 xaizek.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#ifndef VIFM__IO__IOP_H__
+#define VIFM__IO__IOP_H__
+
+#include "ioc.h"
+
+/* iop - I/O primitive - Input/Output primitive */
+
+/* All functions return zero on success and non-zero on error. */
+
+/* Creates file.  Expects path in arg1.  Fails if one already exists. */
+int iop_mkfile(io_args_t *const args);
+
+/* Creates directory or hierarchical directory structure (if parent is true).
+ * Expects path in arg1 and parent in arg3.  When parent is false, fails if
+ * destination already exists. */
+int iop_mkdir(io_args_t *const args);
+
+/* Deletes file.  Expects path in arg1. */
+int iop_rmfile(io_args_t *const args);
+
+/* Delete empty directory.  Expects path in arg1.  Fails if directory is not
+ * empty. */
+int iop_rmdir(io_args_t *const args);
+
+/* Copies file.  Expects path in arg1 and overwrite in arg3. */
+int iop_cp(io_args_t *const args);
+
+/* Moves/renames file/directory.  Expects src in arg1, dst in arg2 and overwrite
+ * in arg3. */
+int iop_mv(io_args_t *const args);
+
+/* Change owner of file/directory.  Expects path in arg1 and uid in arg3. */
+int iop_chown(io_args_t *const args);
+
+/* Change group of file/directory.  Expects path in arg1 and gid in arg3. */
+int iop_chgrp(io_args_t *const args);
+
+/* Change permissions of file/directory.  Expects path in arg1 and mode in
+ * arg3. */
+int iop_chmod(io_args_t *const args);
+
+/* Create symbolic link or change its target.  Expects path in arg1, target in
+ * arg2 and overwrite in arg3.  Fails to overwrite if path exists and not a
+ * symbolic link. */
+int iop_ln(io_args_t *const args);
+
+#endif /* VIFM__IO__IOP_H__ */
+
+/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
+/* vim: set cinoptions+=t0 : */
