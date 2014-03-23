d471d835bbd4ea46d7dc2f22c36ae3fbb0be19eb
xaizek <xaizek@openmailbox.org>
2014-03-23 15:34:50 +0200

    Rename dirsta{s,c}k_menu.{h,c}
    A typo.

--- src/menus/dirstack_menu.c
+++ src/menus/dirstack_menu.c
@@ -17,7 +17,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#include "dirstask_menu.h"
+#include "dirstack_menu.h"
 
 #include <string.h> /* strdup() */
 
