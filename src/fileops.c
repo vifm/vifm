782bbe62afa947b60682506e823db56f95ea39fa
xaizek <xaizek@openmailbox.org>
2014-06-16 19:22:16 +0300

    Use background task in cpmv_files_bg()

--- src/fileops.c
+++ src/fileops.c
@@ -125,6 +125,7 @@ static int have_read_access(FileView *view);
 static char ** edit_list(size_t count, char **orig, int *nlines,
 		int ignore_change);
 static int edit_file(const char filepath[], int force_changed);
+static void cpmv_in_bg(job_t *job, void *arg);
 static void general_prepare_for_bg_task(FileView *view, bg_args_t *args);
 static const char * get_cancellation_suffix(void);
 
@@ -2839,31 +2840,11 @@ cp_file(const char src_dir[], const char dst_dir[], const char src[],
 	return result;
 }
 
-static void *
-cpmv_stub(void *arg)
-{
-	bg_args_t *args = (bg_args_t *)arg;
-
-	add_inner_bg_job(args->job);
-
-	cpmv_files_bg_i(args->list, args->nlines, args->move, args->force,
-			args->sel_list, args->sel_list_len, args->from_trash, args->src,
-			args->path);
-
-	remove_inner_bg_job();
-
-	free_string_array(args->list, args->nlines);
-	free_string_array(args->sel_list, args->sel_list_len);
-	free(args);
-	return NULL;
-}
-
 int
 cpmv_files_bg(FileView *view, char **list, int nlines, int move, int force)
 {
-	pthread_t id;
 	int i;
-	char buf[COMMAND_GROUP_INFO_LEN];
+	char task_desc[COMMAND_GROUP_INFO_LEN];
 	bg_args_t *args = malloc(sizeof(*args));
 	
 	args->list = NULL;
@@ -2871,34 +2852,41 @@ cpmv_files_bg(FileView *view, char **list, int nlines, int move, int force)
 	args->move = move;
 	args->force = force;
 
-	i = cpmv_prepare(view, &list, &args->nlines, move, 0, force, buf, sizeof(buf),
-			args->path, &args->from_file, &args->from_trash);
+	i = cpmv_prepare(view, &list, &args->nlines, move, 0, force, task_desc,
+			sizeof(task_desc), args->path, &args->from_file, &args->from_trash);
 	if(i != 0)
 	{
 		free(args);
 		return i > 0;
 	}
 
-	if(args->from_file)
-		args->list = list;
-	else
-		args->list = copy_string_array(list, nlines);
+	args->list = args->from_file ? list : copy_string_array(list, nlines);
 
 	general_prepare_for_bg_task(view, args);
 
-	args->job = add_background_job(-1, buf, NO_JOB_ID);
-	if(args->job == NULL)
+	if(bg_execute(task_desc, args->sel_list_len, &cpmv_in_bg, args) != 0)
 	{
 		free_string_array(args->list, args->nlines);
 		free_string_array(args->sel_list, args->sel_list_len);
 		free(args);
-		return 0;
 	}
 
-	args->job->total = args->sel_list_len;
-
-	pthread_create(&id, NULL, cpmv_stub, args);
 	return 0;
+}
+
+/* Entry point for a background task that copies/moves files. */
+static void
+cpmv_in_bg(job_t *job, void *arg)
+{
+	bg_args_t *const args = arg;
+
+	cpmv_files_bg_i(args->list, args->nlines, args->move, args->force,
+			args->sel_list, args->sel_list_len, args->from_trash, args->src,
+			args->path);
+
+	free_string_array(args->list, args->nlines);
+	free_string_array(args->sel_list, args->sel_list_len);
+	free(args);
 }
 
 /* Fills basic fields of the args structure. */
