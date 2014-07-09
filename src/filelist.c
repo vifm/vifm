5d29305e9bbeb78eb1c10ec94c60f2d50beead9b
xaizek <xaizek@openmailbox.org>
2014-07-09 09:23:03 +0300

    Fix a typo filelist.c:CASE_SENS{A,I}TIVE_FILTER

--- src/filelist.c
+++ src/filelist.c
@@ -75,9 +75,9 @@
 #include "ui.h"
 
 #ifdef _WIN32
-#define CASE_SENSATIVE_FILTER 0
+#define CASE_SENSITIVE_FILTER 0
 #else
-#define CASE_SENSATIVE_FILTER 1
+#define CASE_SENSITIVE_FILTER 1
 #endif
 
 /* Mark for a cursor position of inactive pane. */
@@ -506,7 +506,7 @@ reset_filter(filter_t *filter)
 {
 	if(filter->raw == NULL)
 	{
-		filter_init(filter, CASE_SENSATIVE_FILTER);
+		filter_init(filter, CASE_SENSITIVE_FILTER);
 	}
 	else
 	{
