b97e5cb2a14d40ce7599e93fdee14fa41d4d3080
xaizek <xaizek@openmailbox.org>
2014-07-24 20:29:24 +0300

    Add simple bookmarks querying unit-test

--- tests/misc/bookmarks.c
+++ tests/misc/bookmarks.c
@@ -1,5 +1,8 @@
 #include "seatest.h"
 
+#include <stddef.h> /* NULL */
+#include <string.h> /* strlen() */
+
 #include "../../src/bookmarks.h"
 #include "../../src/ui.h"
 
@@ -9,12 +12,24 @@ test_unexistant_bookmark(void)
 	assert_int_equal(1, goto_bookmark(&lwin, 'b'));
 }
 
+static void
+test_all_valid_bookmarks_can_be_queried(void)
+{
+	const int bookmark_count = strlen(valid_bookmarks);
+	int i;
+	for(i = 0; i < bookmark_count; ++i)
+	{
+		assert_true(get_bookmark(i) != NULL);
+	}
+}
+
 void
 bookmarks_tests(void)
 {
 	test_fixture_start();
 
 	run_test(test_unexistant_bookmark);
+	run_test(test_all_valid_bookmarks_can_be_queried);
 
 	test_fixture_end();
 }
