141395675c4355fbab60923fcd33e96921894cd2
xaizek <xaizek@openmailbox.org>
2014-08-16 13:45:15 +0300

    Add engine/mode unit
    Contains generic mode related routines.

---
+++ src/engine/mode.c
@@ -0,0 +1,36 @@
+/* vifm
+ * Copyright (C) 2014 xaizek.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include "mode.h"
+
+static vle_mode_t current_mode;
+
+const vle_mode_t
+vle_mode_get(void)
+{
+	return current_mode;
+}
+
+void
+vle_mode_set(vle_mode_t mode)
+{
+	current_mode = mode;
+}
+
+/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
+/* vim: set cinoptions+=t0 : */
---
+++ src/engine/mode.h
@@ -0,0 +1,34 @@
+/* vifm
+ * Copyright (C) 2014 xaizek.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#ifndef VIFM__ENGINE__MODE_H__
+#define VIFM__ENGINE__MODE_H__
+
+/* Mode identifier.  It's of integer type. */
+typedef int vle_mode_t;
+
+/* Gets identifier of currently active mode.  Returns the id. */
+const vle_mode_t vle_mode_get(void);
+
+/* Sets current mode. */
+void vle_mode_set(vle_mode_t mode);
+
+#endif // VIFM__ENGINE__MODE_H__
+
+/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
+/* vim: set cinoptions+=t0 : */
