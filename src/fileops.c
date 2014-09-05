b616e9b9158892043d95cf2e2a9cb37f301881c9
xaizek <xaizek@openmailbox.org>
2014-09-05 21:28:18 +0300

    Declare fileops:complete_filename() on Windows

--- src/fileops.c
+++ src/fileops.c
@@ -122,9 +122,7 @@ static void change_owner_cb(const char new_owner[]);
 #ifndef _WIN32
 static int complete_group(const char str[], void *arg);
 #endif
-#ifndef _WIN32
 static int complete_filename(const char str[], void *arg);
-#endif
 static void put_confirm_cb(const char dest_name[]);
 static void prompt_what_to_do(const char src_name[]);
 TSTATIC const char * gen_clone_name(const char normal_name[]);
