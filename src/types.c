2997f4248a481090c5eaf9cb4e997653129725d9
xaizek <xaizek@openmailbox.org>
2015-01-30 20:32:36 +0200

    Make switch() statements of types.c more readable

--- src/types.c
+++ src/types.c
@@ -62,28 +62,17 @@ get_type_from_mode(mode_t mode)
 {
 	switch(mode & S_IFMT)
 	{
+		case S_IFDIR:  return FT_DIR;
+		case S_IFCHR:  return FT_CHAR_DEV;
+		case S_IFBLK:  return FT_BLOCK_DEV;
+		case S_IFIFO:  return FT_FIFO;
 #ifndef _WIN32
-		case S_IFLNK:
-			return FT_LINK;
-#endif
-		case S_IFDIR:
-			return FT_DIR;
-		case S_IFCHR:
-			return FT_CHAR_DEV;
-		case S_IFBLK:
-			return FT_BLOCK_DEV;
-#ifndef _WIN32
-		case S_IFSOCK:
-			return FT_SOCK;
-#endif
-		case S_IFREG:
-#ifndef _WIN32
-			return S_ISEXE(mode) ? FT_EXEC : FT_REG;
+		case S_IFLNK:  return FT_LINK;
+		case S_IFSOCK: return FT_SOCK;
+		case S_IFREG:  return S_ISEXE(mode) ? FT_EXEC : FT_REG;
 #else
-			return FT_REG;
+		case S_IFREG:  return FT_REG;
 #endif
-		case S_IFIFO:
-			return FT_FIFO;
 
 		default:
 			return FT_UNK;
@@ -91,30 +80,25 @@ get_type_from_mode(mode_t mode)
 }
 
 #ifndef _WIN32
+
 FileType
 type_from_dir_entry(const struct dirent *d)
 {
 	switch(d->d_type)
 	{
-		case DT_CHR:
-			return FT_CHAR_DEV;
-		case DT_BLK:
-			return FT_BLOCK_DEV;
-		case DT_DIR:
-			return FT_DIR;
-		case DT_LNK:
-			return FT_LINK;
-		case DT_REG:
-			return FT_REG;
-		case DT_SOCK:
-			return FT_SOCK;
-		case DT_FIFO:
-			return FT_FIFO;
+		case DT_LNK:  return FT_LINK;
+		case DT_DIR:  return FT_DIR;
+		case DT_CHR:  return FT_CHAR_DEV;
+		case DT_BLK:  return FT_BLOCK_DEV;
+		case DT_SOCK: return FT_SOCK;
+		case DT_REG:  return FT_REG;
+		case DT_FIFO: return FT_FIFO;
 
 		default:
 			return FT_UNK;
 	}
 }
+
 #endif
 
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
