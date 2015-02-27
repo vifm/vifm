4c334e94802d0075938c4ff0ec98fa8808d0ad12
xaizek <xaizek@openmailbox.org>
2015-02-27 22:18:05 +0200

    Remove temporary seatest.h header
    Was used for smooth transition to new framework.

--- tests/stic/seatest.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SEATEST_H__
-#define __SEATEST_H__
-
-#include "stic.h"
-
-#endif /* __SEATEST_H__ */
-
-/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
-/* vim: set cinoptions+=t0 : */
