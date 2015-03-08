9557cb079729acba914b207ac605b09ea487405a
xaizek <xaizek@openmailbox.org>
2015-03-08 23:05:13 +0200

    Less duplication in tests/iop/cp fixture
    Extract common functionality to functions, issue with failing test
    reporting will be addressed in stic soon.

--- tests/iop/cp.c
+++ tests/iop/cp.c
@@ -11,6 +11,9 @@
 #include "../../src/utils/fs_limits.h"
 #include "../../src/utils/utils.h"
 
+static void create_test_file(const char name[]);
+static void clone_test_file(const char src[], const char dst[]);
+static void delete_test_file(const char name[]);
 static int not_windows(void);
 
 static int
@@ -35,8 +38,7 @@ files_are_identical(const char a[], const char b[])
 
 TEST(dir_is_not_copied)
 {
-	io_args_t args =
-	{
+	io_args_t args = {
 		.arg1.src = "../existing-files",
 		.arg2.dst = "existing-files",
 	};
@@ -45,17 +47,10 @@ TEST(dir_is_not_copied)
 
 TEST(empty_file_is_copied)
 {
-	{
-		io_args_t args =
-		{
-			.arg1.path = "empty",
-		};
-		assert_int_equal(0, iop_mkfile(&args));
-	}
+	create_test_file("empty");
 
 	{
-		io_args_t args =
-		{
+		io_args_t args = {
 			.arg1.src = "empty",
 			.arg2.dst = "empty-copy",
 		};
@@ -64,45 +59,17 @@ TEST(empty_file_is_copied)
 
 	assert_true(files_are_identical("empty", "empty-copy"));
 
-	{
-		io_args_t args =
-		{
-			.arg1.path = "empty",
-		};
-		assert_int_equal(0, iop_rmfile(&args));
-	}
-
-	{
-		io_args_t args =
-		{
-			.arg1.path = "empty-copy",
-		};
-		assert_int_equal(0, iop_rmfile(&args));
-	}
+	delete_test_file("empty");
+	delete_test_file("empty-copy");
 }
 
 TEST(file_is_not_overwritten_if_not_asked)
 {
-	{
-		io_args_t args =
-		{
-			.arg1.path = "empty",
-		};
-		assert_int_equal(0, iop_mkfile(&args));
-	}
+	create_test_file("empty");
+	clone_test_file("empty", "empty-copy");
 
 	{
-		io_args_t args =
-		{
-			.arg1.src = "empty",
-			.arg2.dst = "empty-copy",
-		};
-		assert_int_equal(0, iop_cp(&args));
-	}
-
-	{
-		io_args_t args =
-		{
+		io_args_t args = {
 			.arg1.src = "../read/two-lines",
 			.arg2.dst = "empty-copy",
 			.arg3.crs = IO_CRS_FAIL,
@@ -113,28 +80,14 @@ TEST(file_is_not_overwritten_if_not_asked)
 	assert_true(files_are_identical("empty", "empty-copy"));
 	assert_false(files_are_identical("../read/two-lines", "empty-copy"));
 
-	{
-		io_args_t args =
-		{
-			.arg1.path = "empty",
-		};
-		assert_int_equal(0, iop_rmfile(&args));
-	}
-
-	{
-		io_args_t args =
-		{
-			.arg1.path = "empty-copy",
-		};
-		assert_int_equal(0, iop_rmfile(&args));
-	}
+	delete_test_file("empty");
+	delete_test_file("empty-copy");
 }
 
 TEST(file_is_overwritten_if_asked)
 {
 	{
-		io_args_t args =
-		{
+		io_args_t args = {
 			.arg1.src = "../read/two-lines",
 			.arg2.dst = "two-lines",
 			.arg3.crs = IO_CRS_FAIL,
@@ -145,8 +98,7 @@ TEST(file_is_overwritten_if_asked)
 	assert_false(files_are_identical("../read/binary-data", "two-lines"));
 
 	{
-		io_args_t args =
-		{
+		io_args_t args = {
 			.arg1.src = "../read/binary-data",
 			.arg2.dst = "two-lines",
 			.arg3.crs = IO_CRS_REPLACE_FILES,
@@ -156,21 +108,14 @@ TEST(file_is_overwritten_if_asked)
 
 	assert_true(files_are_identical("../read/binary-data", "two-lines"));
 
-	{
-		io_args_t args =
-		{
-			.arg1.path = "two-lines",
-		};
-		assert_int_equal(0, iop_rmfile(&args));
-	}
+	delete_test_file("two-lines");
 }
 
 static void
 file_is_copied(const char original[])
 {
 	{
-		io_args_t args =
-		{
+		io_args_t args = {
 			.arg1.src = original,
 			.arg2.dst = "copy",
 		};
@@ -179,13 +124,7 @@ file_is_copied(const char original[])
 
 	assert_true(files_are_identical("copy", original));
 
-	{
-		io_args_t args =
-		{
-			.arg1.path = "copy",
-		};
-		assert_int_equal(0, iop_rmfile(&args));
-	}
+	delete_test_file("copy");
 }
 
 TEST(block_size_file_is_copied)
@@ -222,20 +161,12 @@ TEST(appending_works_for_files)
 {
 	uint64_t size;
 
-	{
-		io_args_t args =
-		{
-			.arg1.src = "../various-sizes/block-size-minus-one-file",
-			.arg2.dst = "appending",
-		};
-		assert_int_equal(0, iop_cp(&args));
-	}
+	clone_test_file("../various-sizes/block-size-minus-one-file", "appending");
 
 	size = get_file_size("appending");
 
 	{
-		io_args_t args =
-		{
+		io_args_t args = {
 			.arg1.src = "../various-sizes/block-size-file",
 			.arg2.dst = "appending",
 			.arg3.crs = IO_CRS_APPEND_TO_FILES,
@@ -246,8 +177,7 @@ TEST(appending_works_for_files)
 	assert_int_equal(size + 1, get_file_size("appending"));
 
 	{
-		io_args_t args =
-		{
+		io_args_t args = {
 			.arg1.src = "../various-sizes/block-size-plus-one-file",
 			.arg2.dst = "appending",
 			.arg3.crs = IO_CRS_APPEND_TO_FILES,
@@ -257,33 +187,19 @@ TEST(appending_works_for_files)
 
 	assert_int_equal(size + 2, get_file_size("appending"));
 
-	{
-		io_args_t args =
-		{
-			.arg1.path = "appending",
-		};
-		assert_int_equal(0, iop_rmfile(&args));
-	}
+	delete_test_file("appending");
 }
 
 TEST(appending_does_not_shrink_files)
 {
 	uint64_t size;
 
-	{
-		io_args_t args =
-		{
-			.arg1.src = "../read/two-lines",
-			.arg2.dst = "two-lines",
-		};
-		assert_int_equal(0, iop_cp(&args));
-	}
+	clone_test_file("../read/two-lines", "two-lines");
 
 	size = get_file_size("two-lines");
 
 	{
-		io_args_t args =
-		{
+		io_args_t args = {
 			.arg1.src = "../existing-files/a",
 			.arg2.dst = "two-lines",
 			.arg3.crs = IO_CRS_APPEND_TO_FILES,
@@ -308,13 +224,7 @@ TEST(file_permissions_are_preserved, IF(not_windows))
 	struct stat src;
 	struct stat dst;
 
-	{
-		io_args_t args =
-		{
-			.arg1.path = "file",
-		};
-		assert_int_equal(0, iop_mkfile(&args));
-	}
+	create_test_file("file");
 
 	assert_int_equal(0, chmod("file", 0200));
 
@@ -324,8 +234,7 @@ TEST(file_permissions_are_preserved, IF(not_windows))
 	assert_int_equal(0, chmod("file", 0600));
 
 	{
-		io_args_t args =
-		{
+		io_args_t args = {
 			.arg1.src = "file",
 			.arg2.dst = "file-copy",
 		};
@@ -336,21 +245,8 @@ TEST(file_permissions_are_preserved, IF(not_windows))
 	assert_int_equal(0, lstat("file-copy", &dst));
 	assert_int_equal(src.st_mode & 0777, dst.st_mode & 0777);
 
-	{
-		io_args_t args =
-		{
-			.arg1.path = "file",
-		};
-		assert_int_equal(0, iop_rmfile(&args));
-	}
-
-	{
-		io_args_t args =
-		{
-			.arg1.path = "file-copy",
-		};
-		assert_int_equal(0, iop_rmfile(&args));
-	}
+	delete_test_file("file");
+	delete_test_file("file-copy");
 }
 
 /* Creating symbolic links on Windows requires administrator rights. */
@@ -360,8 +256,7 @@ TEST(file_symlink_copy_is_symlink, IF(not_windows))
 	char new_target[PATH_MAX];
 
 	{
-		io_args_t args =
-		{
+		io_args_t args = {
 			.arg1.path = "../read/two-lines",
 			.arg2.target = "sym-link",
 		};
@@ -371,8 +266,7 @@ TEST(file_symlink_copy_is_symlink, IF(not_windows))
 	assert_true(is_symlink("sym-link"));
 
 	{
-		io_args_t args =
-		{
+		io_args_t args = {
 			.arg1.src = "sym-link",
 			.arg2.dst = "sym-link-copy",
 		};
@@ -389,21 +283,8 @@ TEST(file_symlink_copy_is_symlink, IF(not_windows))
 
 	assert_string_equal(new_target, old_target);
 
-	{
-		io_args_t args =
-		{
-			.arg1.path = "sym-link",
-		};
-		assert_int_equal(0, iop_rmfile(&args));
-	}
-
-	{
-		io_args_t args =
-		{
-			.arg1.path = "sym-link-copy",
-		};
-		assert_int_equal(0, iop_rmfile(&args));
-	}
+	delete_test_file("sym-link");
+	delete_test_file("sym-link-copy");
 }
 
 /* Creating symbolic links on Windows requires administrator rights. */
@@ -413,8 +294,7 @@ TEST(dir_symlink_copy_is_symlink, IF(not_windows))
 	assert_true(is_dir("dir"));
 
 	{
-		io_args_t args =
-		{
+		io_args_t args = {
 			.arg1.path = "dir",
 			.arg2.target = "dir-sym-link",
 		};
@@ -425,8 +305,7 @@ TEST(dir_symlink_copy_is_symlink, IF(not_windows))
 	assert_true(is_dir("dir-sym-link"));
 
 	{
-		io_args_t args =
-		{
+		io_args_t args = {
 			.arg1.src = "dir-sym-link",
 			.arg2.dst = "dir-sym-link-copy",
 		};
@@ -438,29 +317,43 @@ TEST(dir_symlink_copy_is_symlink, IF(not_windows))
 	assert_true(is_symlink("dir-sym-link-copy"));
 	assert_true(is_dir("dir-sym-link-copy"));
 
-	{
-		io_args_t args =
-		{
-			.arg1.path = "dir-sym-link",
-		};
-		assert_int_equal(0, iop_rmfile(&args));
-	}
+	delete_test_file("dir-sym-link");
+	delete_test_file("dir-sym-link-copy");
 
 	{
-		io_args_t args =
-		{
-			.arg1.path = "dir-sym-link-copy",
-		};
-		assert_int_equal(0, iop_rmfile(&args));
-	}
-
-	{
-		io_args_t args =
-		{
+		io_args_t args = {
 			.arg1.path = "dir",
 		};
 		assert_int_equal(0, iop_rmdir(&args));
 	}
+}
+
+static void
+create_test_file(const char name[])
+{
+	io_args_t args = {
+		.arg1.path = name,
+	};
+	assert_success(iop_mkfile(&args));
+}
+
+static void
+clone_test_file(const char src[], const char dst[])
+{
+	io_args_t args = {
+		.arg1.src = src,
+		.arg2.dst = dst,
+	};
+	assert_success(iop_cp(&args));
+}
+
+static void
+delete_test_file(const char name[])
+{
+	io_args_t args = {
+		.arg1.path = name,
+	};
+	assert_success(iop_rmfile(&args));
 }
 
 static int
