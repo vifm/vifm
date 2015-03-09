46eb13f4be9011a1a7c6e72d13135dde62dab845
xaizek <xaizek@openmailbox.org>
2015-03-09 23:56:48 +0200

    Reset current dirs of views for all misc tests
    Some parts of the code implicitly rely on that data to be non-empty.
    
    And it's better to have something distinguishable there.

--- tests/misc/suite.c
+++ tests/misc/suite.c
@@ -1,6 +1,16 @@
 #include <stic.h>
 
+#include <string.h> /* strcpy() */
+
+#include "../../src/ui/ui.h"
+
 DEFINE_SUITE();
+
+SETUP()
+{
+	strcpy(lwin.curr_dir, "/non-existing-dir");
+	strcpy(rwin.curr_dir, "/non-existing-dir");
+}
 
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
 /* vim: set cinoptions+=t0 : */
