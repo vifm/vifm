6f3743ad9b36a6cdefe3a9d7b5e78b201cee2a6a
xaizek <xaizek@openmailbox.org>
2015-04-06 15:48:09 +0300

    Use /bin/[ instead of /bin/cat

--- tests/misc/cmdline_completion.c
+++ tests/misc/cmdline_completion.c
@@ -423,9 +423,9 @@ TEST(abbreviations)
  * path. */
 TEST(bang_abs_path_completion, IF(has_unix_like_fs))
 {
-	prepare_for_line_completion(L"!/bin/cat");
+	prepare_for_line_completion(L"!/bin/[");
 	assert_success(line_completion(&stats));
-	assert_wstring_equal(L"!/bin/cat", stats.line);
+	assert_wstring_equal(L"!/bin/\\[", stats.line);
 
 	assert_int_equal(2, vle_compl_get_count());
 }
