57db3355c50e03b850de702a15295efb6d5ff869
xaizek <xaizek@openmailbox.org>
2015-05-07 11:15:37 +0300

    Test + less duplication for missing sourcing error

---
+++ tests/misc/sourcing.c
@@ -0,0 +1,24 @@
+#include <stic.h>
+
+#include "../../src/cfg/config.h"
+#include "../../src/ui/ui.h"
+#include "../../src/commands.h"
+
+SETUP()
+{
+	init_commands();
+	lwin.selected_files = 0;
+}
+
+TEST(wrong_command_name_causes_error)
+{
+	assert_failure(cfg_source_file("test-data/scripts/wrong-cmd-name.vifm"));
+}
+
+TEST(wrong_user_defined_command_name_causes_error)
+{
+	assert_failure(cfg_source_file("test-data/scripts/wrong-udcmd-name.vifm"));
+}
+
+/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
+/* vim: set cinoptions+=t0 : */
