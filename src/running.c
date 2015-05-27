vifm/vifm

commit b4e774de6b61c38eb98aca573a2d19d597bf8577

Consistency in processing macros in ! and file[x]t
Thanks to Miodrag Tokić (a.k.a. loonies).

See https://github.com/vifm/vifm/issues/90#issuecomment-103858176.

src/running.c
@@ -574,22 +574,37 @@ run_using_prog(FileView *view, const char program[], int dont_execute,
 	}
 	else if(strchr(program, '%') != NULL)
 	{
-		int background;
+		int bg;
 		MacroFlags flags;
-		char *command = expand_macros(program, NULL, &flags, 1);
+		int save_msg = 0;
+		char *const cmd = expand_macros(program, NULL, &flags, 1);
+		int handled;
 
-		background = ends_with(command, " &");
-		if(background)
-			command[strlen(command) - 2] = '\0';
+		bg = ends_with(cmd, " &");
+		if(bg)
+		{
+			cmd[strlen(cmd) - 2] = '\0';
+		}
+		bg = !pause && (bg || force_background);
 
-		if(!pause && (background || force_background))
-			start_background_job(command, flags == MF_IGNORE);
-		else if(flags == MF_IGNORE)
-			output_to_nowhere(command);
+		handled = run_ext_command(cmd, flags, bg, &save_msg);
+		if(handled)
+		{
+			if(save_msg)
+			{
+				curr_stats.save_msg = 1;
+			}
+		}
+		else if(bg)
+		{
+			start_background_job(cmd, flags == MF_IGNORE);
+		}
 		else
-			shellout(command, pause ? 1 : -1, flags != MF_NO_TERM_MUX);
+		{
+			shellout(cmd, pause ? 1 : -1, flags != MF_NO_TERM_MUX);
+		}
 
-		free(command);
+		free(cmd);
 	}
 	else
 	{

commit 6f980ffb911928f7a0c03d173c6325445d28dece

Don't kill background child on quiting vifm
Was broken in b4e774de6b61.

Test case:
 1. exec vifm (exec is shell builtin)
 2. run some app in background
 3. quit vifm
 4. app is closed (it should be normally)

src/running.c
@@ -603,6 +603,7 @@ run_explicit_prog(const char prog_spec[], int pause, int force_bg)
 	}
 	else if(bg)
 	{
+		assert(flags != MF_IGNORE && "This case is for run_ext_command()");
 		start_background_job(cmd, flags == MF_IGNORE);
 	}
 	else
@@ -1122,8 +1123,19 @@ run_ext_command(const char cmd[], MacroFlags flags, int bg, int *save_msg)
 	}
 	else if(flags == MF_IGNORE)
 	{
-		output_to_nowhere(cmd);
 		*save_msg = 0;
+		if(bg)
+		{
+			if(start_background_job(cmd, 1) != 0)
+			{
+				status_bar_errorf("Failed to start in bg: %s", cmd);
+				*save_msg = 1;
+			}
+		}
+		else
+		{
+			output_to_nowhere(cmd);
+		}
 		return -1;
 	}
 	else if(flags == MF_MENU_OUTPUT || flags == MF_MENU_NAV_OUTPUT)