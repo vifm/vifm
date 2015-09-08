d53f7ed721d555962d3f105a434cfba45ad50cd8
xaizek <xaizek@openmailbox.org>
2015-09-08 21:29:20 +0300

    Windows part of IPC on pipes

--- src/ipc.c
+++ src/ipc.c
@@ -44,9 +44,19 @@ ipc_send(char *data[])
 
 #else
 
-#include <sys/stat.h> /* mkfifo */
+#ifndef _WIN32
 #include <sys/types.h>
-#include <sys/select.h> /* FD_* select */
+#include <sys/select.h> /* FD_* select() */
+#else
+#define O_NONBLOCK 0
+#define REQUIRED_WINVER 0x0600 /* To get PIPE_REJECT_REMOTE_CLIENTS. */
+#include "utils/windefs.h"
+#ifndef PIPE_REJECT_REMOTE_CLIENTS
+#define PIPE_REJECT_REMOTE_CLIENTS 1
+#endif
+#include <windows.h>
+#endif
+#include <sys/stat.h> /* mkfifo() stat() */
 #include <dirent.h> /* DIR closedir() opendir() readdir() */
 #include <fcntl.h>
 #include <unistd.h> /* close() open() select() unlink() */
@@ -70,15 +80,28 @@ ipc_send(char *data[])
 /* Prefix for names of all pipes to distinguish them from other pipes. */
 #define PREFIX "vifm-ipc-"
 
+/* Holds list information for add_to_list(). */
+typedef struct
+{
+	char **lst;          /* List of strings. */
+	size_t len;          /* Number of items. */
+	const char *ipc_dir; /* Root of IPC objects. */
+}
+list_data_t;
+
 static void clean_at_exit(void);
 static FILE * create_pipe(char path_buf[], size_t len);
-static char * receive(void);
+static char * receive_pkg(void);
 static FILE * try_use_pipe(const char path[]);
 static void handle_pkg(const char pkg[]);
-static int send(const char whom[], const char what[], size_t len);
+static int send_pkg(const char whom[], const char what[], size_t len);
 static char * get_the_only_target(void);
 static char ** list_pipes(int *len);
+static int add_to_list(const char name[], const void *data, void *param);
+#ifndef _WIN32
 static int pipe_is_in_use(const char path[]);
+#endif
+static const char * get_ipc_dir(void);
 static int sorter(const void *first, const void *second);
 
 /* Stores callback to report received messages. */
@@ -134,7 +157,7 @@ ipc_check(void)
 		return;
 	}
 
-	pkg = receive();
+	pkg = receive_pkg();
 	if(pkg == NULL)
 	{
 		return;
@@ -148,15 +171,17 @@ ipc_check(void)
  * message or on failure to read it, otherwise newly allocated string is
  * returned. */
 static char *
-receive(void)
+receive_pkg(void)
 {
 	uint32_t size;
 	char *pkg;
 	char *p;
 
+#ifndef _WIN32
 	fd_set ready;
 	int max_fd;
 	struct timeval ts = { .tv_sec = 0, .tv_usec = 10000 };
+#endif
 
 	if(fread(&size, sizeof(size), 1U, pipe_file) != 1U)
 	{
@@ -169,20 +194,51 @@ receive(void)
 		return NULL;
 	}
 
+#ifndef _WIN32
 	max_fd = fileno(pipe_file);
 	FD_ZERO(&ready);
 	FD_SET(max_fd, &ready);
+#endif
 
 	p = pkg;
-	while(size != 0U && select(max_fd + 1, &ready, NULL, NULL, &ts) > 0)
+	while(size != 0U
+#ifndef _WIN32
+			&& select(max_fd + 1, &ready, NULL, NULL, &ts) > 0
+#endif
+			)
 	{
-		const size_t read = fread(p, 1U, size, pipe_file);
+		size_t read;
+
+#ifdef _WIN32
+		/* TODO: maybe use OVERLAPPED I/O on Windows instead, it's just so
+		 *       inconvenient... */
+		usleep(10000);
+#endif
+
+		read = fread(p, 1U, size, pipe_file);
 		size -= read;
 		p += read;
 
+		if(read == 0U)
+		{
+			break;
+		}
+
+#ifndef _WIN32
 		ts.tv_sec = 0;
 		ts.tv_usec = 10000;
+#endif
 	}
+
+#ifdef _WIN32
+	{
+		/* Weird requirement for named pipes, need to break and set connection every
+		 * time. */
+		const HANDLE pipe_handle = (HANDLE)_get_osfhandle(fileno(pipe_file));
+		DisconnectNamedPipe(pipe_handle);
+		ConnectNamedPipe(pipe_handle, NULL);
+	}
+#endif
 
 	if(size != 0U)
 	{
@@ -207,7 +263,7 @@ create_pipe(char path_buf[], size_t len)
 
 	do
 	{
-		snprintf(path_buf, len, "%s/" PREFIX "%03d", get_tmpdir(), ++id);
+		snprintf(path_buf, len, "%s/" PREFIX "%03d", get_ipc_dir(), ++id);
 
 		if(id == 0)
 		{
@@ -226,6 +282,9 @@ create_pipe(char path_buf[], size_t len)
 static FILE *
 try_use_pipe(const char path[])
 {
+	FILE *f;
+
+#ifndef _WIN32
 	int fd;
 
 	/* Try to create a pipe. */
@@ -249,7 +308,33 @@ try_use_pipe(const char path[])
 
 	/* Use this file if we're the only one willing to read from it. */
 	fd = open(path, O_RDONLY | O_NONBLOCK);
-	return fdopen(fd, "r");
+#else
+	HANDLE h;
+	int fd;
+
+	h = CreateNamedPipeA(path,
+			PIPE_ACCESS_INBOUND | FILE_FLAG_FIRST_PIPE_INSTANCE,
+			PIPE_TYPE_BYTE | PIPE_NOWAIT | PIPE_REJECT_REMOTE_CLIENTS,
+			PIPE_UNLIMITED_INSTANCES, 4096, 4096, 10, NULL);
+	if(h == INVALID_HANDLE_VALUE)
+	{
+		return NULL;
+	}
+
+	fd = _open_osfhandle((intptr_t)h, _O_APPEND | _O_RDONLY);
+	if(fd == -1)
+	{
+		CloseHandle(h);
+		return NULL;
+	}
+#endif
+
+	f = fdopen(fd, "r");
+	if(f == NULL)
+	{
+		close(fd);
+	}
+	return f;
 }
 
 /* Parses pkg into array of strings and invokes callback. */
@@ -309,20 +394,20 @@ ipc_send(char *data[])
 		return 1;
 	}
 
-	return send(name, pkg, len);
+	return send_pkg(name, pkg, len);
 }
 
 /* Performs actual sending of package to another instance.  Returns zero on
  * success and non-zero otherwise. */
 static int
-send(const char whom[], const char what[], size_t len)
+send_pkg(const char whom[], const char what[], size_t len)
 {
 	char path[PATH_MAX];
 	int fd;
 	FILE *dst;
 	uint32_t size;
 
-	snprintf(path, sizeof(path), "%s/" PREFIX "%s", get_tmpdir(), whom);
+	snprintf(path, sizeof(path), "%s/" PREFIX "%s", get_ipc_dir(), whom);
 
 	fd = open(path, O_WRONLY | O_NONBLOCK);
 	if(fd == -1)
@@ -375,50 +460,58 @@ get_the_only_target(void)
 static char **
 list_pipes(int *len)
 {
-	DIR *dir;
-	struct dirent *d;
-	const char *tmp_dir = get_tmpdir();
+	list_data_t data = { .ipc_dir = get_ipc_dir() };
 
-	char **list = NULL;
-	*len = 0;
-
-	if((dir = opendir(tmp_dir)) == NULL)
+	if(enum_dir_content(data.ipc_dir, &add_to_list, &data) != 0)
 	{
+		*len = 0;
 		return NULL;
 	}
 
-	while((d = readdir(dir)) != NULL)
+	qsort(data.lst, data.len, sizeof(*data.lst), &sorter);
+
+	*len = data.len;
+	return data.lst;
+}
+
+/* Analyzes pipe and adds it to the list of pipes.  Returns zero on success or
+ * non-zero on error. */
+static int
+add_to_list(const char name[], const void *data, void *param)
+{
+	list_data_t *const list_data = param;
+
+	if(!starts_with_lit(name, PREFIX))
+	{
+		return 0;
+	}
+
+	/* Skip ourself. */
+	if(strcmp(name, get_last_path_component(pipe_path)) == 0)
+	{
+		return 0;
+	}
+
+	/* On Windows it's guaranteed to be a valid pipe. */
+#ifndef _WIN32
 	{
 		char path[PATH_MAX];
 		struct stat statbuf;
-
-		if(!starts_with_lit(d->d_name, PREFIX))
-		{
-			continue;
-		}
-
-		/* Skip ourself. */
-		if(strcmp(d->d_name, get_last_path_component(pipe_path)) == 0)
-		{
-			continue;
-		}
-
-		snprintf(path, sizeof(path), "%s/%s", tmp_dir, d->d_name);
-
+		snprintf(path, sizeof(path), "%s/%s", list_data->ipc_dir, name);
 		if(stat(path, &statbuf) != 0 || !S_ISFIFO(statbuf.st_mode) ||
 				!pipe_is_in_use(path))
 		{
-			continue;
+			return 0;
 		}
-
-		*len = add_to_string_array(&list, *len, 1, d->d_name + strlen(PREFIX));
 	}
-	closedir(dir);
+#endif
 
-	qsort(list, *len, sizeof(*list), &sorter);
-
-	return list;
+	list_data->len = add_to_string_array(&list_data->lst, list_data->len, 1,
+			name + strlen(PREFIX));
+	return 0;
 }
+
+#ifndef _WIN32
 
 /* Tries to open a pipe to check whether it has any readers or it's
  * abandoned.  Returns non-zero if somebody is reading from the pipe and zero
@@ -436,6 +529,19 @@ pipe_is_in_use(const char path[])
 		return 1;
 	}
 	return 0;
+}
+
+#endif
+
+/* Retrieves directory where FIFO objects are created.  Returns the path. */
+static const char *
+get_ipc_dir(void)
+{
+#ifndef _WIN32
+	return get_tmpdir();
+#else
+	return "//./pipe";
+#endif
 }
 
 /* Wraps strcmp() for use with qsort(). */
