vifm/vifm

commit 8ca283aacd4316e79bcd3d715f33b94e93e32dc2

Display lower bound on item count in progress
Progress on status bar, like on :grep.

This gives better idea of how many items are there and what's the
speed of the operation.

src/ui/ui.c
@@ -816,38 +816,52 @@ clear_num_window(void)
 	}
 }
 
-/* msg can't be NULL
- * period - how often status bar should be updated
- * if period equals 0 reset inner counter
- */
 void
-show_progress(const char *msg, int period)
+show_progress(const char msg[], int period)
 {
 	static char marks[] = {'|', '/', '-', '\\'};
-	static int count = 0;
-	static int pause = 1;
+	static int mark = 0;
+	static int count = 1;
+	static int total = 0;
 
+	/* Do nothing if UI is not functional. */
 	if(curr_stats.load_stage < 1)
 	{
 		return;
 	}
 
+	/* Reset state. */
 	if(period == 0)
 	{
-		pause = 1;
+		count = 1;
+		total = 0;
 		return;
 	}
 
-	pause++;
+	/* Advance state. */
+	++count;
+	++total;
 
-	if(pause%period != 0)
+	/* Skip intermediate updates to do not hammer UI with refreshes. */
+	if(count%period != 1)
+	{
 		return;
+	}
+	count = 1;
 
-	pause = 1;
+	/* Assume that period equal to one means that message already contains counter
+	 * (maybe along with total number). */
+	if(period == 1)
+	{
+		ui_sb_quick_msgf("%s %c", msg, marks[mark]);
+	}
+	else
+	{
+		ui_sb_quick_msgf("%s %c %d", msg, marks[mark], total);
+	}
 
-	ui_sb_quick_msgf("%s %c", msg, marks[count]);
-
-	count = (count + 1) % sizeof(marks);
+	/* Pick next mark character. */
+	mark = (mark + 1) % sizeof(marks);
 }
 
 void

src/ui/ui.h
@@ -370,7 +370,10 @@ void update_input_bar(const wchar_t *str);
 
 void clear_num_window(void);
 
-void show_progress(const char *msg, int period);
+/* Displays progress on the status bar, not updating it frequently.  msg can't
+ * be NULL.  period - how often status bar should be updated.  If period equals
+ * 0 inner counter is reset, do this on start of operation. */
+void show_progress(const char msg[], int period);
 
 void redraw_lists(void);
 

commit 6ab59219a9dbefadacba94168f96124e38cfe4e4

Restore unary progress messages (show_progress())
Got broken by 8ca283a, because anything % 1 == 0.

Thanks to Daniel R., a.k.a. reicheltd.

src/ui/ui.c
@@ -843,7 +843,7 @@ show_progress(const char msg[], int period)
 	++total;
 
 	/* Skip intermediate updates to do not hammer UI with refreshes. */
-	if(count%period != 1)
+	if(period != 1 && count%period != 1)
 	{
 		return;
 	}