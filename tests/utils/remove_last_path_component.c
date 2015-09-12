bf37a6ea7b144ca86781296a01b11f0d33ed16b0
xaizek <xaizek@openmailbox.org>
2015-09-12 19:20:19 +0300

    Fix possible hang during startup on Windows
    After removal of last visited directory.
    
    E.g. when you had "c:/dir" and removed it while vifm wasn't running.
    Actually, maybe it's not related to startup, although when tui is not
    ready user doesn't see any messages so it's definitely worth there.

--- tests/utils/remove_last_path_component.c
+++ tests/utils/remove_last_path_component.c
@@ -4,6 +4,8 @@
 
 #include "../../src/utils/path.h"
 
+static int windows(void);
+
 TEST(empty_path)
 {
 	char path[PATH_MAX] = "";
@@ -37,6 +39,35 @@ TEST(path_ends_with_multiple_slashes)
 	char path[PATH_MAX] = "/a/b/c///";
 	remove_last_path_component(path);
 	assert_string_equal("/a/b", path);
+}
+
+TEST(can_remove_path_completely)
+{
+	char path[PATH_MAX] = "name";
+
+	remove_last_path_component(path);
+	assert_true(path[0] == '\0');
+}
+
+TEST(can_remove_path_completely_on_windows, IF(windows))
+{
+	char path[PATH_MAX] = "c:/a";
+
+	remove_last_path_component(path);
+	assert_false(path[0] == '\0');
+
+	remove_last_path_component(path);
+	assert_true(path[0] == '\0');
+}
+
+static int
+windows(void)
+{
+#ifdef _WIN32
+	return 1;
+#else
+	return 0;
+#endif
 }
 
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
