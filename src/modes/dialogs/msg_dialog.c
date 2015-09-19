vifm/vifm

commit 1f52c98cc868560f9be3c07cf062751c99fe46de

Fix message dialog redraw aafter window resize
Multiple copies could appear on the screen.

src/modes/dialogs/msg_dialog.c
@@ -81,7 +81,7 @@ static void prompt_msg_internal(const char title[], const char message[],
 		const response_variant variants[]);
 static void enter(int result_mask);
 static void redraw_error_msg(const char title_arg[], const char message_arg[],
-		int prompt_skip);
+		int prompt_skip, int lazy);
 static const char * get_control_msg(Dialog msg_kind, int global_skip);
 static const char * get_custom_control_msg(const response_variant responses[]);
 static void draw_msg(const char title[], const char msg[],
@@ -220,9 +220,9 @@ leave(Result r)
 }
 
 void
-redraw_msg_dialog(void)
+redraw_msg_dialog(int lazy)
 {
-	redraw_error_msg(NULL, NULL, 0);
+	redraw_error_msg(NULL, NULL, 0, 1);
 }
 
 void
@@ -294,7 +294,7 @@ prompt_error_msg_internal(const char title[], const char message[],
 
 	msg_kind = D_ERROR;
 
-	redraw_error_msg(title, message, prompt_skip);
+	redraw_error_msg(title, message, prompt_skip, 0);
 
 	enter(MASK(R_OK) | (prompt_skip ? MASK(R_CANCEL) : 0));
 
@@ -334,7 +334,7 @@ prompt_msg_internal(const char title[], const char message[],
 	responses = variants;
 	msg_kind = D_QUERY;
 
-	redraw_error_msg(title, message, 0);
+	redraw_error_msg(title, message, 0, 0);
 
 	enter(MASK(R_YES, R_NO));
 
@@ -370,7 +370,7 @@ enter(int result_mask)
  * title_arg and message_arg are NULL. */
 static void
 redraw_error_msg(const char title_arg[], const char message_arg[],
-		int prompt_skip)
+		int prompt_skip, int lazy)
 {
 	/* TODO: refactor this function redraw_error_msg() */
 
@@ -398,6 +398,15 @@ redraw_error_msg(const char title_arg[], const char message_arg[],
 	ctrl_msg = get_control_msg(msg_kind, ctrl_c);
 	draw_msg(title, message, ctrl_msg, centered ? S_CENTERED : S_USUAL);
 	wrefresh(error_win);
+
+	if(lazy)
+	{
+		wnoutrefresh(error_win);
+	}
+	else
+	{
+		wrefresh(error_win);
+	}
 }
 
 /* Picks control message (information on available actions) basing on dialog

src/event_loop.c
@@ -327,7 +327,7 @@ process_scheduled_updates(void)
 
 		if(changed && vle_mode_is(MSG_MODE))
 		{
-			redraw_msg_dialog();
+			redraw_msg_dialog(0);
 		}
 	}
 }

src/modes/dialogs/msg_dialog.h
@@ -36,7 +36,7 @@ response_variant;
 void init_msg_dialog_mode(void);
 
 /* Redraws currently visible error message on the screen. */
-void redraw_msg_dialog(void);
+void redraw_msg_dialog(int lazy);
 
 /* Shows error message to a user. */
 void show_error_msg(const char title[], const char message[]);

src/modes/modes.c
@@ -230,7 +230,7 @@ modes_redraw(void)
 		menu_redraw();
 		if(vle_mode_is(MSG_MODE))
 		{
-			redraw_msg_dialog();
+			redraw_msg_dialog(0);
 		}
 		goto finish;
 	}
@@ -265,7 +265,7 @@ modes_redraw(void)
 	}
 	else if(vle_mode_is(MSG_MODE))
 	{
-		redraw_msg_dialog();
+		redraw_msg_dialog(0);
 	}
 
 finish:

src/ui/ui.c
@@ -715,7 +715,9 @@ touch_all_windows(void)
 
 	if(vle_mode_is(MSG_MODE))
 	{
-		update_window_lazy(error_win);
+		/* Location of message dialog can change, so it's better to let it position
+		 * itself or multiple copies appear on the screen. */
+		redraw_msg_dialog(1);
 	}
 }
 

commit 75619975a09d52e807cdf09c84e45d91734585cb

Fix dialog box redraw in menu after term resize
The dialog disappeared, but everything else was good.
Howo to reproduce:
1. :marks.
2. Remove all marks until dialog appears.
3. Resize terminal.

Yes, that's unused parameter.

Broken by 1f52c98.

src/modes/dialogs/msg_dialog.c
@@ -214,7 +214,7 @@ leave(Result r)
 void
 redraw_msg_dialog(int lazy)
 {
-	redraw_error_msg(NULL, NULL, 0, 1);
+	redraw_error_msg(NULL, NULL, 0, lazy);
 }
 
 void