b71d37db9455f7fd5063b5408d3a588f69ea3489
xaizek <xaizek@openmailbox.org>
2015-10-05 16:06:05 +0200

    Add basic tests for fswatch
    These tests should work even without inotify.

---
+++ tests/utils/fswatch.c
@@ -0,0 +1,78 @@
+#include <stic.h>
+
+#include <stdlib.h> /* remove() */
+
+#include "../../src/compat/os.h"
+#include "../../src/utils/fswatch.h"
+
+TEST(watch_is_created_for_existing_directory)
+{
+	fswatch_t *watch;
+
+	assert_non_null(watch = fswatch_create(SANDBOX_PATH));
+	fswatch_free(watch);
+}
+
+TEST(two_watches_for_the_same_directory)
+{
+	fswatch_t *watch1, *watch2;
+
+	assert_non_null(watch1 = fswatch_create(SANDBOX_PATH));
+	assert_non_null(watch2 = fswatch_create(SANDBOX_PATH));
+	fswatch_free(watch2);
+	fswatch_free(watch1);
+}
+
+TEST(events_are_accumulated)
+{
+	fswatch_t *watch;
+	int error;
+
+	assert_non_null(watch = fswatch_create(SANDBOX_PATH));
+
+	os_mkdir(SANDBOX_PATH "/testdir", 0700);
+	remove(SANDBOX_PATH "/testdir");
+	assert_true(fswatch_changed(watch, &error));
+	assert_false(error);
+	assert_false(fswatch_changed(watch, &error));
+	assert_false(error);
+
+	fswatch_free(watch);
+}
+
+TEST(started_as_not_changed)
+{
+	fswatch_t *watch;
+	int error;
+
+	assert_non_null(watch = fswatch_create(SANDBOX_PATH));
+
+	assert_false(fswatch_changed(watch, &error));
+	assert_false(error);
+
+	fswatch_free(watch);
+}
+
+TEST(handles_several_events_in_a_row)
+{
+	fswatch_t *watch;
+	int error;
+
+	assert_non_null(watch = fswatch_create(SANDBOX_PATH));
+
+	assert_false(fswatch_changed(watch, &error));
+	assert_false(error);
+
+	os_mkdir(SANDBOX_PATH "/testdir", 0700);
+	assert_true(fswatch_changed(watch, &error));
+	assert_false(error);
+
+	remove(SANDBOX_PATH "/testdir");
+	assert_true(fswatch_changed(watch, &error));
+	assert_false(error);
+
+	fswatch_free(watch);
+}
+
+/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
+/* vim: set cinoptions+=t0 filetype=c : */
