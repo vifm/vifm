vifm/vifm

commit a3c87473aa561fcb061c2a818f9259e6268b968d

Select correct file in cv with dups on mark nav
Used to pick the first one.

This is actually the second part of 648773d, which fixed reloading,
which should be loading, but that reset custom view.  Now custom view is
preserved if possible and local filter is reset if it hides marked file.

Thanks to filterfalse.

src/filelist.c
@@ -96,6 +96,8 @@ static void correct_list_pos_up(FileView *view, size_t pos_delta);
 static void move_cursor_out_of_scope(FileView *view, predicate_func pred);
 static void navigate_to_history_pos(FileView *view, int pos);
 static void save_selection(FileView *view);
+static int navigate_to_file_in_custom_view(FileView *view, const char dir[],
+		const char file[]);
 static void free_saved_selection(FileView *view);
 static int fill_dir_entry_by_path(dir_entry_t *entry, const char path[]);
 #ifndef _WIN32
@@ -110,6 +112,7 @@ static int data_is_dir_entry(const WIN32_FIND_DATAW *ffd);
 static int is_in_list(FileView *view, const dir_entry_t *entry, void *arg);
 static void load_dir_list_internal(FileView *view, int reload, int draw_only);
 static int populate_dir_list_internal(FileView *view, int reload);
+static int custom_list_is_incomplete(const FileView *view);
 static int is_dead_or_filtered(FileView *view, const dir_entry_t *entry,
 		void *arg);
 static void update_entries_data(FileView *view);
@@ -872,6 +875,15 @@ navigate_back(FileView *view)
 void
 navigate_to_file(FileView *view, const char dir[], const char file[])
 {
+	if(flist_custom_active(view))
+	{
+		/* Try to find requested element in custom list of files. */
+		if(navigate_to_file_in_custom_view(view, dir, file) == 0)
+		{
+			return;
+		}
+	}
+
 	/* Do not change directory if we already there. */
 	if(!paths_are_equal(view->curr_dir, dir))
 	{
@@ -885,6 +897,47 @@ navigate_to_file(FileView *view, const char dir[], const char file[])
 	{
 		(void)ensure_file_is_selected(view, file);
 	}
+}
+
+/* navigate_to_file() helper that tries to find requested file in custom view.
+ * Returns non-zero on failure to find such file, otherwise zero is returned. */
+static int
+navigate_to_file_in_custom_view(FileView *view, const char dir[],
+		const char file[])
+{
+	char full_path[PATH_MAX];
+	dir_entry_t *entry;
+
+	snprintf(full_path, sizeof(full_path), "%s/%s", dir, file);
+
+	if(custom_list_is_incomplete(view))
+	{
+		entry = entry_from_path(view->custom.entries, view->custom.entry_count,
+				full_path);
+		if(entry == NULL)
+		{
+			/* No such entry in the view at all. */
+			return 1;
+		}
+
+		if(!local_filter_matches(view, entry))
+		{
+			/* The item is filtered-out by current settings, undo this filtering. */
+			local_filter_remove(view);
+			load_dir_list(view, 1);
+		}
+	}
+
+	entry = entry_from_path(view->dir_entry, view->list_rows, full_path);
+	if(entry == NULL)
+	{
+		/* File might not exist anymore at that location. */
+		return 1;
+	}
+
+	view->list_pos = entry_to_pos(view, entry);
+	ui_view_schedule_redraw(view);
+	return 0;
 }
 
 int
@@ -1595,8 +1648,7 @@ populate_dir_list_internal(FileView *view, int reload)
 
 	if(flist_custom_active(view))
 	{
-		if(view->custom.entry_count != 0 &&
-				view->list_rows != view->custom.entry_count)
+		if(custom_list_is_incomplete(view))
 		{
 			/* Load initial list of custom entries if it's available. */
 			replace_dir_entries(view, &view->dir_entry, &view->list_rows,
@@ -1673,6 +1725,16 @@ populate_dir_list_internal(FileView *view, int reload)
 	fview_list_updated(view);
 
 	return 0;
+}
+
+/* Checks whether currently loaded custom list of files is missing some files
+ * compared to the original custom list.  Returns non-zero if so, otherwise zero
+ * is returned. */
+static int
+custom_list_is_incomplete(const FileView *view)
+{
+	return view->custom.entry_count != 0
+	    && view->list_rows != view->custom.entry_count;
 }
 
 /* zap_entries() filter to filter-out inexistent files or files which names

commit b7a031df81b4e69266e069d8d66d805a6792899f

Fix recently broken gf in cusvom views
Was broken by a3c8747.

Thanks to filterfalse.

src/filelist.c
@@ -877,9 +877,10 @@ navigate_back(FileView *view)
 }
 
 void
-navigate_to_file(FileView *view, const char dir[], const char file[])
+navigate_to_file(FileView *view, const char dir[], const char file[],
+		int preserve_cv)
 {
-	if(flist_custom_active(view))
+	if(flist_custom_active(view) && preserve_cv)
 	{
 		/* Try to find requested element in custom list of files. */
 		if(navigate_to_file_in_custom_view(view, dir, file) == 0)
@@ -1771,8 +1772,19 @@ update_dir_mtime(FileView *view)
 static int
 custom_list_is_incomplete(const FileView *view)
 {
-	return view->custom.entry_count != 0
-	    && view->list_rows != view->custom.entry_count;
+	if(view->custom.entry_count == 0)
+	{
+		return 0;
+	}
+
+	if(view->list_rows == 1 && is_parent_dir(view->dir_entry[0].name) &&
+			!(view->custom.entry_count == 1 &&
+				is_parent_dir(view->custom.entries[0].name)))
+	{
+		return 1;
+	}
+
+	return view->list_rows != view->custom.entry_count;
 }
 
 /* zap_entries() filter to filter-out inexistent files or files which names

src/filelist.h
@@ -107,8 +107,10 @@ void navigate_to(FileView *view, const char path[]);
 void navigate_back(FileView *view);
 /* Changes current directory of the view to the dir if it's possible and in case
  * of success reloads filelist of the view and sets its cursor position on the
- * file trying to ensure that it's visible. */
-void navigate_to_file(FileView *view, const char dir[], const char file[]);
+ * file trying to ensure that it's visible.  If preserve_cv is set, tries to do
+ * not reset custom view when possible. */
+void navigate_to_file(FileView *view, const char dir[], const char file[],
+		int preserve_cv);
 /* The directory can either be relative to the current
  * directory - ../
  * or an absolute path - /usr/local/share

src/marks.c
@@ -308,7 +308,7 @@ navigate_to_mark(FileView *view, char m)
 
 	if(is_mark_valid(mark))
 	{
-		navigate_to_file(view, mark->directory, mark->file);
+		navigate_to_file(view, mark->directory, mark->file, 1);
 		return 0;
 	}
 

src/running.c
@@ -134,17 +134,18 @@ follow_file(FileView *view)
 {
 	if(flist_custom_active(view))
 	{
+		/* Entry might be freed on navigation, so make sure name and origin will
+		 * remain available for the call. */
 		const dir_entry_t *const entry = &view->dir_entry[view->list_pos];
 		char *const name = strdup(entry->name);
 		char *const origin = strdup(entry->origin);
-		navigate_to_file(view, origin, name);
+		navigate_to_file(view, origin, name, 0);
 		free(origin);
 		free(name);
+		return;
 	}
-	else
-	{
-		handle_file(view, FHE_RUN, FHL_FOLLOW);
-	}
+
+	handle_file(view, FHE_RUN, FHL_FOLLOW);
 }
 
 static void

tests/misc/chase_links.c
@@ -15,7 +15,6 @@
 #include "../../src/utils/path.h"
 #include "../../src/utils/str.h"
 
-
 static void init_view(FileView *view);
 static void free_view(FileView *view);
 static int not_windows(void);

tests/misc/navigation.c
@@ -1,0 +1,116 @@
+#include <stic.h>
+
+#include <unistd.h> /* chdir() */
+
+#include <stdlib.h> /* free() */
+#include <string.h> /* strdup() */
+
+#include "../../src/cfg/config.h"
+#include "../../src/utils/dynarray.h"
+#include "../../src/utils/str.h"
+#include "../../src/filelist.h"
+#include "../../src/filtering.h"
+#include "../../src/marks.h"
+#include "../../src/running.h"
+
+static void init_view(FileView *view);
+static void free_view(FileView *view);
+
+SETUP()
+{
+	assert_success(chdir(SANDBOX_PATH));
+
+	curr_view = &lwin;
+	other_view = &rwin;
+
+	cfg.fuse_home = strdup("no");
+	cfg.slow_fs_list = strdup("");
+	cfg.chase_links = 1;
+
+	init_view(&lwin);
+	init_view(&rwin);
+}
+
+TEARDOWN()
+{
+	update_string(&cfg.slow_fs_list, NULL);
+	update_string(&cfg.fuse_home, NULL);
+
+	cfg.chase_links = 0;
+
+	free_view(&lwin);
+	free_view(&rwin);
+}
+
+static void
+init_view(FileView *view)
+{
+	filter_init(&view->local_filter.filter, 1);
+	filter_init(&view->manual_filter, 1);
+	filter_init(&view->auto_filter, 1);
+
+	view->dir_entry = NULL;
+	view->list_rows = 0;
+
+	view->window_rows = 1;
+	view->sort[0] = SK_NONE;
+	ui_view_sort_list_ensure_well_formed(view, view->sort);
+}
+
+static void
+free_view(FileView *view)
+{
+	int i;
+
+	for(i = 0; i < view->list_rows; ++i)
+	{
+		free(view->dir_entry[i].name);
+	}
+	dynarray_free(view->dir_entry);
+}
+
+TEST(custom_view_is_preserved_on_goto_mark)
+{
+	flist_custom_start(&lwin, "test");
+	flist_custom_add(&lwin, TEST_DATA_PATH "/existing-files/a");
+	assert_true(flist_custom_finish(&lwin, 0) == 0);
+
+	curr_view = &lwin;
+	assert_success(set_user_mark('a', TEST_DATA_PATH "/existing-files", "a"));
+
+	assert_success(goto_mark(&lwin, 'a'));
+	assert_true(flist_custom_active(&lwin));
+}
+
+TEST(local_filter_is_reset_in_cv_to_follow_mark)
+{
+	flist_custom_start(&lwin, "test");
+	flist_custom_add(&lwin, TEST_DATA_PATH "/existing-files/a");
+	assert_true(flist_custom_finish(&lwin, 0) == 0);
+
+	curr_view = &lwin;
+	assert_success(set_user_mark('a', TEST_DATA_PATH "/existing-files", "a"));
+	assert_true(flist_custom_active(&lwin));
+
+	local_filter_set(&lwin, "b");
+	local_filter_accept(&lwin);
+	assert_true(flist_custom_active(&lwin));
+
+	assert_success(goto_mark(&lwin, 'a'));
+
+	assert_true(flist_custom_active(&lwin));
+	assert_true(filter_is_empty(&lwin.local_filter.filter));
+}
+
+TEST(following_resets_cv)
+{
+	flist_custom_start(&lwin, "test");
+	flist_custom_add(&lwin, TEST_DATA_PATH "/existing-files/a");
+	assert_true(flist_custom_finish(&lwin, 0) == 0);
+
+	follow_file(&lwin);
+	assert_false(flist_custom_active(&lwin));
+}
+
+/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
+/* vim: set cinoptions+=t0 filetype=c : */