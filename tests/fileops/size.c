4dda20884062301c64f578a632bb4480fa3311ee
xaizek <xaizek@openmailbox.org>
2016-04-20 16:06:04 +0300

    Decompose fileops/size.c test into functions

--- tests/fileops/size.c
+++ tests/fileops/size.c
@@ -10,42 +10,70 @@
 #include "../../src/fileops.h"
 #include "../../src/status.h"
 
-TEST(directory_size_is_calculated_in_bg)
+static void setup_single_entry(FileView *view, const char name[]);
+static uint64_t wait_for_size(const char path[]);
+
+SETUP()
+{
+	init_status(&cfg);
+}
+
+TEARDOWN()
 {
 	int i;
-	uint64_t size, nitems;
 
-	strcpy(lwin.curr_dir, TEST_DATA_PATH);
-
-	init_status(&cfg);
-
-	lwin.list_rows = 1;
-	lwin.list_pos = 0;
-	lwin.dir_entry = dynarray_cextend(NULL,
-			lwin.list_rows*sizeof(*lwin.dir_entry));
-	lwin.dir_entry[0].name = strdup("various-sizes");
-	lwin.dir_entry[0].origin = &lwin.curr_dir[0];
-	lwin.dir_entry[0].type = FT_DIR;
-	lwin.dir_entry[0].selected = 1;
-
-	calculate_size_bg(&lwin, 0);
-
-	dcache_get_at(TEST_DATA_PATH "/various-sizes", &size, &nitems);
-	while(size == DCACHE_UNKNOWN)
-	{
-		usleep(200);
-		dcache_get_at(TEST_DATA_PATH "/various-sizes", &size, &nitems);
-	}
-
-	assert_int_equal(73728, size);
+	reset_status(&cfg);
 
 	for(i = 0; i < lwin.list_rows; ++i)
 	{
 		free_dir_entry(&lwin, &lwin.dir_entry[i]);
 	}
 	dynarray_free(lwin.dir_entry);
+}
 
-	reset_status(&cfg);
+TEST(directory_size_is_calculated_in_bg)
+{
+	strcpy(lwin.curr_dir, TEST_DATA_PATH);
+	setup_single_entry(&lwin, "various-sizes");
+	lwin.dir_entry[0].selected = 1;
+
+	calculate_size_bg(&lwin, 0);
+	assert_int_equal(73728, wait_for_size(TEST_DATA_PATH "/various-sizes"));
+}
+
+static void
+setup_single_entry(FileView *view, const char name[])
+{
+	view->user_selection = 1;
+	view->list_rows = 1;
+	view->list_pos = 0;
+	view->dir_entry = dynarray_cextend(NULL,
+			view->list_rows*sizeof(*view->dir_entry));
+	view->dir_entry[0].name = strdup(name);
+	view->dir_entry[0].origin = &view->curr_dir[0];
+	view->dir_entry[0].type = FT_DIR;
+}
+
+static uint64_t
+wait_for_size(const char path[])
+{
+	uint64_t size, nitems;
+	int counter;
+
+	dcache_get_at(path, &size, &nitems);
+	counter = 0;
+	while(size == DCACHE_UNKNOWN)
+	{
+		usleep(200);
+		dcache_get_at(path, &size, &nitems);
+		if(++counter > 100)
+		{
+			assert_fail("Waiting for too long.");
+			break;
+		}
+	}
+
+	return size;
 }
 
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
