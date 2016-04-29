vifm/vifm

commit d82295183011d279a69454c723f1f494ca2eee54

Add 'wildstyle' option to control wild menu style
Enables displaying it as a menu with one item per line.

See #166 on GitHub and
http://q2a.vifm.info/101/command-autocompletion-option-suggestions-descriptions?show=101#q101.

Thanks to santhoshr and octos.

src/modes/cmdline.c
@@ -141,6 +141,8 @@ static void cmd_ctrl_i(key_info_t key_info, keys_info_t *keys_info);
 static void cmd_shift_tab(key_info_t key_info, keys_info_t *keys_info);
 static void do_completion(void);
 static void draw_wild_menu(int op);
+static int draw_wild_bar(int *last_pos, int *pos, int *len);
+static int draw_wild_popup(int *last_pos, int *pos, int *len);
 static void cmd_ctrl_k(key_info_t key_info, keys_info_t *keys_info);
 static void cmd_ctrl_m(key_info_t key_info, keys_info_t *keys_info);
 static int is_input_line_empty(void);
@@ -1016,9 +1018,8 @@ draw_wild_menu(int op)
 {
 	static int last_pos;
 
-	const char ** list = vle_compl_get_list();
 	int pos = vle_compl_get_pos();
-	int count = vle_compl_get_count() - 1;
+	const int count = vle_compl_get_count() - 1;
 	int i;
 	int len = getmaxx(stdscr);
 
@@ -1039,59 +1040,13 @@ draw_wild_menu(int op)
 		last_pos = 0;
 	if(last_pos == 0 && pos == count - 1)
 		last_pos = count;
-	if(pos < last_pos)
-	{
-		int l = len;
-		while(last_pos > 0 && l > 2)
-		{
-			last_pos--;
-			l -= strlen(list[last_pos]);
-			if(last_pos != 0)
-				l -= 2;
-		}
-		if(l < 2)
-			last_pos++;
-	}
 
 	werase(stat_win);
-	checked_wmove(stat_win, 0, 0);
 
-	for(i = last_pos; i < count && len > 0; i++)
-	{
-		len -= strlen(list[i]);
-		if(i != 0)
-			len -= 2;
+	i = cfg.wild_popup
+	  ? draw_wild_popup(&last_pos, &pos, &len)
+	  : draw_wild_bar(&last_pos, &pos, &len);
 
-		if(i == last_pos && last_pos > 0)
-		{
-			wprintw(stat_win, "< ");
-		}
-		else if(i > last_pos)
-		{
-			if(len < 2)
-			{
-				wprintw(stat_win, " >");
-				break;
-			}
-			wprintw(stat_win, "  ");
-		}
-
-		if(i == pos)
-		{
-			col_attr_t col;
-			col = cfg.cs.color[STATUS_LINE_COLOR];
-			mix_colors(&col, &cfg.cs.color[WILD_MENU_COLOR]);
-
-			wbkgdset(stat_win, COLOR_PAIR(colmgr_get_pair(col.fg, col.bg)) | col.attr);
-		}
-		wprint(stat_win, list[i]);
-		if(i == pos)
-		{
-			wbkgdset(stat_win, COLOR_PAIR(cfg.cs.pair[STATUS_LINE_COLOR]) |
-					cfg.cs.color[STATUS_LINE_COLOR].attr);
-			pos = -pos;
-		}
-	}
 	if(pos > 0 && pos != count)
 	{
 		last_pos = pos;
@@ -1103,6 +1058,118 @@ draw_wild_menu(int op)
 	wrefresh(stat_win);
 
 	update_cursor();
+}
+
+/* Draws wild menu as a bar.  Returns index of the last displayed item. */
+static int
+draw_wild_bar(int *last_pos, int *pos, int *len)
+{
+	int i;
+
+	const char **const list = vle_compl_get_list();
+	const int count = vle_compl_get_count() - 1;
+
+	wresize(stat_win, 1, 0);
+	checked_wmove(stat_win, 0, 0);
+
+	if(*pos < *last_pos)
+	{
+		int l = *len;
+		while(*last_pos > 0 && l > 2)
+		{
+			--*last_pos;
+			l -= strlen(list[*last_pos]);
+			if(*last_pos != 0)
+				l -= 2;
+		}
+		if(l < 2)
+			++*last_pos;
+	}
+
+	for(i = *last_pos; i < count && *len > 0; ++i)
+	{
+		*len -= strlen(list[i]);
+		if(i != 0)
+			*len -= 2;
+
+		if(i == *last_pos && *last_pos > 0)
+		{
+			wprintw(stat_win, "< ");
+		}
+		else if(i > *last_pos)
+		{
+			if(*len < 2)
+			{
+				wprintw(stat_win, " >");
+				break;
+			}
+			wprintw(stat_win, "  ");
+		}
+
+		if(i == *pos)
+		{
+			col_attr_t col = cfg.cs.color[STATUS_LINE_COLOR];
+			mix_colors(&col, &cfg.cs.color[WILD_MENU_COLOR]);
+
+			wbkgdset(stat_win,
+					COLOR_PAIR(colmgr_get_pair(col.fg, col.bg)) | col.attr);
+		}
+		wprint(stat_win, list[i]);
+		if(i == *pos)
+		{
+			wbkgdset(stat_win, COLOR_PAIR(cfg.cs.pair[STATUS_LINE_COLOR]) |
+					cfg.cs.color[STATUS_LINE_COLOR].attr);
+			*pos = -*pos;
+		}
+	}
+
+	return i;
+}
+
+/* Draws wild menu as a popup.  Returns index of the last displayed item. */
+static int
+draw_wild_popup(int *last_pos, int *pos, int *len)
+{
+	int i;
+	int j;
+
+	const char **const list = vle_compl_get_list();
+	const int count = vle_compl_get_count() - 1;
+	const int max_height = getmaxy(stdscr) - get_required_height() -
+		ui_stat_job_bar_height();
+	const int height = MIN(count, MIN(10, max_height));
+
+	if(*pos < *last_pos)
+	{
+		*last_pos = MAX(0, *last_pos - height);
+	}
+
+	wresize(stat_win, height, getmaxx(stdscr));
+	ui_stat_reposition(get_required_height());
+
+	for(i = *last_pos, j = 0; i < count && j < height; ++i, ++j)
+	{
+		checked_wmove(stat_win, j, 0);
+
+		if(i == *pos)
+		{
+			col_attr_t col = cfg.cs.color[STATUS_LINE_COLOR];
+			mix_colors(&col, &cfg.cs.color[WILD_MENU_COLOR]);
+
+			wbkgdset(stat_win,
+					COLOR_PAIR(colmgr_get_pair(col.fg, col.bg)) | col.attr);
+		}
+		wprint(stat_win, list[i]);
+		wclrtoeol(stat_win);
+		if(i == *pos)
+		{
+			wbkgdset(stat_win, COLOR_PAIR(cfg.cs.pair[STATUS_LINE_COLOR]) |
+					cfg.cs.color[STATUS_LINE_COLOR].attr);
+			*pos = -*pos;
+		}
+	}
+
+	return i;
 }
 
 static void
@@ -2478,6 +2545,12 @@ stop_history_completion(void)
 	{
 		if(cfg.display_statusline)
 		{
+			if(cfg.wild_popup)
+			{
+				/* Multi-line popup could mess these up. */
+				ui_view_schedule_redraw(curr_view);
+				ui_view_schedule_redraw(other_view);
+			}
 			update_stat_window(curr_view);
 		}
 		else

data/man/vifm.1
@@ -1,4 +1,4 @@
-.TH VIFM 1 "February 10, 2016" "vifm 0.8.1a"
+.TH VIFM 1 "February 26, 2016" "vifm 0.8.1a"
 .\" ---------------------------------------------------------------------------
 .SH NAME
 .\" ---------------------------------------------------------------------------
@@ -3951,6 +3951,15 @@ default: false
 .br
 Controls whether possible matches of completion will be shown above the command
 line.
+.TP
+.BI 'wildstyle'
+type: enumeration
+.br
+default: bar
+.br
+Picks presentation style of wild menu.  Possible values:
+ - bar   - one-line with left-to-right cursor
+ - popup - multi-line with top-to-bottom cursor
 .TP
 .BI 'wordchars'
 type: string list

data/vim/syntax/vifm.vim
@@ -1,6 +1,6 @@
 " vifm syntax file
 " Maintainer:  xaizek <xaizek@openmailbox.org>
-" Last Change: January 04, 2016
+" Last Change: February 26, 2016
 " Based On:    Vim syntax file by Dr. Charles E. Campbell, Jr.
 
 if exists('b:current_syntax')
@@ -123,7 +123,7 @@ syntax keyword vifmOption contained aproposprg autochpos cdpath cd chaselinks
 		\ sort sortgroups sortorder sortnumbers shell sh shortmess shm slowfs
 		\ smartcase scs statusline stl syscalls tabstop timefmt timeoutlen title tm
 		\ trash trashdir ts tuioptions to undolevels ul vicmd viewcolumns vifminfo
-		\ vimhelp vixcmd wildmenu wmnu wordchars wrap wrapscan ws
+		\ vimhelp vixcmd wildmenu wmnu wildstyle wordchars wrap wrapscan ws
 
 " Disabled boolean options
 syntax keyword vifmOption contained noautochpos noconfirm nocf nochaselinks

src/cfg/config.c
@@ -137,6 +137,7 @@ cfg_init(void)
 	cfg.use_term_multiplexer = 0;
 	cfg.use_vim_help = 0;
 	cfg.wild_menu = 0;
+	cfg.wild_popup = 0;
 	cfg.ignore_case = 0;
 	cfg.smart_case = 0;
 	cfg.hl_search = 1;

src/cfg/config.h
@@ -109,7 +109,11 @@ typedef struct config_t
 	int follow_links; /* Follow links on l or Enter. */
 	int confirm; /* Ask user about permanent deletion of files. */
 	int fast_run;
+
+	/* Whether wild menu should be used. */
 	int wild_menu;
+	/* Whether wild menu should be a popup instead of a bar. */
+	int wild_popup;
 	int ignore_case;
 	int smart_case;
 	int hl_search;

src/cfg/info.c
@@ -1046,6 +1046,7 @@ write_options(FILE *const fp)
 
 	fprintf(fp, "=%svimhelp\n", cfg.use_vim_help ? "" : "no");
 	fprintf(fp, "=%swildmenu\n", cfg.wild_menu ? "" : "no");
+	fprintf(fp, "=wildstyle=%s\n", cfg.wild_popup ? "popup" : "bar");
 	fprintf(fp, "=wordchars=%s\n",
 			escape_spaces(get_option_value("wordchars", OPT_GLOBAL)));
 	fprintf(fp, "=%swrap\n", cfg.wrap_quick_view ? "" : "no");

src/opt_handlers.c
@@ -187,6 +187,7 @@ static void vixcmd_handler(OPT_OP op, optval_t val);
 static void vifminfo_handler(OPT_OP op, optval_t val);
 static void vimhelp_handler(OPT_OP op, optval_t val);
 static void wildmenu_handler(OPT_OP op, optval_t val);
+static void wildstyle_handler(OPT_OP op, optval_t val);
 static void wordchars_handler(OPT_OP op, optval_t val);
 static void wrap_handler(OPT_OP op, optval_t val);
 static void text_option_changed(void);
@@ -308,6 +309,13 @@ static const char *vifminfo_set[] = {
 	"phistory",
 	"fhistory",
 };
+
+/* Possible values of 'wildstyle'. */
+static const char *wildstyle_vals[] = {
+	"bar",
+	"popup",
+};
+ARRAY_GUARD(wildstyle_vals, 2);
 
 /* Empty value to satisfy default initializer. */
 static char empty_val[] = "";
@@ -538,6 +546,11 @@ options[] = {
 	{ "wildmenu", "wmnu",
 	  OPT_BOOL, 0, NULL, &wildmenu_handler, NULL,
 	  { .ref.bool_val = &cfg.wild_menu },
+	},
+	{ "wildstyle", "",
+	  OPT_ENUM, ARRAY_LEN(wildstyle_vals), wildstyle_vals, &wildstyle_handler,
+		NULL,
+	  { .ref.enum_item = &cfg.wild_popup },
 	},
 	{ "wordchars", "",
 	  OPT_STRLIST, 0, NULL, &wordchars_handler, NULL,
@@ -2288,6 +2301,14 @@ static void
 wildmenu_handler(OPT_OP op, optval_t val)
 {
 	cfg.wild_menu = val.bool_val;
+}
+
+/* Wild menu style, which defines how it's presented. */
+static void
+wildstyle_handler(OPT_OP op, optval_t val)
+{
+	/* There are only two possible values. */
+	cfg.wild_popup = (val.enum_item == 1);
 }
 
 /* Handles setting value of 'wordchars' by parsing list of ranges into character

src/tags.c
@@ -114,6 +114,7 @@ const char *tags[] = {
 	"vifm-'vimhelp'",
 	"vifm-'vixcmd'",
 	"vifm-'wildmenu'",
+	"vifm-'wildstyle'",
 	"vifm-'wmnu'",
 	"vifm-'wordchars'",
 	"vifm-'wrap'",

commit f9a2f9df46e044d3b992839fc31102cc2b25690a

Fix drawing wildmenu bar via pdcurses (Windows)
Got broken by d82295183011d279a69454c723f1f494ca2eee54.

Not sure why there is a resize to 1x0, pdcurses probably doesn't ignore
it, while other implementations do.

src/modes/cmdline.c
@@ -1039,7 +1039,6 @@ draw_wild_bar(int *last_pos, int *pos, int *len)
 	const vle_compl_t *const items = vle_compl_get_items();
 	const int count = vle_compl_get_count() - 1;
 
-	wresize(stat_win, 1, 0);
 	checked_wmove(stat_win, 0, 0);
 
 	if(*pos < *last_pos)