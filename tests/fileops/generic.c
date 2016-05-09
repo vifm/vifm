e768cd1148d39acd681600a7ee5b71d9513f533e
xaizek <xaizek@openmailbox.org>
2016-05-09 21:58:56 +0300

    Check for st_[am]tim in fileops/generic tests

--- tests/fileops/generic.c
+++ tests/fileops/generic.c
@@ -230,6 +230,10 @@ perform_merge(int op)
 #ifndef _WIN32
 	{
 		assert_success(os_stat("second/nested1", &dst));
+#ifndef HAVE_STRUCT_STAT_ST_MTIM
+#define st_atim st_atime
+#define st_mtim st_mtime
+#endif
 		assert_success(memcmp(&src.st_atim, &dst.st_atim, sizeof(src.st_atim)));
 		assert_success(memcmp(&src.st_mtim, &dst.st_mtim, sizeof(src.st_mtim)));
 		assert_success(memcmp(&src.st_mode, &dst.st_mode, sizeof(src.st_mode)));
