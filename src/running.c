619467a6b8e38aa826dc8d4e981582692f86003e
xaizek <xaizek@openmailbox.org>
2016-07-22 13:39:33 +0300

    Try to preserve term state while running :commands
    Thanks to Losiara.
    
    See #192 on GitHub.

--- src/running.c
+++ src/running.c
@@ -870,6 +870,8 @@ setup_shellout_env(void)
 	char *escaped_path;
 	char *cmd;
 
+	def_prog_mode();
+
 	/* Need to use internal value instead of getcwd() for a symlink directory. */
 	env_set("PWD", curr_view->curr_dir);
 
@@ -905,6 +907,8 @@ cleanup_shellout_env(void)
 {
 	const char *term_multiplexer_fmt;
 	char *cmd;
+
+	reset_prog_mode();
 
 	switch(curr_stats.term_multiplexer)
 	{
