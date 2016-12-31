b670d4ac18bbd95c88124e22a9ab69d8cb4ec950
xaizek <xaizek@openmailbox.org>
2016-12-31 19:09:07 +0200

    Fix parsing of 'classify' with entry w/o prefix
    For example:
        set classify=:dir:/,:link:@,:fifo:|
    Processing stops after ":dir:/".
    
    Got broken by ae9f51428c1b86b31afa224e22a6df48c1d2b74d.
    
    Thanks to filterfalse.

--- tests/utils/split_and_get_dc.c
+++ tests/utils/split_and_get_dc.c
@@ -12,5 +12,15 @@ TEST(empty_string_null_returned)
 	assert_string_equal(NULL, part = split_and_get_dc(part, &state));
 }
 
+TEST(several_elements_are_extracted)
+{
+	char input[] = ":dir:/,:link:@";
+	char *part = input, *state = NULL;
+
+	assert_string_equal(":dir:/", part = split_and_get_dc(part, &state));
+	assert_string_equal(":link:@", part = split_and_get_dc(part, &state));
+	assert_string_equal(NULL, part = split_and_get_dc(part, &state));
+}
+
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
 /* vim: set cinoptions+=t0 filetype=c : */

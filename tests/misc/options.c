vifm/vifm

commit ae9f51428c1b86b31afa224e22a6df48c1d2b74d

Fix setting 'classify' to empty value
Hanged in infinite loop inside utils/str.c:split_and_get_dc().

src/utils/str.c
@@ -904,6 +904,11 @@ split_and_get(char str[], char sep, char **state)
 char *
 split_and_get_dc(char str[], char **state)
 {
+	if(str[0] == '\0')
+	{
+		return NULL;
+	}
+
 	if(*state != NULL)
 	{
 		if(**state == '\0')
@@ -916,9 +921,9 @@ split_and_get_dc(char str[], char **state)
 
 	while(str != NULL)
 	{
-		char *ptr;
+		char *ptr = strchr(str, ',');
 
-		if((ptr = strchr(str, ',')) != NULL)
+		if(ptr != NULL)
 		{
 			while(ptr != NULL && ptr[1] == ',')
 			{

tests/misc/options.c
@@ -226,6 +226,23 @@ TEST(classify_pattern_list)
 	assert_string_equal(">", suffix);
 }
 
+TEST(classify_can_be_set_to_empty_value)
+{
+	dir_entry_t entry = {
+		.name = "binary-data",
+		.origin = TEST_DATA_PATH "/test-data/read",
+		.name_dec_num = -1,
+	};
+
+	const char *prefix, *suffix;
+
+	assert_success(exec_commands("set classify=", &lwin, CIT_COMMAND));
+
+	ui_get_decors(&entry, &prefix, &suffix);
+	assert_string_equal("", prefix);
+	assert_string_equal("", suffix);
+}
+
 TEST(suggestoptions_all_values)
 {
 	cfg.sug.flags = 0;

tests/utils/split_and_get_dc.c
@@ -1,0 +1,16 @@
+#include <stic.h>
+
+#include <stddef.h> /* NULL */
+
+#include "../../src/utils/str.h"
+
+TEST(empty_string_null_returned)
+{
+	char input[] = "";
+	char *part = input, *state = NULL;
+
+	assert_string_equal(NULL, part = split_and_get_dc(part, &state));
+}
+
+/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
+/* vim: set cinoptions+=t0 filetype=c : */

commit b670d4ac18bbd95c88124e22a9ab69d8cb4ec950

Fix parsing of 'classify' with entry w/o prefix
For example:
    set classify=:dir:/,:link:@,:fifo:|
Processing stops after ":dir:/".

Got broken by ae9f51428c1b86b31afa224e22a6df48c1d2b74d.

Thanks to filterfalse.

src/utils/str.c
@@ -911,18 +911,23 @@ split_and_get(char str[], char sep, char **state)
 char *
 split_and_get_dc(char str[], char **state)
 {
-	if(str[0] == '\0')
+	if(*state == NULL)
 	{
-		return NULL;
+		/* Do nothing on empty input. */
+		if(str[0] == '\0')
+		{
+			return NULL;
+		}
 	}
-
-	if(*state != NULL)
+	else
 	{
+		/* Check if we reached end of input. */
 		if(**state == '\0')
 		{
 			return NULL;
 		}
 
+		/* Process next item of the list. */
 		str = *state;
 	}
 

tests/misc/options.c
@@ -280,6 +280,35 @@ TEST(classify_state_is_not_changed_if_format_is_wong)
 	assert_int_equal(1, cfg.name_dec_count);
 }
 
+TEST(classify_does_not_stop_on_empty_prefix)
+{
+	dir_entry_t entry = {
+		.name = "read",
+		.origin = TEST_DATA_PATH,
+		.name_dec_num = -1,
+	};
+
+	const char *prefix, *suffix;
+
+	assert_success(exec_commands("set classify=:dir:/,:link:@,:fifo:|", &lwin,
+				CIT_COMMAND));
+
+	entry.type = FT_DIR;
+	ui_get_decors(&entry, &prefix, &suffix);
+	assert_string_equal("", prefix);
+	assert_string_equal("/", suffix);
+
+	entry.type = FT_LINK;
+	ui_get_decors(&entry, &prefix, &suffix);
+	assert_string_equal("", prefix);
+	assert_string_equal("@", suffix);
+
+	entry.type = FT_FIFO;
+	ui_get_decors(&entry, &prefix, &suffix);
+	assert_string_equal("", prefix);
+	assert_string_equal("|", suffix);
+}
+
 TEST(suggestoptions_all_values)
 {
 	cfg.sug.flags = 0;

tests/utils/split_and_get_dc.c
@@ -12,5 +12,15 @@ TEST(empty_string_null_returned)
 	assert_string_equal(NULL, part = split_and_get_dc(part, &state));
 }
 
+TEST(several_elements_are_extracted)
+{
+	char input[] = ":dir:/,:link:@";
+	char *part = input, *state = NULL;
+
+	assert_string_equal(":dir:/", part = split_and_get_dc(part, &state));
+	assert_string_equal(":link:@", part = split_and_get_dc(part, &state));
+	assert_string_equal(NULL, part = split_and_get_dc(part, &state));
+}
+
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
 /* vim: set cinoptions+=t0 filetype=c : */