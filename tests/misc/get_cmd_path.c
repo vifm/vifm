25caeb2162dc90f1fd88877226f87775a2bf69ce
xaizek <xaizek@openmailbox.org>
2017-01-09 22:09:18 +0200

    Fix ~ expansion when checking command existence
    Thanks to sudo-nice.
    
    Fixes #250 on GitHub.

---
+++ tests/misc/get_cmd_path.c
@@ -0,0 +1,24 @@
+#include <stic.h>
+
+#include "../../src/cfg/config.h"
+#include "../../src/compat/fs_limits.h"
+#include "../../src/utils/str.h"
+#include "../../src/cmd_completion.h"
+
+TEST(emarks_are_skipped)
+{
+	char path[PATH_MAX];
+	assert_success(get_cmd_path("!!/prog", sizeof(path), path));
+	assert_string_equal("/prog", path);
+}
+
+TEST(tilde_is_expanded)
+{
+	char path[PATH_MAX];
+	copy_str(cfg.home_dir, sizeof(cfg.home_dir), "tilde_is_expanded/");
+	assert_success(get_cmd_path("~/prog", sizeof(path), path));
+	assert_string_equal("tilde_is_expanded/prog", path);
+}
+
+/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
+/* vim: set cinoptions+=t0 filetype=c : */
