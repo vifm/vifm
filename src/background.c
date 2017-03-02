vifm/vifm

commit f5d3058f7f00f70547f5700a11d857c48eec68ff

Abstract cancellation for bg*_and_wait_for_errors

src/background.c
@@ -42,7 +42,6 @@
 #include "cfg/config.h"
 #include "compat/pthread.h"
 #include "modes/dialogs/msg_dialog.h"
-#include "ui/cancellation.h"
 #include "ui/statusline.h"
 #include "utils/cancellation.h"
 #include "utils/env.h"
@@ -388,7 +387,8 @@ error_msg(const char *title, const char *text)
 #endif
 
 int
-background_and_wait_for_errors(char cmd[], int cancellable)
+background_and_wait_for_errors(char cmd[],
+		const struct cancellation_t *cancellation)
 {
 #ifndef _WIN32
 	pid_t pid;
@@ -422,12 +422,7 @@ background_and_wait_for_errors(char cmd[], int cancellable)
 
 		close(error_pipe[1]); /* Close write end of pipe. */
 
-		if(cancellable)
-		{
-			ui_cancellation_enable();
-		}
-
-		wait_for_data_from(pid, NULL, error_pipe[0], &ui_cancellation_info);
+		wait_for_data_from(pid, NULL, error_pipe[0], cancellation);
 
 		buf[0] = '\0';
 		while((nread = read(error_pipe[0], linebuf, sizeof(linebuf) - 1)) > 0)
@@ -441,14 +436,9 @@ background_and_wait_for_errors(char cmd[], int cancellable)
 				strncat(buf, linebuf, sizeof(buf) - strlen(buf) - 1);
 			}
 
-			wait_for_data_from(pid, NULL, error_pipe[0], &ui_cancellation_info);
+			wait_for_data_from(pid, NULL, error_pipe[0], cancellation);
 		}
 		close(error_pipe[0]);
-
-		if(cancellable)
-		{
-			ui_cancellation_disable();
-		}
 
 		if(result != 0)
 		{

tests/misc/background.c
@@ -1,6 +1,7 @@
 #include <stic.h>
 
 #include "../../src/cfg/config.h"
+#include "../../src/utils/cancellation.h"
 #include "../../src/utils/str.h"
 #include "../../src/background.h"
 
@@ -9,7 +10,7 @@
 TEST(background_redirects_streams_properly, IF(not_windows))
 {
 	update_string(&cfg.shell, "/bin/sh");
-	assert_int_equal(0, background_and_wait_for_errors("echo a", 0));
+	assert_success(background_and_wait_for_errors("echo a", &no_cancellation));
 	update_string(&cfg.shell, NULL);
 }
 

src/background.h
@@ -107,7 +107,8 @@ int background_and_wait_for_status(char cmd[],
 /* Runs command in background and displays its errors to a user.  To determine
  * an error uses both stderr stream and exit status.  Returns zero on success,
  * otherwise non-zero is returned. */
-int background_and_wait_for_errors(char cmd[], int cancellable);
+int background_and_wait_for_errors(char cmd[],
+		const struct cancellation_t *cancellation);
 
 /* Runs command in a background and redirects its stdout and stderr streams to
  * file streams which are set.  Returns id of background process ((pid_t)0 for

src/ops.c
@@ -43,6 +43,7 @@
 #include "io/ior.h"
 #include "modes/dialogs/msg_dialog.h"
 #include "ui/cancellation.h"
+#include "utils/cancellation.h"
 #include "utils/fs.h"
 #include "utils/log.h"
 #include "utils/macros.h"
@@ -125,6 +126,7 @@ static IoErrCbResult dispatch_error(io_args_t *args, const ioe_err_t *err);
 static char prompt_user(const io_args_t *args, const char title[],
 		const char msg[], const response_variant variants[]);
 static int ui_cancellation_hook(void *arg);
+static int run_operation_command(ops_t *ops, char cmd[], int cancellable);
 
 /* List of functions that implement operations. */
 static op_func op_funcs[] = {
@@ -316,7 +318,7 @@ op_removesl(ops_t *ops, void *data, const char *src, const char *dst)
 		free(escaped);
 
 		LOG_INFO_MSG("Running trash command: \"%s\"", cmd);
-		return background_and_wait_for_errors(cmd, cancellable);
+		return run_operation_command(ops, cmd, cancellable);
 #else
 		char cmd[PATH_MAX*2 + 1];
 		snprintf(cmd, sizeof(cmd), "%s \"%s\"", delete_prg, src);
@@ -340,7 +342,7 @@ op_removesl(ops_t *ops, void *data, const char *src, const char *dst)
 
 		snprintf(cmd, sizeof(cmd), "rm -rf %s", escaped);
 		LOG_INFO_MSG("Running rm command: \"%s\"", cmd);
-		result = background_and_wait_for_errors(cmd, cancellable);
+		result = run_operation_command(ops, cmd, cancellable);
 
 		free(escaped);
 		return result;
@@ -455,7 +457,7 @@ op_cp(ops_t *ops, void *data, const char src[], const char dst[],
 				fast_file_cloning ? REFLINK_AUTO : "",
 				escaped_src, escaped_dst);
 		LOG_INFO_MSG("Running cp command: \"%s\"", cmd);
-		result = background_and_wait_for_errors(cmd, cancellable);
+		result = run_operation_command(ops, cmd, cancellable);
 
 		free(escaped_dst);
 		free(escaped_src);
@@ -563,7 +565,7 @@ op_mv(ops_t *ops, void *data, const char src[], const char dst[],
 		free(escaped_src);
 
 		LOG_INFO_MSG("Running mv command: \"%s\"", cmd);
-		result = background_and_wait_for_errors(cmd, cancellable);
+		result = run_operation_command(ops, cmd, cancellable);
 		if(result != 0)
 		{
 			return result;
@@ -638,7 +640,7 @@ op_chown(ops_t *ops, void *data, const char *src, const char *dst)
 	free(escaped);
 
 	LOG_INFO_MSG("Running chown command: \"%s\"", cmd);
-	return background_and_wait_for_errors(cmd, 1);
+	return run_operation_command(ops, cmd, 1);
 #else
 	return -1;
 #endif
@@ -657,7 +659,7 @@ op_chgrp(ops_t *ops, void *data, const char *src, const char *dst)
 	free(escaped);
 
 	LOG_INFO_MSG("Running chgrp command: \"%s\"", cmd);
-	return background_and_wait_for_errors(cmd, 1);
+	return run_operation_command(ops, cmd, 1);
 #else
 	return -1;
 #endif
@@ -675,7 +677,7 @@ op_chmod(ops_t *ops, void *data, const char *src, const char *dst)
 	free(escaped);
 
 	LOG_INFO_MSG("Running chmod command: \"%s\"", cmd);
-	return background_and_wait_for_errors(cmd, 1);
+	return run_operation_command(ops, cmd, 1);
 }
 
 static int
@@ -689,7 +691,7 @@ op_chmodr(ops_t *ops, void *data, const char *src, const char *dst)
 	free(escaped);
 
 	LOG_INFO_MSG("Running chmodr command: \"%s\"", cmd);
-	return background_and_wait_for_errors(cmd, 1);
+	return run_operation_command(ops, cmd, 1);
 }
 #else
 static int
@@ -761,7 +763,7 @@ op_symlink(ops_t *ops, void *data, const char *src, const char *dst)
 #ifndef _WIN32
 		snprintf(cmd, sizeof(cmd), "ln -s %s %s", escaped_src, escaped_dst);
 		LOG_INFO_MSG("Running ln command: \"%s\"", cmd);
-		result = background_and_wait_for_errors(cmd, 1);
+		result = run_operation_command(ops, cmd, 1);
 #else
 		if(get_exe_dir(exe_dir, ARRAY_LEN(exe_dir)) != 0)
 		{
@@ -802,7 +804,7 @@ op_mkdir(ops_t *ops, void *data, const char *src, const char *dst)
 				escaped);
 		free(escaped);
 		LOG_INFO_MSG("Running mkdir command: \"%s\"", cmd);
-		return background_and_wait_for_errors(cmd, 1);
+		return run_operation_command(ops, cmd, 1);
 #else
 		if(data == NULL)
 		{
@@ -858,7 +860,7 @@ op_rmdir(ops_t *ops, void *data, const char *src, const char *dst)
 		snprintf(cmd, sizeof(cmd), "rmdir %s", escaped);
 		free(escaped);
 		LOG_INFO_MSG("Running rmdir command: \"%s\"", cmd);
-		return background_and_wait_for_errors(cmd, 1);
+		return run_operation_command(ops, cmd, 1);
 #else
 		wchar_t *const utf16_path = utf8_to_utf16(src);
 		const BOOL r = RemoveDirectoryW(utf16_path);
@@ -886,7 +888,7 @@ op_mkfile(ops_t *ops, void *data, const char *src, const char *dst)
 		snprintf(cmd, sizeof(cmd), "touch %s", escaped);
 		free(escaped);
 		LOG_INFO_MSG("Running touch command: \"%s\"", cmd);
-		return background_and_wait_for_errors(cmd, 1);
+		return run_operation_command(ops, cmd, 1);
 #else
 		HANDLE hfile;
 
@@ -1114,6 +1116,27 @@ static int
 ui_cancellation_hook(void *arg)
 {
 	return ui_cancellation_requested();
+}
+
+/* Runs command in background and displays its errors to a user.  To determine
+ * an error uses both stderr stream and exit status.  Returns zero on success,
+ * otherwise non-zero is returned. */
+static int
+run_operation_command(ops_t *ops, char cmd[], int cancellable)
+{
+	int result;
+
+	if(!cancellable)
+	{
+		return background_and_wait_for_errors(cmd, &no_cancellation);
+	}
+
+	/* ui_cancellation_reset() should be called outside this unit to allow bulking
+	 * several operations together. */
+	ui_cancellation_enable();
+	result = background_and_wait_for_errors(cmd, &ui_cancellation_info);
+	ui_cancellation_disable();
+	return result;
 }
 
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */

commit 4056fe6023f9c641f56e75d242fe224fd3865241

Fix handling input for dialogs of bg processes
Was broken since f5d3058f7f00f70547f5700a11d857c48eec68ff.

E.g. error on deletion left user without a way to close the dialog,
because input wasn't processed.

src/background.c
@@ -43,6 +43,7 @@
 #include "cfg/config.h"
 #include "compat/pthread.h"
 #include "modes/dialogs/msg_dialog.h"
+#include "ui/cancellation.h"
 #include "ui/statusline.h"
 #include "utils/cancellation.h"
 #include "utils/env.h"
@@ -600,7 +601,9 @@ report_error_msg(const char title[], const char text[])
 	bg_job_t *const job = pthread_getspecific(current_job);
 	if(job == NULL)
 	{
+		const int cancellation_state = ui_cancellation_pause();
 		show_error_msg(title, text);
+		ui_cancellation_resume(cancellation_state);
 	}
 	else
 	{