d56530717df577fe176cbc12890b1677dc1b56cf
xaizek <xaizek@openmailbox.org>
2017-03-21 20:33:41 +0200

    Fix running test suites on Windows
    _wenviron gets created only if we invoke __wgetmainargs(), so do
    that in several test suites, which end up using _wenviron in runtime.

--- tests/utils/suite.c
+++ tests/utils/suite.c
@@ -2,5 +2,17 @@
 
 DEFINE_SUITE();
 
+#ifdef _WIN32
+SETUP_ONCE()
+{
+	extern int _CRT_glob;
+	extern void __wgetmainargs(int *, wchar_t ***, wchar_t ***, int, int *);
+
+	wchar_t **envp, **argv;
+	int argc, si = 0;
+	__wgetmainargs(&argc, &argv, &envp, _CRT_glob, &si);
+}
+#endif
+
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
 /* vim: set cinoptions+=t0 filetype=c : */
