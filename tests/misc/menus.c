vifm/vifm

commit 984c2c12dbec459349bb0b330315956e45d1d129

Fix displaying separator in `:*map prefix` menus
Thanks to nicodebo.

src/menus/map_menu.c
@@ -73,15 +73,19 @@ add_mapping_item(const wchar_t lhs[], const wchar_t rhs[], const char descr[])
 
 	char *mb_lhs;
 
-	if(wcsncmp(prefix, lhs, prefix_len) != 0)
+	const int is_separator = (lhs[0] == L'\0');
+	if(!is_separator)
 	{
-		return;
-	}
+		if(wcsncmp(prefix, lhs, prefix_len) != 0)
+		{
+			return;
+		}
 
-	/* Handle empty RHS, but don't affect separator line. */
-	if(rhs[0] == L'\0' && lhs[0] != L'\0' && descr[0] == '\0')
-	{
-		rhs = L"<nop>";
+		/* Handle empty RHS. */
+		if(rhs[0] == L'\0' && descr[0] == '\0')
+		{
+			rhs = L"<nop>";
+		}
 	}
 
 	mb_lhs = wstr_to_spec(lhs);

THANKS
@@ -91,6 +91,7 @@ Milan Svoboda (tex)
 Miles (pkordy)
 Miodrag Tokić (loonies)
 mvucBmM0
+nicodebo
 obtroston
 octos
 Oleg Gordienko (gordio)

commit c5edac5cd0a5126216270c8999e2b8df5cf305b5

Skip opening mappings list with empty line
This is a regression caused by 984c2c12dbec459.

src/menus/map_menu.c
@@ -62,6 +62,15 @@ show_map_menu(FileView *view, const char mode_str[], int mode,
 
 	vle_keys_list(mode, &add_mapping_item, dialogs);
 
+	/* If we filtered out all meaningful lines, clear the menu. */
+	if(m.len == 1 && *m.items[0] == '\0')
+	{
+		free(m.items[0]);
+		free(m.items);
+		m.items = NULL;
+		m.len = 0;
+	}
+
 	return display_menu(m.state, view);
 }
 
@@ -92,7 +101,11 @@ add_mapping_item(const wchar_t lhs[], const wchar_t rhs[], const char descr[])
 
 	m.items = reallocarray(m.items, m.len + 1, sizeof(char *));
 
-	if(rhs[0] == L'\0')
+	if(is_separator)
+	{
+		m.items[m.len++] = strdup("");
+	}
+	else if(rhs[0] == L'\0')
 	{
 		m.items[m.len++] = format_str("%-*s %s", MAP_WIDTH, mb_lhs, descr);
 	}

tests/misc/menus.c
@@ -6,7 +6,11 @@
 #include <string.h> /* strcpy() strdup() */
 
 #include "../../src/cfg/config.h"
+#include "../../src/engine/keys.h"
+#include "../../src/menus/map_menu.h"
 #include "../../src/menus/menus.h"
+#include "../../src/modes/modes.h"
+#include "../../src/ui/statusbar.h"
 #include "../../src/utils/fs.h"
 #include "../../src/utils/string_array.h"
 
@@ -201,6 +205,22 @@ TEST(null_pattern_causes_pattern_reuse)
 	assert_int_equal(1, m.pos);
 	assert_true(search_menu_list(NULL, &m, 1));
 	assert_int_equal(2, m.pos);
+}
+
+TEST(empty_mappings_menu_is_not_displayed)
+{
+	init_modes();
+
+	status_bar_message("");
+	assert_failure(show_map_menu(&lwin, "normal", NORMAL_MODE, L"nonsense"));
+	assert_string_equal("No mappings found", get_last_message());
+
+	vle_keys_user_add(L"this", L"that", NORMAL_MODE, 0);
+	status_bar_message("");
+	assert_failure(show_map_menu(&lwin, "normal", NORMAL_MODE, L"nonsense"));
+	assert_string_equal("No mappings found", get_last_message());
+
+	vle_keys_reset();
 }
 
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */