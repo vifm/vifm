vifm/vifm

commit 6e8fb6c3379ce9015740530d9bdc5c63b13636be

Fix ruler update on `--remote --select`
Thanks to Marcin Kurczewski (a.k.a. rr-).

Fixes #202 on GitHub.

src/ui/ui.c
@@ -114,7 +114,7 @@ static void fixup_titles_attributes(const FileView *view, int active_view);
 static uint64_t get_updated_time(uint64_t prev);
 
 void
-ui_ruler_update(FileView *view)
+ui_ruler_update(FileView *view, int lazy_redraw)
 {
 	char *expanded;
 
@@ -124,6 +124,10 @@ ui_ruler_update(FileView *view)
 	expanded = break_in_two(expanded, getmaxx(ruler_win));
 
 	ui_ruler_set(expanded);
+	if(!lazy_redraw)
+	{
+		wrefresh(ruler_win);
+	}
 
 	free(expanded);
 }
@@ -594,7 +598,7 @@ update_screen(UpdateType update_kind)
 		}
 		else
 		{
-			ui_ruler_update(curr_view);
+			ui_ruler_update(curr_view, 1);
 		}
 	}
 

THANKS
@@ -70,6 +70,7 @@ Losiara
 lyuts
 MadMaverick9
 Marcin Juszkiewicz (hrw)
+Marcin Kurczewski (rr-)
 Marcos Cruz
 Marius Schmidl
 Martin Fischer

src/event_loop.c
@@ -430,7 +430,7 @@ process_scheduled_updates_of_view(FileView *view)
 			load_saving_pos(view, 1);
 			if(view == curr_view && !is_status_bar_multiline())
 			{
-				ui_ruler_update(view);
+				ui_ruler_update(view, 1);
 			}
 			return 1;
 		case UUE_FULL_RELOAD:

src/modes/cmdline.c
@@ -818,7 +818,7 @@ leave_cmdline_mode(void)
 
 	if(!vle_mode_is(MENU_MODE))
 	{
-		ui_ruler_update(curr_view);
+		ui_ruler_update(curr_view, 1);
 	}
 
 	attr = cfg.cs.color[CMD_LINE_COLOR].attr;

src/modes/modes.c
@@ -174,7 +174,7 @@ modes_post(void)
 		if(!is_status_bar_multiline())
 		{
 			update_stat_window(curr_view, 0);
-			ui_ruler_update(curr_view);
+			ui_ruler_update(curr_view, 1);
 		}
 	}
 

src/modes/visual.c
@@ -1345,7 +1345,7 @@ static void
 update(void)
 {
 	redraw_view(view);
-	ui_ruler_update(view);
+	ui_ruler_update(view, 1);
 }
 
 void

src/ui/fileview.c
@@ -1629,9 +1629,16 @@ fview_position_updated(FileView *view)
 	refresh_view_win(view);
 	update_stat_window(view, 0);
 
-	if(view == curr_view && curr_stats.view)
+	if(view == curr_view)
 	{
-		quick_view_file(view);
+		/* We're updating view non-lazily above, so doing the same with the
+		 * ruler. */
+		ui_ruler_update(view, 0);
+
+		if(curr_stats.view)
+		{
+			quick_view_file(view);
+		}
 	}
 }
 

src/ui/ui.h
@@ -379,8 +379,8 @@ WINDOW *lborder;
 WINDOW *mborder;
 WINDOW *rborder;
 
-/* Updates the ruler with information from the view. */
-void ui_ruler_update(FileView *view);
+/* Updates the ruler with information from the view (possibly lazily). */
+void ui_ruler_update(FileView *view, int lazy_redraw);
 
 /* Sets text to be displayed on the ruler.  Real window update is postponed for
  * efficiency reasons. */

commit 64db601da5e6bf43a1a301c170fc83bfde708a64

Fix ruler being updated when it shouldn't appear
Was broken by 6e8fb6c3379ce9015740530d9bdc5c63b13636be.

Filtering updates cursor position, which eventually calls
ui_ruler_update().  This isn't a perfect solution, but better one
requires better UI code design.

Thanks to filterfalse.

src/ui/ui.c
@@ -117,7 +117,15 @@ static uint64_t get_updated_time(uint64_t prev);
 void
 ui_ruler_update(FileView *view, int lazy_redraw)
 {
+	const int ruler_is_visible = !vle_mode_is(CMDLINE_MODE);
 	char *expanded;
+
+	if(!ruler_is_visible)
+	{
+		/* Do nothing, especially don't update layout because it might be a custom
+		 * layout at the moment. */
+		return;
+	}
 
 	update_statusbar_layout();
 