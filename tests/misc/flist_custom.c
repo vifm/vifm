vifm/vifm

commit 14440fe4bb6e40f07748d1bfd1392e2622d6e8b4

Update cursor position in zap_entries()
This allows cursor to point somewhere near original position even
when it's gone.

src/filelist.c
@@ -2281,6 +2281,11 @@ zap_entries(FileView *view, dir_entry_t *entries, int *count, zap_filter filter,
 			free_dir_entry(view, &entry[k]);
 		}
 
+		if(view->list_pos >= i && view->list_pos < i + nremoved)
+		{
+			view->list_pos = j;
+		}
+
 		/* Add directory leaf if we just removed last child of the last of nodes
 		 * that wasn't filtered.  We can use one entry because if something was
 		 * filtered out, there is space for at least one extra entry. */

tests/misc/flist_tree.c
@@ -356,12 +356,14 @@ TEST(excluding_all_children_places_a_dummy)
 	lwin.dir_entry[3].selected = 1;
 	lwin.dir_entry[4].selected = 1;
 	lwin.selected_files = 2;
+	lwin.list_pos = 4;
 
 	flist_custom_exclude(&lwin);
 	validate_tree(&lwin);
 
 	assert_int_equal(0, lwin.selected_files);
 	assert_int_equal(0, lwin.filtered);
+	assert_int_equal(3, lwin.list_pos);
 	assert_int_equal(11, lwin.list_rows);
 }
 

commit 352298f641a4fcc3a62ffe47eb8a1fe1e109ee06

Fix incorrect cursor positioning on file deletion
Specifically on removing elements from custom view with local filter.

Was broken by 14440fe4bb6e40f07748d1bfd1392e2622d6e8b4.

src/filelist.c
@@ -1659,9 +1659,15 @@ populate_custom_view(FileView *view, int reload)
 	{
 		if(custom_list_is_incomplete(view))
 		{
-			/* Load initial list of custom entries if it's available. */
+			char selected_path[PATH_MAX];
+			get_current_full_path(view, sizeof(selected_path), selected_path);
+
+			/* Replacing list of entries invalidates cursor position, so we remember
+			 * previously selected file and try to position at it again. */
 			replace_dir_entries(view, &view->dir_entry, &view->list_rows,
 					view->local_filter.entries, view->local_filter.entry_count);
+
+			(void)set_position_by_path(view, selected_path);
 		}
 
 		(void)zap_entries(view, view->dir_entry, &view->list_rows,
@@ -1932,7 +1938,11 @@ zap_entries(FileView *view, dir_entry_t *entries, int *count, zap_filter filter,
 			fentry_free(view, &entry[k]);
 		}
 
-		if(view->list_pos >= i && view->list_pos < i + nremoved)
+		/* If we're removing file from main list of entries and cursor is right on
+		 * this file, move cursor at position this file would take in resulting
+		 * list. */
+		if(entries == view->dir_entry && view->list_pos >= i &&
+				view->list_pos < i + nremoved)
 		{
 			view->list_pos = j;
 		}

tests/misc/flist_custom.c
@@ -718,6 +718,54 @@ TEST(loading_cv_resets_search_results)
 	assert_int_equal(0, lwin.matches);
 }
 
+TEST(cursor_is_positioned_close_to_disappeared_file)
+{
+	make_abs_path(lwin.curr_dir, sizeof(lwin.curr_dir), SANDBOX_PATH, "", cwd);
+	assert_success(chdir(SANDBOX_PATH));
+	create_file("1");
+	create_file("2");
+	create_file("3");
+	create_file("4");
+	create_file("5");
+	create_file("6");
+
+	filters_view_reset(&lwin);
+
+	flist_custom_start(&lwin, "test");
+	flist_custom_add(&lwin, "1");
+	flist_custom_add(&lwin, "2");
+	flist_custom_add(&lwin, "3");
+	flist_custom_add(&lwin, "4");
+	flist_custom_add(&lwin, "5");
+	flist_custom_add(&lwin, "6");
+	assert_true(flist_custom_finish(&lwin, CV_REGULAR, 0) == 0);
+	assert_int_equal(6, lwin.list_rows);
+
+	local_filter_apply(&lwin, "[4-6]");
+	load_dir_list(&lwin, 1);
+	assert_int_equal(3, lwin.list_rows);
+
+	lwin.list_pos = 2;
+	assert_success(unlink("6"));
+	/* This reloads view zipping removed and filtered-out files.  It should try to
+	 * stay close to original position. */
+	load_dir_list(&lwin, 1);
+	assert_int_equal(2, lwin.list_rows);
+
+	/* Both 2 and 1 are valid answers, list_pos is automatically corrected. */
+	assert_true(lwin.list_pos == 1 || lwin.list_pos == 2);
+
+	filter_dispose(&lwin.manual_filter);
+	filter_dispose(&lwin.auto_filter);
+
+	assert_success(chdir(SANDBOX_PATH));
+	assert_success(unlink("1"));
+	assert_success(unlink("2"));
+	assert_success(unlink("3"));
+	assert_success(unlink("4"));
+	assert_success(unlink("5"));
+}
+
 static void
 column_line_print(const void *data, int column_id, const char buf[],
 		size_t offset, AlignType align, const char full_column[])