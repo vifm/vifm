606ada666ea91ac0d1376770a5c7f0092424d266
xaizek <xaizek@posteo.net>
2018-01-12 21:03:15 +0200

    Fix cloning of files like `.name`
    To produce `.name(1)` instead of `(1).name`.
    
    Thanks to filterfalse.

--- tests/fileops/gen_clone_name.c
+++ tests/fileops/gen_clone_name.c
@@ -38,5 +38,16 @@ TEST(name_inc)
 			gen_clone_name(".", "name(1).tar.ext2"));
 }
 
+TEST(dot_files)
+{
+	assert_string_equal(".name(1)", gen_clone_name(".", ".name"));
+	assert_string_equal(".name(1).ext", gen_clone_name(".", ".name.ext"));
+	assert_string_equal(".name(2)", gen_clone_name(".", ".name(1)"));
+
+	assert_string_equal(".config(1).d", gen_clone_name(".", ".config.d"));
+	assert_string_equal(".config(2).d", gen_clone_name(".", ".config(1).d"));
+	assert_string_equal(".config(1).d(1)", gen_clone_name(".", ".config.d(1)"));
+}
+
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
 /* vim: set cinoptions+=t0 filetype=c : */
