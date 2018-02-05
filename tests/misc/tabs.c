8ea385d840a7d47c8255d8f7fe1dfd469a334ceb
xaizek <xaizek@posteo.net>
2018-02-05 23:36:35 +0200

    Complete coverage of ui/tabs unit
    Only handling of out of memory errors isn't covered.

--- tests/misc/tabs.c
+++ tests/misc/tabs.c
@@ -82,8 +82,6 @@ TEST(invisible_panes_are_accounted_for_on_counting_visitors_with_pane_tabs)
 
 TEST(pane_tabs_are_swapped_on_switch)
 {
-	strcpy(lwin.curr_dir, "/l");
-	strcpy(rwin.curr_dir, "/r");
 	cfg.pane_tabs = 1;
 	tabs_new(NULL);
 	tabs_new(NULL);
@@ -94,6 +92,156 @@ TEST(pane_tabs_are_swapped_on_switch)
 
 	assert_int_equal(1, tabs_count(&lwin));
 	assert_int_equal(3, tabs_count(&rwin));
+}
+
+TEST(goto_tab_stays_on_current_pane_tab)
+{
+	tab_info_t tab_info;
+
+	cfg.pane_tabs = 1;
+	tabs_new(NULL);
+	tabs_new(NULL);
+
+	tabs_goto(2);
+
+	assert_true(tabs_get(&lwin, 2, &tab_info));
+	assert_true(tab_info.view == &lwin);
+}
+
+TEST(goto_tab_checks_boundaries_of_pane_tabs)
+{
+	tab_info_t tab_info;
+
+	cfg.pane_tabs = 1;
+	tabs_new(NULL);
+	tabs_new(NULL);
+
+	tabs_goto(-1);
+	assert_true(tabs_get(&lwin, 2, &tab_info));
+	assert_true(tab_info.view == &lwin);
+
+	tabs_goto(10);
+	assert_true(tabs_get(&lwin, 2, &tab_info));
+	assert_true(tab_info.view == &lwin);
+}
+
+TEST(goto_tab_stays_on_current_global_tab)
+{
+	tab_info_t tab_info;
+
+	tabs_new(NULL);
+	tabs_new(NULL);
+
+	tabs_goto(2);
+
+	assert_true(tabs_get(&lwin, 2, &tab_info));
+	assert_true(tab_info.view == &lwin);
+}
+
+TEST(goto_tab_checks_boundaries_of_global_tabs)
+{
+	tab_info_t tab_info;
+
+	tabs_new(NULL);
+	tabs_new(NULL);
+
+	tabs_goto(-1);
+	assert_true(tabs_get(&lwin, 2, &tab_info));
+	assert_true(tab_info.view == &lwin);
+
+	tabs_goto(10);
+	assert_true(tabs_get(&lwin, 2, &tab_info));
+	assert_true(tab_info.view == &lwin);
+}
+
+TEST(view_roles_are_swapped_on_switching_global_tabs)
+{
+	tabs_new(NULL);
+	tabs_new(NULL);
+	curr_view = &rwin;
+	other_view = &lwin;
+
+	tabs_goto(1);
+
+	assert_true(curr_view == &lwin);
+	assert_true(other_view == &rwin);
+}
+
+TEST(pane_tab_is_closed)
+{
+	cfg.pane_tabs = 1;
+	tabs_new(NULL);
+
+	tabs_goto(0);
+	tabs_close();
+
+	assert_int_equal(1, tabs_count(&lwin));
+}
+
+TEST(global_tab_is_closed)
+{
+	tabs_new(NULL);
+
+	tabs_goto(0);
+	tabs_close();
+
+	assert_int_equal(1, tabs_count(&lwin));
+}
+
+TEST(pane_tabs_are_traversed_forward)
+{
+	tab_info_t tab_info;
+
+	cfg.pane_tabs = 1;
+	tabs_new(NULL);
+
+	tabs_next(1);
+
+	assert_true(tabs_get(&lwin, 0, &tab_info));
+	assert_true(tab_info.view == &lwin);
+}
+
+TEST(pane_tabs_are_traversed_back)
+{
+	tab_info_t tab_info;
+
+	cfg.pane_tabs = 1;
+	tabs_new(NULL);
+
+	tabs_previous(1);
+
+	assert_true(tabs_get(&lwin, 0, &tab_info));
+	assert_true(tab_info.view == &lwin);
+}
+
+TEST(tabs_get_checks_index)
+{
+	tab_info_t tab_info;
+
+	cfg.pane_tabs = 0;
+	assert_false(tabs_get(&lwin, -1, &tab_info));
+	assert_false(tabs_get(&lwin, 1, &tab_info));
+
+	cfg.pane_tabs = 1;
+	assert_false(tabs_get(&lwin, -1, &tab_info));
+	assert_false(tabs_get(&lwin, 1, &tab_info));
+}
+
+TEST(tabs_get_returns_inactive_global_tab)
+{
+	tab_info_t tab_info;
+
+	tabs_new(NULL);
+	assert_true(tabs_get(&lwin, 0, &tab_info));
+	assert_true(tab_info.view != &lwin);
+	assert_true(tab_info.view != &rwin);
+
+	curr_view = &rwin;
+	other_view = &lwin;
+	tabs_new(NULL);
+	assert_true(tabs_get(&lwin, 1, &tab_info));
+	assert_true(tab_info.view != &lwin);
+	assert_true(tab_info.view != &rwin);
 }
 
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
