vifm/vifm

commit 246eca0d8f88d534e6a31cf9f0bd676960909e3b

Drop unfinished if-else stmts on leaving cmdline
Thanks to Marcin Kurczewski (a.k.a. rr-).

See https://q2a.vifm.info/305/how-to-map-a-key-to-a-condition

src/modes/cmdline.c
@@ -1295,7 +1295,15 @@ cmd_return(key_info_t key_info, keys_info_t *keys_info)
 			real_start++;
 		}
 
+		if(sub_mode == CLS_COMMAND)
+		{
+			commands_scope_start();
+		}
 		curr_stats.save_msg = exec_commands(real_start, curr_view, cmd_type);
+		if(sub_mode == CLS_COMMAND)
+		{
+			curr_stats.save_msg = (commands_scope_finish() != 0);
+		}
 	}
 	else if(sub_mode == CLS_PROMPT)
 	{

src/cmd_core.c
@@ -1202,6 +1202,12 @@ cmds_scoped_endif(void)
 	return 0;
 }
 
+int
+cmds_scoped_empty(void)
+{
+	return int_stack_is_empty(&if_levels);
+}
+
 /* Checks that bottom of block scope is reached.  Returns non-zero if so,
  * otherwise zero is returned. */
 static int

src/cmd_core.h
@@ -138,8 +138,12 @@ int cmds_scoped_elseif(int cond);
 int cmds_scoped_else(void);
 
 /* Terminates if-else-endif statement.  Returns non-zero if endif wasn't
- * expected at this point, otherwise zero is returned.  */
+ * expected at this point, otherwise zero is returned. */
 int cmds_scoped_endif(void);
+
+/* Checks whether there are any scope or scoped command registered at the
+ * moment.  Returns non-zero if so, otherwise zero is returned. */
+int cmds_scoped_empty(void);
 
 #ifdef TEST
 #include "engine/cmds.h"

tests/misc/cmdline_scope.c
@@ -1,0 +1,47 @@
+#include <stic.h>
+
+#include "../../src/engine/keys.h"
+#include "../../src/modes/cmdline.h"
+#include "../../src/modes/modes.h"
+#include "../../src/modes/wk.h"
+#include "../../src/ui/ui.h"
+#include "../../src/cmd_core.h"
+
+static line_stats_t *stats;
+
+SETUP_ONCE()
+{
+	stats = get_line_stats();
+	curr_view = &lwin;
+}
+
+SETUP()
+{
+	init_modes();
+	enter_cmdline_mode(CLS_COMMAND, "", NULL);
+}
+
+TEARDOWN()
+{
+	(void)vle_keys_exec_timed_out(WK_C_c);
+
+	vle_keys_reset();
+}
+
+TEST(unfinished_scope_is_terminated_at_the_end)
+{
+	(void)vle_keys_exec_timed_out(L":if 'a' == 'b'" WK_C_m);
+	assert_true(cmds_scoped_empty());
+
+	(void)vle_keys_exec_timed_out(L":if 'a' == 'a' | | else" WK_C_m);
+	assert_true(cmds_scoped_empty());
+}
+
+TEST(finished_scope_is_terminated_at_the_end)
+{
+	(void)vle_keys_exec_timed_out(L":if 'a' == 'a' | else | endif" WK_C_m);
+	assert_true(cmds_scoped_empty());
+}
+
+/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
+/* vim: set cinoptions+=t0 filetype=c : */

commit 2fe51c9f91a0f197d7ac29f0650a8d084f196b0a

Fix keeping status bar messages after :command
Just broke it in 246eca0d8f88d534e6a31cf9f0bd676960909e3b:

    Drop unfinished if-else stmts on leaving cmdline

src/modes/cmdline.c
@@ -1302,7 +1302,10 @@ cmd_return(key_info_t key_info, keys_info_t *keys_info)
 		curr_stats.save_msg = exec_commands(real_start, curr_view, cmd_type);
 		if(sub_mode == CLS_COMMAND)
 		{
-			curr_stats.save_msg = (commands_scope_finish() != 0);
+			if(commands_scope_finish() != 0)
+			{
+				curr_stats.save_msg = 1;
+			}
 		}
 	}
 	else if(sub_mode == CLS_PROMPT)