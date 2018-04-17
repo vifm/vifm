de590135f48c4bcb7482acdcae68eaeb36ad54b1
xaizek <xaizek@posteo.net>
2018-04-17 21:34:16 +0300

    Formatting fixes for the registers unit

--- src/registers.c
+++ src/registers.c
@@ -36,6 +36,7 @@
 #include "utils/shmem.h"
 #include "utils/str.h"
 #include "utils/string_array.h"
+#include "utils/test_helpers.h"
 #include "utils/utils.h"
 #include "trash.h"
 
@@ -64,17 +65,19 @@ const char valid_registers[] = {
  * uppercase registers (virtual ones) + termination null character. */
 ARRAY_GUARD(valid_registers, NUM_REGISTERS + NUM_LETTER_REGISTERS + 1);
 
-/* Shared memory register synchronization */
-struct register_metadata {
-	unsigned write_counter;
-	size_t num_entries;
-	size_t offset;
-	size_t length_used;
-	size_t length_available;
-};
+/* Describes register contents in a shared memory. */
+typedef struct
+{
+	unsigned int generation; /* Generation of the data. */
+	size_t num_entries;      /* Number of file paths in the register. */
+	size_t offset;           /* Offset of the first path. */
+	size_t length_used;      /* Length currently used. */
+	size_t length_available; /* Total length available for the register. */
+}
+reg_metadata_t;
 
 /* Describes shared state. */
-struct shared_registers
+typedef struct
 {
 	/* Whether data is in consistent state.  Probably not bulletproof, but still
 	 * an attempt to avoid using broken data. */
@@ -84,19 +87,15 @@ struct shared_registers
 	 * shared_initial and shared_mmap_bytes. */
 	size_t size_backed;
 
-	/* Generation of the data. */
-	unsigned int write_counter;
-	/* Excludes metadata. */
-	size_t length_area_used;
+	unsigned int generation; /* Generation of the data. */
+	size_t length_area_used; /* Length without metadata. */
 
-	/* BLACKHOLE, DEFAULT, a-z */
-	struct register_metadata register_metadata[NUM_REGISTERS];
-};
+	reg_metadata_t reg_metadata[NUM_REGISTERS]; /* BLACKHOLE, DEFAULT, a-z */
+}
+shared_state_t;
 
-#define SHARED_ALL_METADATA_SIZE (sizeof(struct shared_registers))
-
-/* Maximum length the shared memory name will take. */
-#define SHARED_USE_NAME_MAX 4096
+/* Size of the metadata. */
+#define SHARED_ALL_METADATA_SIZE (sizeof(shared_state_t))
 
 /*
  * The following defines two sizes
@@ -150,26 +149,32 @@ static size_t shared_mmap_bytes = 1024 * 1024 * 16; /* 16 MiB ~ enough? */
 static size_t shared_initial    = 1024 * 1024 * 16;
 #endif
 
-static struct shared_registers* shmem = NULL;
 /* Mutex that protects shared memory of shmem_obj. */
 static gmux_t *shmem_gmux;
 /* Shared memory area object. */
 static shmem_t *shmem_obj;
-/* pointer to the shared memory as an unstructured blob of bytes */
-static char* shmem_raw = NULL;
-static unsigned my_write_counter = 0;
-static char debug_print_to_stdout = 0;
+/* Pointer to the shared memory as an unstructured blob of bytes. */
+static char *shmem_raw;
+/* Pointer to shared memory as a structure. */
+static shared_state_t *shmem;
+/* Last generation number that we've seen. */
+static unsigned int seen_generation;
+/* Whether we're in debug mode. */
+static int debug_print_to_stdout;
 
 static void regs_sync_error(const char msg[]);
-static char regs_sync_to_shared_memory_critical();
-static char regs_sync_enter_critical_section();
-static void regs_sync_rewrite_critical();
+static int regs_sync_to_shared_memory_critical(void);
+static int regs_sync_enter_critical_section(void);
+static void regs_sync_rewrite_critical(void);
 static size_t regs_sync_store_register_contents_critical(size_t current_offset,
 	size_t reg_id);
 static size_t regs_sync_store_register_contents_in_place(size_t current_offset,
 	size_t reg_id);
-static char regs_sync_resize_allocation(size_t newsz);
-static void regs_sync_leave_critical_section();
+static int regs_sync_resize_allocation(size_t newsz);
+static void regs_sync_leave_critical_section(void);
+TSTATIC void regs_sync_debug_print_memory(void);
+static void regs_sync_debug_print_area(size_t offset, size_t length);
+TSTATIC void regs_sync_enable_test_mode(void);
 
 void
 regs_init(void)
@@ -425,23 +430,14 @@ regs_suggest(regs_suggest_cb cb, int max_entries_per_reg)
 /* Shared memory synchronization of register contents -- implementation */
 
 void
-regs_sync_enable(char* shared_memory_name)
+regs_sync_enable(const char shared_memory_name[])
 {
-	/*
-	 * In order to allow shared memory names to be written without knowing the
-	 * details of the shared memory implementation (e.g. Linux needs a leading
-	 * slash for the shared memory name), a different name than the one supplied
-	 * is actually used. Currently, the provided name is just prefixed by
-	 * /vifm-
-	 */
-	char use_name[SHARED_USE_NAME_MAX];
+	char use_name[NAME_MAX + 1];
 	snprintf(use_name, sizeof(use_name), "regs-%s", shared_memory_name);
 
-	/*
-	 * Disable feature if it was active before.
+	/* Disable feature if it was active before.
 	 * It is not sufficient to return if the feature is already enabled because
-	 * the shared_memory_name may have changed.
-	 */
+	 * the shared_memory_name may have changed. */
 	regs_sync_disable();
 
 	shmem_gmux = gmux_create(use_name);
@@ -470,12 +466,12 @@ regs_sync_enable(char* shared_memory_name)
 	shmem_raw = shmem_get_ptr(shmem_obj);
 
 	/* structured view on the same data */
-	shmem = (struct shared_registers*)shmem_raw;
+	shmem = (shared_state_t *)shmem_raw;
 
 	/* Initialization of just created shared memory area. */
 	if(shmem_created_by_us(shmem_obj))
 	{
-		my_write_counter = shmem->write_counter;
+		seen_generation = shmem->generation;
 		shmem->data_is_consistent = 0;
 		shmem->size_backed = shared_initial;
 
@@ -492,6 +488,7 @@ regs_sync_enable(char* shared_memory_name)
 	regs_sync_leave_critical_section();
 }
 
+/* Displays the message either in a dialog or on stdout. */
 static void
 regs_sync_error(const char msg[])
 {
@@ -506,7 +503,7 @@ regs_sync_error(const char msg[])
 }
 
 void
-regs_sync_disable()
+regs_sync_disable(void)
 {
 	gmux_free(shmem_gmux);
 	shmem_gmux = NULL;
@@ -518,7 +515,7 @@ regs_sync_disable()
 }
 
 void
-regs_sync_to_shared_memory()
+regs_sync_to_shared_memory(void)
 {
 	if(regs_sync_enter_critical_section())
 	{
@@ -527,71 +524,90 @@ regs_sync_to_shared_memory()
 	}
 }
 
-static char
-regs_sync_to_shared_memory_critical()
+/* Puts contents of our registers into shared memory.  Returns 1 on success, 0
+ * on failure (cleans up as needed on fail). */
+static int
+regs_sync_to_shared_memory_critical(void)
 {
-	/* returns 1 on success, 0 on failure (cleans up as needed on fail) */
-
 	shmem->data_is_consistent = 0;
-	my_write_counter = ++shmem->write_counter;
+	seen_generation = ++shmem->generation;
 
-	/* determine memory requirements for state to be synchronized */
+	/* Determine memory requirements for state to be synchronized. */
 	size_t new_register_sizes_total = 0;
 	size_t new_register_sizes[NUM_REGISTERS];
 
-	size_t i;
+	int i;
 	int j;
 
-	for(i = 0; i < NUM_REGISTERS; ++i) {
+	for(i = 0; i < NUM_REGISTERS; ++i)
+	{
 		new_register_sizes[i] = 0;
-		for(j = 0; j < registers[i].nfiles; j++) {
-			/* +1 to count the 0 terminator byte */
+		for(j = 0; j < registers[i].nfiles; ++j)
+		{
+			/* +1 to count the 0 terminator byte. */
 			new_register_sizes[i] += strlen(registers[i].files[j]) + 1;
 		}
 		new_register_sizes_total += new_register_sizes[i];
 	}
 
-	/* check which registers grow over their currently available space */
+	/* Check which registers grow over their currently available space. */
 	size_t size_not_fit_to_existing = 0;
-	for(i = 0; i < NUM_REGISTERS; ++i) {
-		if(new_register_sizes[i] > shmem->register_metadata[i].length_available)
+	for(i = 0; i < NUM_REGISTERS; ++i)
+	{
+		if(new_register_sizes[i] > shmem->reg_metadata[i].length_available)
+		{
 			size_not_fit_to_existing += new_register_sizes[i];
+		}
 	}
 
 	if(size_not_fit_to_existing <= shmem->size_backed - SHARED_ALL_METADATA_SIZE -
-			shmem->length_area_used) {
-		size_t halved_sz = shmem->size_backed / 2;
-		if(new_register_sizes_total < (halved_sz - SHARED_ALL_METADATA_SIZE)
-				&& shmem->size_backed > shared_initial) {
-			/* halve allocation size */
-			if(!regs_sync_resize_allocation(halved_sz))
+			shmem->length_area_used)
+	{
+		const size_t halved_size = shmem->size_backed/2;
+		if(new_register_sizes_total < (halved_size - SHARED_ALL_METADATA_SIZE)
+				&& shmem->size_backed > shared_initial)
+		{
+			/* Halve allocation size. */
+			if(!regs_sync_resize_allocation(halved_size))
+			{
 				return 0;
+			}
 			regs_sync_rewrite_critical();
-		} else {
+		}
+		else
+		{
 			size_t offset = SHARED_ALL_METADATA_SIZE + shmem->length_area_used;
-			for(i = 0; i < NUM_REGISTERS; ++i) {
+			for(i = 0; i < NUM_REGISTERS; ++i)
+			{
 				if(new_register_sizes[i] >
-						shmem->register_metadata[i].length_available) {
-					/* append to end */
+						shmem->reg_metadata[i].length_available)
+				{
+					/* Append at the end. */
 					offset = regs_sync_store_register_contents_critical(offset, i);
-				} else {
-					/* update in place */
+				}
+				else
+				{
+					/* Update in place. */
 					regs_sync_store_register_contents_in_place(
-						shmem->register_metadata[i].offset, i);
+						shmem->reg_metadata[i].offset, i);
 				}
 			}
 			shmem->length_area_used = offset - SHARED_ALL_METADATA_SIZE;
 		}
 	} else {
-		/* double allocation size till fit */
+		/* Double allocation size till fit. */
 		size_t new_size_backed = shmem->size_backed;
 		while(new_register_sizes_total >
 				(new_size_backed - SHARED_ALL_METADATA_SIZE))
+		{
 			new_size_backed *= 2;
+		}
 
 		if(new_size_backed != shmem->size_backed &&
 				!regs_sync_resize_allocation(new_size_backed))
+		{
 			return 0;
+		}
 
 		regs_sync_rewrite_critical();
 	}
@@ -599,8 +615,10 @@ regs_sync_to_shared_memory_critical()
 	return 1;
 }
 
-static char
-regs_sync_enter_critical_section()
+/* Locks the mutex protecting shared memory.  Returns non-zero on success,
+ * otherwise zero is returned. */
+static int
+regs_sync_enter_critical_section(void)
 {
 	if(shmem == NULL)
 	{
@@ -617,49 +635,57 @@ regs_sync_enter_critical_section()
 	return 1;
 }
 
+/* Rewrites shared memory from scratch. */
 static void
-regs_sync_rewrite_critical()
+regs_sync_rewrite_critical(void)
 {
-	/* assumption: enough space in shared memory */
+	/* Assumption: enough space in shared memory. */
 	size_t offset = SHARED_ALL_METADATA_SIZE;
-	size_t i;
+	int i;
 	for(i = 0; i < NUM_REGISTERS; ++i)
+	{
 		offset = regs_sync_store_register_contents_critical(offset, i);
+	}
 	shmem->length_area_used = offset - SHARED_ALL_METADATA_SIZE;
 }
 
+/* Dumps contents of a register into shared memory at specified offset anew.
+ * Returns new offset. */
 static size_t
 regs_sync_store_register_contents_critical(size_t current_offset, size_t reg_id)
 {
-	/* returns new offset */
-	size_t new_offset =
+	const size_t new_offset =
 		regs_sync_store_register_contents_in_place(current_offset, reg_id);
-	shmem->register_metadata[reg_id].length_available =
-		shmem->register_metadata[reg_id].length_used;
+	shmem->reg_metadata[reg_id].length_available =
+		shmem->reg_metadata[reg_id].length_used;
 	return new_offset;
 }
 
+/* Dumps contents of a register into shared memory at specified offset.
+ * Returns new offset. */
 static size_t
 regs_sync_store_register_contents_in_place(size_t current_offset, size_t reg_id)
 {
 	int i;
-	shmem->register_metadata[reg_id].write_counter = my_write_counter;
-	shmem->register_metadata[reg_id].num_entries   = registers[reg_id].nfiles;
-	shmem->register_metadata[reg_id].offset        = current_offset;
-	for(i = 0; i < registers[reg_id].nfiles; ++i) {
-		size_t entry_len = strlen(registers[reg_id].files[i]) + 1;
+	shmem->reg_metadata[reg_id].generation  = seen_generation;
+	shmem->reg_metadata[reg_id].num_entries = registers[reg_id].nfiles;
+	shmem->reg_metadata[reg_id].offset      = current_offset;
+	for(i = 0; i < registers[reg_id].nfiles; ++i)
+	{
+		const size_t entry_len = strlen(registers[reg_id].files[i]) + 1;
 		memcpy(shmem_raw + current_offset, registers[reg_id].files[i], entry_len);
 		current_offset += entry_len;
 	}
-	shmem->register_metadata[reg_id].length_used =
-		current_offset - shmem->register_metadata[reg_id].offset;
+	shmem->reg_metadata[reg_id].length_used =
+		current_offset - shmem->reg_metadata[reg_id].offset;
 	return current_offset;
 }
 
-static char
+/* Changes size of shared area.  Returns 1 on success, 0 on failure (performs
+ * cleanup for failure case). */
+static int
 regs_sync_resize_allocation(size_t newsz)
 {
-	/* returns 1 on success, 0 on failure (performs cleanup for failure case) */
 	if(!shmem_resize(shmem_obj, newsz))
 	{
 		/* Shared memory ends here. */
@@ -671,8 +697,9 @@ regs_sync_resize_allocation(size_t newsz)
 	return 1;
 }
 
+/* Unlocks mutex that protects shared memory. */
 static void
-regs_sync_leave_critical_section()
+regs_sync_leave_critical_section(void)
 {
 	shmem->data_is_consistent = 1;
 	if(gmux_unlock(shmem_gmux) != 0)
@@ -682,97 +709,117 @@ regs_sync_leave_critical_section()
 }
 
 void
-regs_sync_from_shared_memory()
+regs_sync_from_shared_memory(void)
 {
 	if(!regs_sync_enter_critical_section())
+	{
 		return;
+	}
 
-	size_t i;
-	int j;
+	if(shmem->generation != seen_generation && shmem->data_is_consistent)
+	{
+		/* Other instance changed the register contents, let's check the details. */
 
-	if(shmem->write_counter != my_write_counter && shmem->data_is_consistent) {
-		/* Other instance canged the register contents, let's check
-		 * the details. */
-		for(i = 0; i < NUM_REGISTERS; ++i) {
-			if(shmem->register_metadata[i].write_counter != my_write_counter) {
+		int i;
+		for(i = 0; i < NUM_REGISTERS; ++i)
+		{
+			if(shmem->reg_metadata[i].generation != seen_generation) {
+				free_string_array(registers[i].files, registers[i].nfiles);
+
+				registers[i].nfiles = shmem->reg_metadata[i].num_entries;
+				registers[i].files = reallocarray(NULL, registers[i].nfiles,
+						sizeof(char *));
+
+				int j;
+				const char *curstrptr = shmem_raw + shmem->reg_metadata[i].offset;
 				for(j = 0; j < registers[i].nfiles; ++j)
-					free(registers[i].files[j]);
-				free(registers[i].files);
-				registers[i].nfiles = shmem->register_metadata[i].num_entries;
-				registers[i].files = malloc(registers[i].nfiles * sizeof(char*));
-				char* curstrptr = shmem_raw + shmem->register_metadata[i].offset;
-				for(j = 0; j < registers[i].nfiles; ++j) {
+				{
 					size_t curlen = strlen(curstrptr) + 1;
-					registers[i].files[j] = malloc(curlen * sizeof(char));
+					registers[i].files[j] = malloc(curlen);
 					memcpy(registers[i].files[j], curstrptr, curlen);
 					curstrptr += curlen;
 				}
 			}
 		}
-		my_write_counter = shmem->write_counter;
+		seen_generation = shmem->generation;
 	}
 
 	regs_sync_leave_critical_section();
 }
 
-static void regs_sync_debug_print_area(size_t offset, size_t length)
-{
-	size_t i;
-	for(i = 0; i < length; ++i) {
-		char val = shmem_raw[offset + i];
-		if(val == 0)
-			printf("(0)");
-		else
-			putchar(val);
-	}
-	putchar('\n');
-}
-
-void regs_sync_debug_print_memory()
+TSTATIC void
+regs_sync_debug_print_memory(void)
 {
 	int i;
-	int j;
 
 	printf("-- BEGIN VIFM shared memory synchronization DUMP --\n");
 	printf("| local\n");
-	for(i = 0; i < NUM_REGISTERS; ++i) {
+	for(i = 0; i < NUM_REGISTERS; ++i)
+	{
 		printf("| | register %2d name=%c, nfiles=%d, files=\n", i,
 			registers[i].name, registers[i].nfiles);
+
+		int j;
 		for(j = 0; j < registers[i].nfiles; ++j)
+		{
 			printf("| | | %s\n", registers[i].files[j]);
+		}
 	}
-	printf("| meta shmem=%p, shmem_raw=%p, my_write_counter=%u\n",
-		shmem, shmem_raw, my_write_counter);
-	if(shmem != NULL) {
-		printf("| shmem size_backed=%d, write_counter=%u, length_area_used=%d, "
-			"register_metadata=\n", (int)shmem->size_backed, shmem->write_counter,
+	printf("| meta shmem=%p, shmem_raw=%p, seen_generation=%u\n",
+		shmem, shmem_raw, seen_generation);
+	if(shmem != NULL)
+	{
+		printf("| shmem size_backed=%d, generation=%u, length_area_used=%d, "
+			"reg_metadata=\n", (int)shmem->size_backed, shmem->generation,
 			(int)shmem->length_area_used);
-		for(i = 0; i < NUM_REGISTERS; ++i) {
+		for(i = 0; i < NUM_REGISTERS; ++i)
+		{
 			printf(
-				"| | register %2d write_counter=%u, num_entries=%d, "
+				"| | register %2d generation=%u, num_entries=%d, "
 					"offset=%d, length_used=%d, length_available=%d\n",
 				i,
-				shmem->register_metadata[i].write_counter,
-				(int)shmem->register_metadata[i].num_entries,
-				(int)shmem->register_metadata[i].offset,
-				(int)shmem->register_metadata[i].length_used,
-				(int)shmem->register_metadata[i].length_available
+				shmem->reg_metadata[i].generation,
+				(int)shmem->reg_metadata[i].num_entries,
+				(int)shmem->reg_metadata[i].offset,
+				(int)shmem->reg_metadata[i].length_used,
+				(int)shmem->reg_metadata[i].length_available
 			);
 			printf("| | | used  =");
-			regs_sync_debug_print_area(shmem->register_metadata[i].offset,
-				shmem->register_metadata[i].length_used);
+			regs_sync_debug_print_area(shmem->reg_metadata[i].offset,
+				shmem->reg_metadata[i].length_used);
 			printf("| | | unused=");
 			regs_sync_debug_print_area(
-				shmem->register_metadata[i].length_used,
-				shmem->register_metadata[i].length_available - 
-					shmem->register_metadata[i].length_used
+				shmem->reg_metadata[i].length_used,
+				shmem->reg_metadata[i].length_available -
+					shmem->reg_metadata[i].length_used
 			);
 		}
 	}
 	printf("-- END   VIFM shared memory synchronization DUMP --\n");
 }
 
-void regs_sync_enable_test_mode()
+/* Dumps region of shared memory to stdout. */
+static void
+regs_sync_debug_print_area(size_t offset, size_t length)
+{
+	size_t i;
+	for(i = 0; i < length; ++i)
+	{
+		const char val = shmem_raw[offset + i];
+		if(val == 0)
+		{
+			printf("(0)");
+		}
+		else
+		{
+			putchar(val);
+		}
+	}
+	putchar('\n');
+}
+
+TSTATIC void
+regs_sync_enable_test_mode(void)
 {
 	debug_print_to_stdout = 1;
 #ifdef __linux__
