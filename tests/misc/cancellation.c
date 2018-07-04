efec09b3761aa8ed4e110745a23de335aa0f33b9
xaizek <xaizek@posteo.net>
2018-07-04 10:03:02 +0300

    Commit ui_cancellation_* test
    It was written some time ago, but wasn't committed by mistake.

---
+++ tests/misc/cancellation.c
@@ -0,0 +1,19 @@
+#include <stic.h>
+
+#include "../../src/ui/cancellation.h"
+
+TEST(generic)
+{
+	assert_false(ui_cancellation_requested());
+
+	ui_cancellation_enable();
+	ui_cancellation_request();
+	ui_cancellation_disable();
+
+	assert_true(ui_cancellation_requested());
+	ui_cancellation_reset();
+	assert_false(ui_cancellation_requested());
+}
+
+/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
+/* vim: set cinoptions+=t0 : */
