b348f10827ed8f1b040e7270324d297206e97fd1
xaizek <xaizek@posteo.net>
2018-07-17 10:43:37 +0300

    Make usercmd handler configurable in cmds tests

---
+++ tests/commands/suite.h
@@ -0,0 +1,11 @@
+#ifndef VIFM_TESTS__SUITE_H__
+#define VIFM_TESTS__SUITE_H__
+
+struct cmd_info_t;
+
+extern int (*user_cmd_handler)(const struct cmd_info_t *cmd_info);
+
+#endif /* VIFM_TESTS__SUITE_H__ */
+
+/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
+/* vim: set cinoptions+=t0 : */
