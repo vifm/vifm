3775e2c7a73bc32b6fe707d6a609acfb7a878411
xaizek <xaizek@posteo.net>
2018-08-08 00:00:22 +0300

    Fix parsing state not being reinitialized
    It could cause unstable parsing outcome for lists of expressions.
    
    Thanks to Jose Riha (a.k.a. jose1711).
    
    See #355 on GitHub.

--- tests/parsing/general.c
+++ tests/parsing/general.c
@@ -2,10 +2,13 @@
 
 #include <stdlib.h> /* free() */
 
+#include "../../src/engine/functions.h"
 #include "../../src/engine/parsing.h"
 #include "../../src/engine/var.h"
 
 #include "asserts.h"
+
+static var_t dummy(const call_info_t *call_info);
 
 TEST(empty_fail)
 {
@@ -71,6 +74,28 @@ TEST(expression_and_comment)
 TEST(priority_of_operators)
 {
 	ASSERT_OK("1.'0' + 1 > 10 && 1 + 2 != 0 || +0 == -3 + 2 && 0", "1");
+}
+
+TEST(state_is_reset_on_each_parsing)
+{
+	ASSERT_FAIL("1 1", PE_INVALID_EXPRESSION);
+	assert_true(is_prev_token_whitespace());
+	ASSERT_FAIL("", PE_INVALID_EXPRESSION);
+	assert_false(is_prev_token_whitespace());
+
+	static const function_t function_a = { "a", "adescr", {1,1}, &dummy };
+	assert_success(function_register(&function_a));
+
+	ASSERT_FAIL("a('a'", PE_INVALID_EXPRESSION);
+	assert_int_equal(VTYPE_ERROR, get_parsing_result().type);
+
+	function_reset_all();
+}
+
+static var_t
+dummy(const call_info_t *call_info)
+{
+	return var_from_str("");
 }
 
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
