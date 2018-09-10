vifm/vifm

commit def460353b81224f06ee974ac4fc5bad839ec2c6

Sort entries of :bmarks menu
Thanks to anonymous at Vifm Q2A site.

See https://q2a.vifm.info/299/how-can-i-order-bookmarks

src/menus/bmarks_menu.c
@@ -52,6 +52,11 @@ show_bmarks_menu(view_t *view, const char tags[], int go_on_single_match)
 		bmarks_find(tags, &bmarks_cb, &m);
 	}
 
+	if(m.len > 0)
+	{
+		qsort(m.items, m.len, sizeof(*m.items), &strossorter);
+	}
+
 	if(go_on_single_match && m.len == 1)
 	{
 		(void)menus_goto_file(&m, view, m.items[m.pos], 0);

commit 10d3e7a6e9543c1db8c4cb6ce0f75c82de3f4e64

Fix :bmarks menu entries referring to wrong files
Items were sorted, but their associated data wasn't.

Thanks to Jose Riha (a.k.a. jose1711).

Got broken by def460353b81224f06ee974ac4fc5bad839ec2c6:

    Sort entries of :bmarks menu

Fixes #364 on GitHub.

src/menus/bmarks_menu.c
@@ -25,7 +25,6 @@
 #include "../utils/path.h"
 #include "../utils/str.h"
 #include "../utils/string_array.h"
-#include "../utils/utils.h"
 #include "../bmarks.h"
 #include "../status.h"
 #include "menus.h"
@@ -53,11 +52,9 @@ show_bmarks_menu(view_t *view, const char tags[], int go_on_single_match)
 		bmarks_find(tags, &bmarks_cb, &m);
 	}
 
-	safe_qsort(m.items, m.len, sizeof(*m.items), &strossorter);
-
 	if(go_on_single_match && m.len == 1)
 	{
-		(void)menus_goto_file(&m, view, m.items[m.pos], 0);
+		(void)menus_goto_file(&m, view, m.data[m.pos], 0);
 		menus_reset_data(&m);
 		return curr_stats.save_msg;
 	}
@@ -85,6 +82,22 @@ bmarks_cb(const char path[], const char tags[], time_t timestamp, void *arg)
 
 	(void)add_to_string_array(&m->data, m->len, 1, path);
 	m->len = put_into_string_array(&m->items, m->len, line);
+
+	/* Insertion into sorted array items while simultaneously updating data
+	 * array. */
+	int i = m->len - 1;
+	char *data = m->data[i];
+	while(i > 0 && stroscmp(line, m->items[i - 1]) < 0)
+	{
+		m->items[i] = m->items[i - 1];
+		m->data[i] = m->data[i - 1];
+		--i;
+	}
+	if(i >= 0)
+	{
+		m->items[i] = line;
+		m->data[i] = data;
+	}
 }
 
 /* Callback that is called when menu item is selected.  Should return non-zero

tests/misc/menus_bmarks.c
@@ -169,6 +169,23 @@ TEST(bmgo_navigates_to_single_match)
 	assert_true(paths_are_equal(lwin.curr_dir, test_data));
 }
 
+TEST(sorting_updates_associated_data)
+{
+	assert_success(exec_commands("bmark! /c tagb", &lwin, CIT_COMMAND));
+	assert_success(exec_commands("bmark! /b tagb", &lwin, CIT_COMMAND));
+	assert_success(exec_commands("bmark! /a taga", &lwin, CIT_COMMAND));
+	assert_success(exec_commands("bmarks", &lwin, CIT_COMMAND));
+
+	assert_int_equal(3, menu_get_current()->len);
+	assert_string_equal("/a", menu_get_current()->data[0]);
+	assert_string_equal("/b", menu_get_current()->data[1]);
+	assert_string_equal("/c", menu_get_current()->data[2]);
+	assert_true(starts_with_lit(menu_get_current()->items[0], "/a"));
+	assert_true(starts_with_lit(menu_get_current()->items[1], "/b"));
+	assert_true(starts_with_lit(menu_get_current()->items[2], "/c"));
+	(void)vle_keys_exec(WK_ESC);
+}
+
 static int
 count_bmarks(void)
 {