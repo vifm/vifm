vifm/vifm

commit 8513559bebcd485f1185e92d88ffe8688519aca1

Fix :cmd abbrevs resolution with !/? as custsep
They could be resolved incorrectly, e.g.

    :s!a!b!

was interpreted as `:shell` instead of `:substitute`.

Case of removed if-statement is covered by the new if-statement.

src/engine/cmds.c
@@ -712,15 +712,15 @@ get_cmd_name(const char cmd[], char buf[], size_t buf_len)
 		{
 			if(cmp == 0)
 			{
-				/* Check for user-defined command that ends with the char. */
-				if(cur->type == USER_CMD && cur->name[strlen(cur->name) - 1] == *t)
+				/* Complete match (there are no duplicates in the list). */
+				if(cur->name[len] == '\0')
 				{
 					strncpy(buf, cur->name, buf_len);
 					break;
 				}
-				/* Or builtin abbreviation that supports the mark. */
-				if(cur->type == BUILTIN_ABBR &&
-						((*t == '!' && cur->emark) || (*t == '?' && cur->qmark)))
+				/* Check for user-defined command that ends with the char ('!' or
+				 * '?', see above). */
+				if(cur->type == USER_CMD && cur->name[strlen(cur->name) - 1] == *t)
 				{
 					strncpy(buf, cur->name, buf_len);
 					break;

tests/commands/command_name.c
@@ -7,10 +7,14 @@
 
 static int goto_cmd(const cmd_info_t *cmd_info);
 static int delete_cmd(const cmd_info_t *cmd_info);
+static int shell_cmd(const cmd_info_t *cmd_info);
+static int substitute_cmd(const cmd_info_t *cmd_info);
 
 extern cmds_conf_t cmds_conf;
 
 static cmd_info_t cmdi;
+static int shell_called;
+static int substitute_called;
 
 static const cmd_add_t commands[] = {
 	{ .name = "",             .abbr = NULL,  .id = -1,      .descr = "descr",
@@ -19,6 +23,12 @@ static const cmd_add_t commands[] = {
 	{ .name = "delete",       .abbr = "d",   .id = -1,      .descr = "descr",
 	  .flags = HAS_EMARK | HAS_RANGE,
 	  .handler = &delete_cmd, .min_args = 0, .max_args = 1, },
+	{ .name = "shell",        .abbr = "sh",  .id = -1,      .descr = "descr",
+	  .flags = HAS_EMARK | HAS_QMARK_WITH_ARGS,
+	  .handler = &shell_cmd,  .min_args = 0, .max_args = 1, },
+	{ .name = "substitute",   .abbr = "s",   .id = -1,      .descr = "descr",
+	  .flags = HAS_REGEXP_ARGS | HAS_CUST_SEP,
+	  .handler = &substitute_cmd, .min_args = 0, .max_args = 3, },
 };
 
 static int
@@ -31,6 +41,20 @@ static int
 delete_cmd(const cmd_info_t *cmd_info)
 {
 	cmdi = *cmd_info;
+	return 0;
+}
+
+static int
+shell_cmd(const cmd_info_t *cmd_info)
+{
+	shell_called = 1;
+	return 0;
+}
+
+static int
+substitute_cmd(const cmd_info_t *cmd_info)
+{
+	substitute_called = 1;
 	return 0;
 }
 
@@ -108,6 +132,24 @@ TEST(udf_bang_abbr)
 	assert_int_equal(0, execute_cmd("UDF?"));
 	assert_int_equal(0, execute_cmd("UD?"));
 	assert_int_equal(0, execute_cmd("U?"));
+}
+
+TEST(cust_sep_is_resolved_correctly)
+{
+	shell_called = substitute_called = 0;
+	assert_success(execute_cmd("s!a!b!g"));
+	assert_false(shell_called);
+	assert_true(substitute_called);
+
+	shell_called = substitute_called = 0;
+	assert_success(execute_cmd("s?a?b?g"));
+	assert_false(shell_called);
+	assert_true(substitute_called);
+
+	shell_called = substitute_called = 0;
+	assert_success(execute_cmd("s:a:b:g"));
+	assert_false(shell_called);
+	assert_true(substitute_called);
 }
 
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */

commit a2070c5df330740dbb3feeee8bb5439019790c60

Fix registering user-defined command like :tree!
They are allowed, because :tree! doesn't exist.

Got broken by 8513559bebcd485f1185e92d88ffe8688519aca1.

    Fix :cmd abbrevs resolution with !/? as custsep

    They could be resolved incorrectly, e.g.

        :s!a!b!

    was interpreted as `:shell` instead of `:substitute`.

    Case of removed if-statement is covered by the new if-statement.

> Case of removed if-statement is covered by the new if-statement.

New if-statement covered too much.

src/engine/cmds.c
@@ -712,8 +712,8 @@ get_cmd_name(const char cmd[], char buf[], size_t buf_len)
 		{
 			if(cmp == 0)
 			{
-				/* Complete match (there are no duplicates in the list). */
-				if(cur->name[len] == '\0')
+				/* Complete match for a builtin with a custom separator. */
+				if(cur->cust_sep && cur->name[len] == '\0')
 				{
 					strncpy(buf, cur->name, buf_len);
 					break;
@@ -725,13 +725,21 @@ get_cmd_name(const char cmd[], char buf[], size_t buf_len)
 					strncpy(buf, cur->name, buf_len);
 					break;
 				}
+				/* Or builtin abbreviation that supports the mark. */
+				if(cur->type == BUILTIN_ABBR &&
+						((*t == '!' && cur->emark) || (*t == '?' && cur->qmark)))
+				{
+					strncpy(buf, cur->name, buf_len);
+					break;
+				}
 			}
 			cur = cur->next;
 		}
-		/* For builtin commands, the char is not part of the name. */
+
 		if(cur != NULL && cur->type == USER_CMD &&
 				strncmp(cur->name, buf, len) == 0)
 		{
+			/* For user-defined commands, the char is part of the name. */
 			++t;
 		}
 	}

tests/commands/user_cmds.c
@@ -7,6 +7,7 @@
 #include "suite.h"
 
 static int move_cmd(const cmd_info_t *cmd_info);
+static int dummy_cmd(const cmd_info_t *cmd_info);
 static int usercmd_cmd(const cmd_info_t *cmd_info);
 
 extern cmds_conf_t cmds_conf;
@@ -14,6 +15,7 @@ extern cmds_conf_t cmds_conf;
 cmd_info_t user_cmd_info;
 
 static int move_cmd_called;
+static int dummy_cmd_called;
 
 SETUP()
 {
@@ -59,6 +61,26 @@ TEST(does_not_clash_with_builtins)
 	assert_true(move_cmd_called);
 }
 
+TEST(udc_can_add_suffix_to_builtin_cmd)
+{
+	static const cmd_add_t dummy = {
+	  .name = "dummy",       .abbr = NULL,   .id = -1,          .descr = "descr",
+	  .flags = 0,
+	  .handler = &dummy_cmd, .min_args = 0,  .max_args = 0,
+	};
+
+	user_cmd_handler = &usercmd_cmd;
+
+	add_builtin_commands(&dummy, 1);
+
+	assert_success(execute_cmd("command dummy! move"));
+	move_cmd_called = 0;
+	dummy_cmd_called = 0;
+	assert_success(execute_cmd("dummy!"));
+	assert_true(move_cmd_called);
+	assert_false(dummy_cmd_called);
+}
+
 TEST(cant_redefine_builtin_with_suffix)
 {
 	assert_failure(execute_cmd("command move! a"));
@@ -73,6 +95,12 @@ TEST(self_deletion_does_not_have_use_after_free)
 	assert_success(execute_cmd("command suicidetwo suicideone"));
 	assert_success(execute_cmd("command suicidethree suicidetwo"));
 	assert_success(execute_cmd("suicidethree"));
+}
+
+static int
+dummy_cmd(const cmd_info_t *cmd_info)
+{
+	return 0;
 }
 
 static int