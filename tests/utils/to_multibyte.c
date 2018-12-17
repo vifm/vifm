b606703e0b67fce9396c5f45d1a301d75f266ade
xaizek <xaizek@posteo.net>
2018-12-18 00:43:59 +0200

    Correctly handle conversion fail in to_multibyte()
    It used to allocate 0 bytes and this wasn't equivalent to an empty
    string.

---
+++ tests/utils/to_multibyte.c
@@ -0,0 +1,47 @@
+#include <stic.h>
+
+#include <locale.h> /* LC_ALL setlocale() */
+#include <stdlib.h> /* free() */
+
+#include "../../src/utils/str.h"
+
+#include "utils.h"
+
+SETUP_ONCE()
+{
+	try_enable_utf8_locale();
+}
+
+TEST(empty_string_turns_into_empty_string)
+{
+	char *const narrow = to_multibyte(L"");
+	assert_string_equal("", narrow);
+	free(narrow);
+}
+
+TEST(non_empty_string_turns_into_non_empty_string)
+{
+	char *const narrow = to_multibyte(L"abcde");
+	assert_string_equal("abcde", narrow);
+	free(narrow);
+}
+
+TEST(non_empty_non_latin_string_turns_into_non_empty_string, IF(utf8_locale))
+{
+	char *const narrow = to_multibyte(L"абвгд");
+	assert_string_equal("абвгд", narrow);
+	free(narrow);
+}
+
+TEST(conversion_failure_is_detected, IF(not_windows))
+{
+	/* On Windows narrow locale doesn't really matter because UTF-8 is used
+	 * internally by vifm. */
+	(void)setlocale(LC_ALL, "C");
+	char *const narrow = to_multibyte(L"абвгд");
+	assert_string_equal(NULL, narrow);
+	free(narrow);
+}
+
+/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
+/* vim: set cinoptions+=t0 filetype=c : */
