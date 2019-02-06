0e9ee2cea09341946fb4ea50258763998f7d93e6
xaizek <xaizek@posteo.net>
2019-02-06 20:14:48 +0200

    Fix box of the menu being drawing in wrong colors

--- src/menus/menus.c
+++ src/menus/menus.c
@@ -580,6 +580,8 @@ draw_menu_frame(const menu_state_t *m)
 	char *const ellipsed = right_ellipsis(title, title_len, curr_stats.ellipsis);
 	free(title);
 
+	ui_set_attr(menu_win, &cfg.cs.color[WIN_COLOR], cfg.cs.pair[WIN_COLOR]);
+
 	box(menu_win, 0, 0);
 	wattron(menu_win, A_BOLD);
 	checked_wmove(menu_win, 0, 3);
