vifm/vifm

commit 1a5a0571c37cdc81fcfeb7cba763844cd9591320

Fix item not being redrawn on selection toggle
Using `t` key didn't update the state of the item on the screen.

It was originally fixed by 580ea80e94f284fb27f25b90d81112ce9fc8de5e:

    Fix cursor not being redrawn on t key (selection)

But its effect was undone by 982a22924a8f97dfe9fa7e74275fd8d0391b20f8:

    Fix quickview not being updated on file replace

    Here file replacement means that cursor position on the screen
    remained the same, but file under the cursor has changed.

    The fix is to use a more robust cache, which is now a combination of
    position, current line (these two account for top as well) and file
    path.

src/ui/fileview.c
@@ -826,6 +826,16 @@ fview_cursor_redraw(view_t *view)
 	 * changed, so just let fview_position_updated() deal with it.  With a cache
 	 * of last position, it should be fine. */
 	fview_position_updated(view);
+
+	/* Always redrawing the cell won't hurt and will account for the case when
+	 * selection state of item under the cursor has changed. */
+	if(!ui_view_displays_columns(view))
+	{
+		/* Inactive cell in ls-like view usually takes less space than an active
+		 * one.  Need to clear the cell before drawing over it. */
+		redraw_cell(view, view->top_line, view->curr_line, 0);
+	}
+	redraw_cell(view, view->top_line, view->curr_line, 1);
 }
 
 void

tests/misc/integration.c
@@ -12,6 +12,8 @@
 #include "../../src/int/vim.h"
 #include "../../src/modes/modes.h"
 #include "../../src/modes/wk.h"
+#include "../../src/ui/column_view.h"
+#include "../../src/ui/fileview.h"
 #include "../../src/utils/fs.h"
 #include "../../src/utils/str.h"
 #include "../../src/event_loop.h"
@@ -203,6 +205,8 @@ TEST(externally_edited_local_filter_is_applied, IF(not_windows))
 	curr_view = &lwin;
 	other_view = &rwin;
 	view_setup(&lwin);
+	fview_setup();
+	lwin.columns = columns_create();
 
 	init_modes();
 	opt_handlers_setup();
@@ -238,9 +242,13 @@ TEST(externally_edited_local_filter_is_applied, IF(not_windows))
 	update_string(&cfg.shell, NULL);
 	assert_success(unlink(path));
 
+	columns_free(lwin.columns);
+	lwin.columns = NULL;
+	view_teardown(&lwin);
+
 	vle_keys_reset();
 	opt_handlers_teardown();
-	view_teardown(&lwin);
+	columns_teardown();
 
 	restore_cwd(saved_cwd);
 }

commit 31171936614a46e18927da676aea50a232af663e

Fix inactive mark (*) not being drawn sometimes
Caused by 1a5a0571c37cdc81fcfeb7cba763844cd9591320:

    Fix item not being redrawn on selection toggle

    Using `t` key didn't update the state of the item on the screen.

src/ui/fileview.c
@@ -829,13 +829,20 @@ fview_cursor_redraw(view_t *view)
 
 	/* Always redrawing the cell won't hurt and will account for the case when
 	 * selection state of item under the cursor has changed. */
-	if(!ui_view_displays_columns(view))
+	if(view == other_view)
 	{
-		/* Inactive cell in ls-like view usually takes less space than an active
-		 * one.  Need to clear the cell before drawing over it. */
-		redraw_cell(view, view->top_line, view->curr_line, 0);
+		fview_draw_inactive_cursor(view);
 	}
-	redraw_cell(view, view->top_line, view->curr_line, 1);
+	else
+	{
+		if(!ui_view_displays_columns(view))
+		{
+			/* Inactive cell in ls-like view usually takes less space than an active
+			 * one.  Need to clear the cell before drawing over it. */
+			redraw_cell(view, view->top_line, view->curr_line, 0);
+		}
+		redraw_cell(view, view->top_line, view->curr_line, 1);
+	}
 }
 
 void