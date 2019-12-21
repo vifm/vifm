77596ab96d98adeef4194e9ecaa88d2751a6ab79
xaizek <xaizek@posteo.net>
2019-12-21 14:35:56 +0200

    Don't special case COM_GOTO in cmd_core.c:post()
    There is a dedicated way to specify that selection should be preserved.

--- src/cmd_core.c
+++ src/cmd_core.c
@@ -371,7 +371,7 @@ commands_escape_for_insertion(const char cmd_line[], int pos, const char str[])
 static void
 post(int id)
 {
-	if(id != COM_GOTO && curr_view->selected_files > 0 && !keep_view_selection)
+	if(curr_view->selected_files > 0 && !keep_view_selection)
 	{
 		flist_sel_stash(curr_view);
 	}
