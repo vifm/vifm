vifm/vifm

commit 1a5a0571c37cdc81fcfeb7cba763844cd9591320

Fix item not being redrawn on selection toggle
Using `t` key didn't update the state of the item on the screen.

It was originally fixed by 580ea80e94f284fb27f25b90d81112ce9fc8de5e:

    Fix cursor not being redrawn on t key (selection)

But its effect was undone by 982a22924a8f97dfe9fa7e74275fd8d0391b20f8:

    Fix quickview not being updated on file replace

    Here file replacement means that cursor position on the screen
    remained the same, but file under the cursor has changed.

    The fix is to use a more robust cache, which is now a combination of
    position, current line (these two account for top as well) and file
    path.

src/ui/fileview.c
@@ -826,6 +826,16 @@ fview_cursor_redraw(view_t *view)
 	 * changed, so just let fview_position_updated() deal with it.  With a cache
 	 * of last position, it should be fine. */
 	fview_position_updated(view);
+
+	/* Always redrawing the cell won't hurt and will account for the case when
+	 * selection state of item under the cursor has changed. */
+	if(!ui_view_displays_columns(view))
+	{
+		/* Inactive cell in ls-like view usually takes less space than an active
+		 * one.  Need to clear the cell before drawing over it. */
+		redraw_cell(view, view->top_line, view->curr_line, 0);
+	}
+	redraw_cell(view, view->top_line, view->curr_line, 1);
 }
 
 void

tests/misc/integration.c
@@ -12,6 +12,8 @@
 #include "../../src/int/vim.h"
 #include "../../src/modes/modes.h"
 #include "../../src/modes/wk.h"
+#include "../../src/ui/column_view.h"
+#include "../../src/ui/fileview.h"
 #include "../../src/utils/fs.h"
 #include "../../src/utils/str.h"
 #include "../../src/event_loop.h"
@@ -203,6 +205,8 @@ TEST(externally_edited_local_filter_is_applied, IF(not_windows))
 	curr_view = &lwin;
 	other_view = &rwin;
 	view_setup(&lwin);
+	fview_setup();
+	lwin.columns = columns_create();
 
 	init_modes();
 	opt_handlers_setup();
@@ -238,9 +242,13 @@ TEST(externally_edited_local_filter_is_applied, IF(not_windows))
 	update_string(&cfg.shell, NULL);
 	assert_success(unlink(path));
 
+	columns_free(lwin.columns);
+	lwin.columns = NULL;
+	view_teardown(&lwin);
+
 	vle_keys_reset();
 	opt_handlers_teardown();
-	view_teardown(&lwin);
+	columns_teardown();
 
 	restore_cwd(saved_cwd);
 }

commit 42220b07e61c008e44a8ed73948b46e46bcb7f89

Fix wrong cursor position after initialization
Change 1a5a0571c37cdc81fcfeb7cba763844cd9591320 broke the cursor
position on a freshly opened instance (and also after switching panels).
Previously, the cursor was always correctly positioned on the selected
entry. With said change it is located one line below.
I am not familiar with the code base but reordering the call to
fview_position_updated solves the problem.

src/ui/fileview.c
@@ -822,11 +822,6 @@ redraw_current_view(void)
 void
 fview_cursor_redraw(view_t *view)
 {
-	/* fview_cursor_redraw() is also called in situations when file list has
-	 * changed, so just let fview_position_updated() deal with it.  With a cache
-	 * of last position, it should be fine. */
-	fview_position_updated(view);
-
 	/* Always redrawing the cell won't hurt and will account for the case when
 	 * selection state of item under the cursor has changed. */
 	if(view == other_view)
@@ -843,6 +838,11 @@ fview_cursor_redraw(view_t *view)
 		}
 		redraw_cell(view, view->top_line, view->curr_line, 1);
 	}
+
+	/* fview_cursor_redraw() is also called in situations when file list has
+	 * changed, so just let fview_position_updated() deal with it.  With a cache
+	 * of last position, it should be fine. */
+	fview_position_updated(view);
 }
 
 void