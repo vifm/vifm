vifm/vifm

commit 092af4ac7e9abb2d867e5345d59aff2fac28ec4c

Fix treating characters like š and ć on input
That is characters with low Unicode values (just above 0x100).
They were confused with functional keys on input.

Thanks to granderil.

See
http://q2a.vifm.info/208/ignores-scaron-cacute-backspace-while-trying-rename-files.

src/modes/cmdline.c
@@ -288,15 +288,15 @@ static keys_add_info_t builtin_cmds[] = {
 	{{ALT_PERIOD},    {{&cmd_meta_dot}, .descr = "start/continue last arg completion"}},
 #endif
 #ifdef ENABLE_EXTENDED_KEYS
-	{{KEY_BACKSPACE}, {{&cmd_ctrl_h}, .descr = "remove char to the left"}},
-	{{KEY_DOWN},      {{&cmd_down},   .descr = "prefix-complete next history item"}},
-	{{KEY_UP},        {{&cmd_up},     .descr = "prefix-complete previous history item"}},
-	{{KEY_LEFT},      {{&cmd_left},   .descr = "move cursor to the left"}},
-	{{KEY_RIGHT},     {{&cmd_right},  .descr = "move cursor to the right"}},
-	{{KEY_HOME},      {{&cmd_home},   .descr = "move cursor to the beginning"}},
-	{{KEY_END},       {{&cmd_end},    .descr = "move cursor to the end"}},
-	{{KEY_DC},        {{&cmd_delete}, .descr = "delete current character"}},
-	{{KEY_BTAB},      {{&cmd_shift_tab}, .descr = "complete in reverse order"}},
+	{{K(KEY_BACKSPACE)}, {{&cmd_ctrl_h}, .descr = "remove char to the left"}},
+	{{K(KEY_DOWN)},      {{&cmd_down},   .descr = "prefix-complete next history item"}},
+	{{K(KEY_UP)},        {{&cmd_up},     .descr = "prefix-complete previous history item"}},
+	{{K(KEY_LEFT)},      {{&cmd_left},   .descr = "move cursor to the left"}},
+	{{K(KEY_RIGHT)},     {{&cmd_right},  .descr = "move cursor to the right"}},
+	{{K(KEY_HOME)},      {{&cmd_home},   .descr = "move cursor to the beginning"}},
+	{{K(KEY_END)},       {{&cmd_end},    .descr = "move cursor to the end"}},
+	{{K(KEY_DC)},        {{&cmd_delete}, .descr = "delete current character"}},
+	{{K(KEY_BTAB)},      {{&cmd_shift_tab}, .descr = "complete in reverse order"}},
 #endif /* ENABLE_EXTENDED_KEYS */
 };
 
@@ -332,7 +332,7 @@ def_handler(wchar_t key)
 
 	/* There is no need for resetting completion because of terminal dimensions
 	 * change (note also that iswprint(KEY_RESIZE) might return false). */
-	if(key == KEY_RESIZE)
+	if(key == K(KEY_RESIZE))
 	{
 		return 0;
 	}

src/modes/view.c
@@ -30,6 +30,7 @@
 #include <stdlib.h> /* free() */
 
 #include "../cfg/config.h"
+#include "../compat/curses.h"
 #include "../compat/fs_limits.h"
 #include "../compat/os.h"
 #include "../compat/reallocarray.h"
@@ -281,16 +282,16 @@ static keys_add_info_t builtin_cmds[] = {
 	{{ALT_V},          {{&cmd_b}, .descr = "scroll page up"}},
 #endif
 #ifdef ENABLE_EXTENDED_KEYS
-	{{WC_C_w, KEY_BACKSPACE}, {{&cmd_ctrl_wh}, .descr = "go to left window"}},
-	{{KEY_PPAGE},             {{&cmd_b}, .descr = "scroll page up"}},
-	{{KEY_NPAGE},             {{&cmd_f}, .descr = "scroll page down"}},
-	{{KEY_DOWN},              {{&cmd_j}, .descr = "scroll one line down"}},
-	{{KEY_UP},                {{&cmd_k}, .descr = "scroll one line up"}},
-	{{KEY_HOME},              {{&cmd_g}, .descr = "scroll to the beginning"}},
-	{{KEY_END},               {{&cmd_G}, .descr = "scroll to the end"}},
-	{{KEY_BTAB},              {{&cmd_q}, .descr = "leave view mode"}},
+	{{WC_C_w, K(KEY_BACKSPACE)}, {{&cmd_ctrl_wh}, .descr = "go to left window"}},
+	{{K(KEY_PPAGE)},             {{&cmd_b}, .descr = "scroll page up"}},
+	{{K(KEY_NPAGE)},             {{&cmd_f}, .descr = "scroll page down"}},
+	{{K(KEY_DOWN)},              {{&cmd_j}, .descr = "scroll one line down"}},
+	{{K(KEY_UP)},                {{&cmd_k}, .descr = "scroll one line up"}},
+	{{K(KEY_HOME)},              {{&cmd_g}, .descr = "scroll to the beginning"}},
+	{{K(KEY_END)},               {{&cmd_G}, .descr = "scroll to the end"}},
+	{{K(KEY_BTAB)},              {{&cmd_q}, .descr = "leave view mode"}},
 #else
-	{WK_ESCAPE L"[Z",         {{&cmd_q}, .descr = "leave view mode"}},
+	{WK_ESCAPE L"[Z",            {{&cmd_q}, .descr = "leave view mode"}},
 #endif /* ENABLE_EXTENDED_KEYS */
 };
 

THANKS
@@ -43,6 +43,7 @@ Flaviu Tamas (flaviut)
 Florian Baumann (derflob)
 fogine
 geo909
+granderil
 greye
 gtors
 Hans Kristian Otnes Berge

src/compat/curses.h
@@ -23,6 +23,13 @@
 
 #include <wchar.h> /* wchar_t wint_t */
 
+/* Moves curses functional keys (KEY_*) to Unicode Private Use Area starting
+ * from U+E000, which shouldn't clash with anything except for maybe iconic
+ * fonts, but by convention they start from U+F000, so even that shouldn't
+ * happen. (Solution that guarantees no clashes ever is possible, but very
+ * inconvenient for implementation as it needs more than just wchar_t.) */
+#define K(x) ((wchar_t)((wint_t)0xe000 + (x)))
+
 /* OpenBSD has perverted ncurses library, which has stubs with infinite loops
  * instead of real wide functions.  As there is only a couple of wide functions
  * in use, they can be emulated. */

src/event_loop.c
@@ -150,7 +150,7 @@ event_loop(const int *quit)
 				continue;
 			}
 
-			if(got_input && c == KEY_RESIZE)
+			if(got_input && c == K(KEY_RESIZE))
 			{
 				modes_redraw();
 				continue;
@@ -332,8 +332,8 @@ ensure_term_is_ready(void)
  *  - checks for new IPC messages;
  *  - checks whether contents of displayed directories changed;
  *  - redraws UI if requested.
- * Returns KEY_CODE_YES for functional keys, OK for wide character and ERR
- * otherwise (e.g. after timeout). */
+ * Returns KEY_CODE_YES for functional keys (preprocesses *c in this case), OK
+ * for wide character and ERR otherwise (e.g. after timeout). */
 static int
 get_char_async_loop(WINDOW *win, wint_t *c, int timeout)
 {
@@ -375,6 +375,10 @@ get_char_async_loop(WINDOW *win, wint_t *c, int timeout)
 			result = compat_wget_wch(win, c);
 			if(result != ERR)
 			{
+				if(result == KEY_CODE_YES)
+				{
+					*c = K(*c);
+				}
 				return result;
 			}
 

src/modes/dialogs/attr_dialog_nix.c
@@ -31,6 +31,7 @@
 #include <stdio.h> /* snprintf() */
 #include <string.h> /* strncat() strlen() */
 
+#include "../../compat/curses.h"
 #include "../../compat/fs_limits.h"
 #include "../../engine/keys.h"
 #include "../../engine/mode.h"
@@ -101,11 +102,11 @@ static keys_add_info_t builtin_cmds[] = {
 	{WK_q,      {{&cmd_ctrl_c}, .descr = "close the dialog"}},
 	{WK_t,      {{&cmd_space},  .descr = "toggle current item"}},
 #ifdef ENABLE_EXTENDED_KEYS
-	{{KEY_HOME},  {{&cmd_gg},     .descr = "go to the first item"}},
-	{{KEY_END},   {{&cmd_G},      .descr = "go to the last item"}},
-	{{KEY_UP},    {{&cmd_k},      .descr = "go to item above"}},
-	{{KEY_DOWN},  {{&cmd_j},      .descr = "go to item below"}},
-	{{KEY_RIGHT}, {{&cmd_return}, .descr = "update permissions"}},
+	{{K(KEY_HOME)},  {{&cmd_gg},     .descr = "go to the first item"}},
+	{{K(KEY_END)},   {{&cmd_G},      .descr = "go to the last item"}},
+	{{K(KEY_UP)},    {{&cmd_k},      .descr = "go to item above"}},
+	{{K(KEY_DOWN)},  {{&cmd_j},      .descr = "go to item below"}},
+	{{K(KEY_RIGHT)}, {{&cmd_return}, .descr = "update permissions"}},
 #endif /* ENABLE_EXTENDED_KEYS */
 };
 

src/modes/dialogs/attr_dialog_win.c
@@ -24,6 +24,7 @@
 #include <stddef.h> /* NULL size_t */
 #include <string.h> /* strncat() strlen() */
 
+#include "../../compat/curses.h"
 #include "../../compat/fs_limits.h"
 #include "../../engine/keys.h"
 #include "../../engine/mode.h"
@@ -131,11 +132,11 @@ static keys_add_info_t builtin_cmds[] = {
 	{WK_q,      {{&cmd_ctrl_c}, .descr = "close the dialog"}},
 	{WK_t,      {{&cmd_space},  .descr = "toggle current item"}},
 #ifdef ENABLE_EXTENDED_KEYS
-	{{KEY_HOME},  {{&cmd_gg},     .descr = "go to the first item"}},
-	{{KEY_END},   {{&cmd_G},      .descr = "go to the last item"}},
-	{{KEY_UP},    {{&cmd_k},      .descr = "go to item above"}},
-	{{KEY_DOWN},  {{&cmd_j},      .descr = "go to item below"}},
-	{{KEY_RIGHT}, {{&cmd_return}, .descr = "update permissions"}},
+	{{K(KEY_HOME)},  {{&cmd_gg},     .descr = "go to the first item"}},
+	{{K(KEY_END)},   {{&cmd_G},      .descr = "go to the last item"}},
+	{{K(KEY_UP)},    {{&cmd_k},      .descr = "go to item above"}},
+	{{K(KEY_DOWN)},  {{&cmd_j},      .descr = "go to item below"}},
+	{{K(KEY_RIGHT)}, {{&cmd_return}, .descr = "update permissions"}},
 #endif /* ENABLE_EXTENDED_KEYS */
 };
 

commit e28ad6250ca47383303bbc37a55e6716eb748213

Fix Alt key combinations being broken on Windows
Since v0.9 broken by 092af4ac7e9abb2d867e5345d59aff2fac28ec4c:

    Fix treating characters like š and ć on input

    That is characters with low Unicode values (just above 0x100).
    They were confused with functional keys on input.

Special keys now need to be wrapped by `K()` macro and while it was done
for KEY_*, it wasn't done for ALT_*.

src/bracket_notation.c
@@ -314,58 +314,58 @@ static key_pair_t key_pairs[] = {
 	{ L"<c-m-y>",    L"\033"L"\x19"       },
 	{ L"<c-m-z>",    L"\033"L"\x1a"       },
 #else
-	{ L"<a-a>",      { ALT_A }            },
-	{ L"<a-b>",      { ALT_B }            },
-	{ L"<a-c>",      { ALT_C }            },
-	{ L"<a-d>",      { ALT_D }            },
-	{ L"<a-e>",      { ALT_E }            },
-	{ L"<a-f>",      { ALT_F }            },
-	{ L"<a-g>",      { ALT_G }            },
-	{ L"<a-h>",      { ALT_H }            },
-	{ L"<a-i>",      { ALT_I }            },
-	{ L"<a-j>",      { ALT_J }            },
-	{ L"<a-k>",      { ALT_K }            },
-	{ L"<a-l>",      { ALT_L }            },
-	{ L"<a-m>",      { ALT_M }            },
-	{ L"<a-n>",      { ALT_N }            },
-	{ L"<a-o>",      { ALT_O }            },
-	{ L"<a-p>",      { ALT_P }            },
-	{ L"<a-q>",      { ALT_Q }            },
-	{ L"<a-r>",      { ALT_R }            },
-	{ L"<a-s>",      { ALT_S }            },
-	{ L"<a-t>",      { ALT_T }            },
-	{ L"<a-u>",      { ALT_U }            },
-	{ L"<a-v>",      { ALT_V }            },
-	{ L"<a-w>",      { ALT_W }            },
-	{ L"<a-x>",      { ALT_X }            },
-	{ L"<a-y>",      { ALT_Y }            },
-	{ L"<a-z>",      { ALT_Z }            },
-	{ L"<m-a>",      { ALT_A }            },
-	{ L"<m-b>",      { ALT_B }            },
-	{ L"<m-c>",      { ALT_C }            },
-	{ L"<m-d>",      { ALT_D }            },
-	{ L"<m-e>",      { ALT_E }            },
-	{ L"<m-f>",      { ALT_F }            },
-	{ L"<m-g>",      { ALT_G }            },
-	{ L"<m-h>",      { ALT_H }            },
-	{ L"<m-i>",      { ALT_I }            },
-	{ L"<m-j>",      { ALT_J }            },
-	{ L"<m-k>",      { ALT_K }            },
-	{ L"<m-l>",      { ALT_L }            },
-	{ L"<m-m>",      { ALT_M }            },
-	{ L"<m-n>",      { ALT_N }            },
-	{ L"<m-o>",      { ALT_O }            },
-	{ L"<m-p>",      { ALT_P }            },
-	{ L"<m-q>",      { ALT_Q }            },
-	{ L"<m-r>",      { ALT_R }            },
-	{ L"<m-s>",      { ALT_S }            },
-	{ L"<m-t>",      { ALT_T }            },
-	{ L"<m-u>",      { ALT_U }            },
-	{ L"<m-v>",      { ALT_V }            },
-	{ L"<m-w>",      { ALT_W }            },
-	{ L"<m-x>",      { ALT_X }            },
-	{ L"<m-y>",      { ALT_Y }            },
-	{ L"<m-z>",      { ALT_Z }            },
+	{ L"<a-a>",      { K(ALT_A) }         },
+	{ L"<a-b>",      { K(ALT_B) }         },
+	{ L"<a-c>",      { K(ALT_C) }         },
+	{ L"<a-d>",      { K(ALT_D) }         },
+	{ L"<a-e>",      { K(ALT_E) }         },
+	{ L"<a-f>",      { K(ALT_F) }         },
+	{ L"<a-g>",      { K(ALT_G) }         },
+	{ L"<a-h>",      { K(ALT_H) }         },
+	{ L"<a-i>",      { K(ALT_I) }         },
+	{ L"<a-j>",      { K(ALT_J) }         },
+	{ L"<a-k>",      { K(ALT_K) }         },
+	{ L"<a-l>",      { K(ALT_L) }         },
+	{ L"<a-m>",      { K(ALT_M) }         },
+	{ L"<a-n>",      { K(ALT_N) }         },
+	{ L"<a-o>",      { K(ALT_O) }         },
+	{ L"<a-p>",      { K(ALT_P) }         },
+	{ L"<a-q>",      { K(ALT_Q) }         },
+	{ L"<a-r>",      { K(ALT_R) }         },
+	{ L"<a-s>",      { K(ALT_S) }         },
+	{ L"<a-t>",      { K(ALT_T) }         },
+	{ L"<a-u>",      { K(ALT_U) }         },
+	{ L"<a-v>",      { K(ALT_V) }         },
+	{ L"<a-w>",      { K(ALT_W) }         },
+	{ L"<a-x>",      { K(ALT_X) }         },
+	{ L"<a-y>",      { K(ALT_Y) }         },
+	{ L"<a-z>",      { K(ALT_Z) }         },
+	{ L"<m-a>",      { K(ALT_A) }         },
+	{ L"<m-b>",      { K(ALT_B) }         },
+	{ L"<m-c>",      { K(ALT_C) }         },
+	{ L"<m-d>",      { K(ALT_D) }         },
+	{ L"<m-e>",      { K(ALT_E) }         },
+	{ L"<m-f>",      { K(ALT_F) }         },
+	{ L"<m-g>",      { K(ALT_G) }         },
+	{ L"<m-h>",      { K(ALT_H) }         },
+	{ L"<m-i>",      { K(ALT_I) }         },
+	{ L"<m-j>",      { K(ALT_J) }         },
+	{ L"<m-k>",      { K(ALT_K) }         },
+	{ L"<m-l>",      { K(ALT_L) }         },
+	{ L"<m-m>",      { K(ALT_M) }         },
+	{ L"<m-n>",      { K(ALT_N) }         },
+	{ L"<m-o>",      { K(ALT_O) }         },
+	{ L"<m-p>",      { K(ALT_P) }         },
+	{ L"<m-q>",      { K(ALT_Q) }         },
+	{ L"<m-r>",      { K(ALT_R) }         },
+	{ L"<m-s>",      { K(ALT_S) }         },
+	{ L"<m-t>",      { K(ALT_T) }         },
+	{ L"<m-u>",      { K(ALT_U) }         },
+	{ L"<m-v>",      { K(ALT_V) }         },
+	{ L"<m-w>",      { K(ALT_W) }         },
+	{ L"<m-x>",      { K(ALT_X) }         },
+	{ L"<m-y>",      { K(ALT_Y) }         },
+	{ L"<m-z>",      { K(ALT_Z) }         },
 #endif
 	{ L"<del>",      L"\177"              },
 #ifdef ENABLE_EXTENDED_KEYS

src/modes/cmdline.c
@@ -279,10 +279,10 @@ static keys_add_info_t builtin_cmds[] = {
 	{WK_ESC WK_f,     {{&cmd_meta_f}, .descr = "move cursor to next word"}},
 	{WK_ESC WK_DOT,   {{&cmd_meta_dot}, .descr = "start/continue last arg completion"}},
 #else
-	{{ALT_B},         {{&cmd_meta_b}, .descr = "move cursor to previous word"}},
-	{{ALT_D},         {{&cmd_meta_d}, .descr = "remove next word"}},
-	{{ALT_F},         {{&cmd_meta_f}, .descr = "move cursor to next word"}},
-	{{ALT_PERIOD},    {{&cmd_meta_dot}, .descr = "start/continue last arg completion"}},
+	{{K(ALT_B)},      {{&cmd_meta_b}, .descr = "move cursor to previous word"}},
+	{{K(ALT_D)},      {{&cmd_meta_d}, .descr = "remove next word"}},
+	{{K(ALT_F)},      {{&cmd_meta_f}, .descr = "move cursor to next word"}},
+	{{K(ALT_PERIOD)}, {{&cmd_meta_dot}, .descr = "start/continue last arg completion"}},
 #endif
 #ifdef ENABLE_EXTENDED_KEYS
 	{{K(KEY_BACKSPACE)}, {{&cmd_ctrl_h}, .descr = "remove char to the left"}},

src/modes/view.c
@@ -278,7 +278,7 @@ static keys_add_info_t builtin_cmds[] = {
 #ifndef __PDCURSES__
 	{WK_ALT WK_v,      {{&cmd_b}, .descr = "scroll page up"}},
 #else
-	{{ALT_V},          {{&cmd_b}, .descr = "scroll page up"}},
+	{{K(ALT_V)},       {{&cmd_b}, .descr = "scroll page up"}},
 #endif
 #ifdef ENABLE_EXTENDED_KEYS
 	{{WC_C_w, K(KEY_BACKSPACE)}, {{&cmd_ctrl_wh}, .descr = "go to left window"}},