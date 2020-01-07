vifm/vifm

commit c8aada936c33745cf5fe05502882d842060234d3

Add config_t::shell_cmd_flag
The value specifies argument for the shell to pass command.

src/utils/utils_win.c
@@ -74,19 +74,19 @@ pause_shell(void)
 {
 	if(curr_stats.shell_type == ST_CMD)
 	{
-		run_in_shell_no_cls("pause");
+		run_in_shell_no_cls("pause", SHELL_BY_APP);
 	}
 	else
 	{
-		run_in_shell_no_cls(PAUSE_CMD);
+		run_in_shell_no_cls(PAUSE_CMD, SHELL_BY_APP);
 	}
 }
 
 int
-run_in_shell_no_cls(char command[])
+run_in_shell_no_cls(char command[], ShellRequester by)
 {
 	int ret;
-	char *const sh_cmd = win_make_sh_cmd(command);
+	char *const sh_cmd = win_make_sh_cmd(command, by);
 
 	/* XXX: why do we use different functions for different cases? */
 	if(curr_stats.shell_type == ST_CMD)
@@ -193,7 +193,7 @@ win_exec_cmd(char cmd[], int *returned_exit_code)
 }
 
 char *
-win_make_sh_cmd(const char cmd[])
+win_make_sh_cmd(const char cmd[], ShellRequester by)
 {
 	char buf[strlen(cfg.shell) + 5 + strlen(cmd)*4 + 1 + 1];
 
@@ -201,17 +201,16 @@ win_make_sh_cmd(const char cmd[])
 	{
 		/* Documentation in `cmd /?` seems to LIE, can't make both spaces and
 		 * special characters work at the same time. */
-		const char *const fmt = (cmd[0] == '"') ? "%s /C \"%s\"" : "%s /C %s";
-		snprintf(buf, sizeof(buf), fmt, cfg.shell, cmd);
+		const char *const fmt = (cmd[0] == '"') ? "%s %s \"%s\"" : "%s %s %s";
+		const char *sh_flag = (by == SHELL_BY_USER ? cfg.shell_cmd_flag : "/C");
+		snprintf(buf, sizeof(buf), fmt, cfg.shell, sh_flag, cmd);
 	}
 	else
 	{
-		char *p;
+		const char *sh_flag = (by == SHELL_BY_USER ? cfg.shell_cmd_flag : "/C");
+		snprintf(buf, sizeof(buf), "%s %s '", cfg.shell, sh_flag);
 
-		strcpy(buf, cfg.shell);
-		strcat(buf, " -c '");
-
-		p = buf + strlen(buf);
+		char *p = buf + strlen(buf);
 		while(*cmd != '\0')
 		{
 			if(*cmd == '\\')

src/background.c
@@ -339,7 +339,7 @@ bg_and_wait_for_errors(char cmd[], const struct cancellation_t *cancellation)
 	if(pid == 0)
 	{
 		(void)set_sigchld(0);
-		run_from_fork(error_pipe, 1, 0, cmd);
+		run_from_fork(error_pipe, 1, 0, cmd, SHELL_BY_APP);
 	}
 	else
 	{
@@ -624,6 +624,7 @@ bg_run_and_capture(char cmd[], int user_sh, FILE **out, FILE **err)
 	if(pid == 0)
 	{
 		char *sh;
+		char *sh_flag;
 
 		close(out_pipe[0]);
 		close(error_pipe[0]);
@@ -637,8 +638,9 @@ bg_run_and_capture(char cmd[], int user_sh, FILE **out, FILE **err)
 		}
 
 		sh = user_sh ? get_execv_path(cfg.shell) : "/bin/sh";
+		sh_flag = user_sh ? cfg.shell_cmd_flag : "-c";
 		prepare_for_exec();
-		execvp(sh, make_execv_array(sh, cmd));
+		execvp(sh, make_execv_array(sh, sh_flag, cmd));
 		_Exit(127);
 	}
 
@@ -661,6 +663,7 @@ background_and_capture_internal(char cmd[], int user_sh, FILE **out, FILE **err,
 	int code;
 	wchar_t *final_wide_cmd;
 	wchar_t *wide_sh = NULL;
+	wchar_t *wide_sh_flag;
 	const int use_cmd = (!user_sh || curr_stats.shell_type == ST_CMD);
 
 	if(_dup2(out_pipe[1], _fileno(stdout)) != 0)
@@ -676,13 +679,14 @@ background_and_capture_internal(char cmd[], int user_sh, FILE **out, FILE **err,
 	}
 
 	wide_sh = to_wide(use_cmd ? "cmd" : cfg.shell);
+	wide_sh_flag = to_wide(user_sh ? cfg.shell_cmd_flag : "/C");
 
 	if(use_cmd)
 	{
 		final_wide_cmd = to_wide(cmd);
 
 		args[0] = wide_sh;
-		args[1] = L"/C";
+		args[1] = wide_sh_flag;
 		args[2] = final_wide_cmd;
 		args[3] = NULL;
 	}
@@ -691,7 +695,8 @@ background_and_capture_internal(char cmd[], int user_sh, FILE **out, FILE **err,
 		/* Nobody cares that there is an array of arguments, all arguments just get
 		 * concatenated anyway...  Therefore we need to take care of escaping stuff
 		 * ourselves. */
-		char *const modified_cmd = win_make_sh_cmd(cmd);
+		char *const modified_cmd = win_make_sh_cmd(cmd,
+				user_sh ? SHELL_BY_USER : SHELL_BY_APP);
 		final_wide_cmd = to_wide(modified_cmd);
 		free(modified_cmd);
 
@@ -701,6 +706,7 @@ background_and_capture_internal(char cmd[], int user_sh, FILE **out, FILE **err,
 
 	code = _wspawnvp(P_NOWAIT, wide_sh, args);
 
+	free(wide_sh_flag);
 	free(wide_sh);
 	free(final_wide_cmd);
 
@@ -766,7 +772,7 @@ bg_run_and_capture(char cmd[], int user_sh, FILE **out, FILE **err)
 #endif
 
 int
-bg_run_external(const char cmd[], int skip_errors)
+bg_run_external(const char cmd[], int skip_errors, ShellRequester by)
 {
 	bg_job_t *job = NULL;
 #ifndef _WIN32
@@ -828,8 +834,9 @@ bg_run_external(const char cmd[], int skip_errors)
 		setpgid(0, 0);
 
 		prepare_for_exec();
-		execve(get_execv_path(cfg.shell), make_execv_array(cfg.shell, command),
-				environ);
+		char *sh_flag = (by == SHELL_BY_USER ? cfg.shell_cmd_flag : "-c");
+		execve(get_execv_path(cfg.shell),
+				make_execv_array(cfg.shell, sh_flag, command), environ);
 		_Exit(127);
 	}
 	else
@@ -860,7 +867,7 @@ bg_run_external(const char cmd[], int skip_errors)
 		return -1;
 	}
 
-	sh_cmd = win_make_sh_cmd(command);
+	sh_cmd = win_make_sh_cmd(command, by);
 	free(command);
 
 	wide_cmd = to_wide(sh_cmd);

src/background.h
@@ -29,6 +29,7 @@
 #include <stdio.h>
 
 #include "compat/pthread.h"
+#include "utils/utils.h"
 
 /* Special value of total amount of work in bg_job_t structure to indicate
  * undefined total number of countable operations. */
@@ -110,7 +111,7 @@ void bg_init(void);
 
 /* Creates background job running external command.  Returns zero on success,
  * otherwise non-zero is returned. */
-int bg_run_external(const char cmd[], int skip_errors);
+int bg_run_external(const char cmd[], int skip_errors, ShellRequester by);
 
 struct cancellation_t;
 

src/cfg/config.c
@@ -196,6 +196,7 @@ cfg_init(void)
 	cfg.log_file[0] = '\0';
 
 	cfg_set_shell(env_get_def("SHELL", DEFAULT_SHELL_CMD));
+	cfg.shell_cmd_flag = strdup((curr_stats.shell_type == ST_CMD) ? "/C" : "-c");
 
 	memset(&cfg.type_decs, '\0', sizeof(cfg.type_decs));
 	cfg.type_decs[FT_DIR][DECORATION_SUFFIX][0] = '/';

src/cfg/config.h
@@ -211,7 +211,10 @@ typedef struct config_t
 	int smart_case;
 	int hl_search;
 	int vifm_info;
-	char *shell;
+
+	char *shell;          /* Shell interpreter command. */
+	char *shell_cmd_flag; /* Argument for the shell to pass command. */
+
 	int scroll_off;
 	int gdefault;
 	int scroll_bind;

src/cmd_handlers.c
@@ -978,7 +978,7 @@ emark_cmd(const cmd_info_t *cmd_info)
 	}
 	else if(cmd_info->bg)
 	{
-		bg_run_external(com, 0);
+		bg_run_external(com, 0, SHELL_BY_USER);
 	}
 	else
 	{
@@ -991,14 +991,14 @@ emark_cmd(const cmd_info_t *cmd_info)
 			if(buf != NULL)
 			{
 				(void)shellout(buf, cmd_info->emark ? PAUSE_ALWAYS : PAUSE_ON_ERROR,
-						use_term_mux);
+						use_term_mux, SHELL_BY_USER);
 				free(buf);
 			}
 		}
 		else
 		{
 			(void)shellout(com, cmd_info->emark ? PAUSE_ALWAYS : PAUSE_ON_ERROR,
-					use_term_mux);
+					use_term_mux, SHELL_BY_USER);
 		}
 	}
 
@@ -2542,7 +2542,7 @@ help_cmd(const cmd_info_t *cmd_info)
 
 	if(bg)
 	{
-		bg_run_external(cmd, 0);
+		bg_run_external(cmd, 0, SHELL_BY_APP);
 	}
 	else
 	{
@@ -3185,16 +3185,16 @@ ls_cmd(const cmd_info_t *cmd_info)
 			ui_sb_msg("No terminal multiplexer is in use");
 			return 1;
 		case TM_SCREEN:
-			(void)vifm_system("screen -X eval windowlist");
+			(void)vifm_system("screen -X eval windowlist", SHELL_BY_APP);
 			return 0;
 		case TM_TMUX:
-			if(vifm_system("tmux choose-window") != EXIT_SUCCESS)
+			if(vifm_system("tmux choose-window", SHELL_BY_APP) != EXIT_SUCCESS)
 			{
 				/* Refresh all windows as failed command outputs message, which can't be
 				 * suppressed. */
 				update_all_windows();
 				/* Fall back to worse way of doing the same for tmux versions < 1.8. */
-				(void)vifm_system("tmux command-prompt choose-window");
+				(void)vifm_system("tmux command-prompt choose-window", SHELL_BY_APP);
 			}
 			return 0;
 
@@ -3815,7 +3815,7 @@ shell_cmd(const cmd_info_t *cmd_info)
 
 	/* Run shell with clean PATH environment variable. */
 	load_clean_path_env();
-	shellout(sh, PAUSE_NEVER, cmd_info->emark ? 0 : 1);
+	shellout(sh, PAUSE_NEVER, cmd_info->emark ? 0 : 1, SHELL_BY_APP);
 	load_real_path_env();
 
 	return 0;
@@ -4948,12 +4948,12 @@ usercmd_cmd(const cmd_info_t *cmd_info)
 
 		if(*com_beginning != '\0' && bg)
 		{
-			bg_run_external(com_beginning, 0);
+			bg_run_external(com_beginning, 0, SHELL_BY_USER);
 		}
 		else if(strlen(com_beginning) > 0)
 		{
 			shellout(com_beginning, pause ? PAUSE_ALWAYS : PAUSE_ON_ERROR,
-					flags != MF_NO_TERM_MUX);
+					flags != MF_NO_TERM_MUX, SHELL_BY_USER);
 		}
 	}
 	else if(expanded_com[0] == '/')
@@ -4971,11 +4971,12 @@ usercmd_cmd(const cmd_info_t *cmd_info)
 	}
 	else if(bg)
 	{
-		bg_run_external(expanded_com, 0);
+		bg_run_external(expanded_com, 0, SHELL_BY_USER);
 	}
 	else
 	{
-		shellout(expanded_com, PAUSE_ON_ERROR, flags != MF_NO_TERM_MUX);
+		shellout(expanded_com, PAUSE_ON_ERROR, flags != MF_NO_TERM_MUX,
+				SHELL_BY_USER);
 	}
 
 	if(external)

src/int/fuse.c
@@ -441,7 +441,7 @@ fuse_unmount_all(void)
 				escaped_filename);
 		free(escaped_filename);
 
-		(void)vifm_system(buf);
+		(void)vifm_system(buf, SHELL_BY_APP);
 		if(path_exists(runner->mount_point, DEREF))
 		{
 			rmdir(runner->mount_point);
@@ -630,8 +630,8 @@ run_fuse_command(char cmd[], const cancellation_t *cancellation, int *cancelled)
 		(void)set_sigchld(0);
 
 		prepare_for_exec();
-		(void)execve(get_execv_path(cfg.shell), make_execv_array(cfg.shell, cmd),
-				environ);
+		(void)execve(get_execv_path(cfg.shell),
+				make_execv_array(cfg.shell, "-c", cmd), environ);
 		_Exit(127);
 	}
 

src/int/vim.c
@@ -222,10 +222,10 @@ run_vim(const char cmd[], int bg, int use_term_multiplexer)
 {
 	if(bg)
 	{
-		return bg_run_external(cmd, 0);
+		return bg_run_external(cmd, 0, SHELL_BY_APP);
 	}
 
-	return shellout(cmd, PAUSE_ON_ERROR, use_term_multiplexer);
+	return shellout(cmd, PAUSE_ON_ERROR, use_term_multiplexer, SHELL_BY_APP);
 }
 
 int
@@ -371,7 +371,7 @@ vim_run_choose_cmd(const view_t *view)
 	}
 
 	expanded_cmd = expand_macros(curr_stats.on_choose, NULL, NULL, 1);
-	if(vifm_system(expanded_cmd) != EXIT_SUCCESS)
+	if(vifm_system(expanded_cmd, SHELL_BY_USER) != EXIT_SUCCESS)
 	{
 		free(expanded_cmd);
 		return 1;

commit d9b25168b5193528be272f902cf499280cff0d4b

Fix running non-cmd shells on Windows
Broken by c8aada936c33745cf5fe05502882d842060234d3:

    Add config_t::shell_cmd_flag

    The value specifies argument for the shell to pass command.

src/utils/utils_win.c
@@ -195,18 +195,20 @@ win_exec_cmd(char cmd[], int *returned_exit_code)
 char *
 win_make_sh_cmd(const char cmd[], ShellRequester by)
 {
-	const char *sh_flag = (by == SHELL_BY_USER ? cfg.shell_cmd_flag : "/C");
+	const char *sh_flag;
 	char *free_me = NULL;
 
 	const char *fmt;
 	if(curr_stats.shell_type == ST_CMD)
 	{
+		sh_flag = (by == SHELL_BY_USER ? cfg.shell_cmd_flag : "/C");
 		/* Documentation in `cmd /?` seems to LIE, can't make both spaces and
 		 * special characters work at the same time. */
 		fmt = (cmd[0] == '"') ? "%s %s \"%s\"" : "%s %s %s";
 	}
 	else
 	{
+		sh_flag = (by == SHELL_BY_USER ? cfg.shell_cmd_flag : "-c");
 		fmt = "%s %s \'%s\'";
 
 		free_me = malloc(strlen(cmd)*2 + 1);