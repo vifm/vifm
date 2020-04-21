bc6671a8b5b275fe617711c5f19479bb106eaeaa
xaizek <xaizek@posteo.net>
2020-04-21 23:07:02 +0300

    Extract commands test suite out of misc
    The misc suite has grown too large by now.

---
+++ tests/commands/suite.c
@@ -0,0 +1,46 @@
+#include <stic.h>
+
+#include <string.h> /* strcpy() */
+
+#include <test-utils.h>
+
+#include "../../src/ui/tabs.h"
+#include "../../src/ui/ui.h"
+#include "../../src/utils/fs.h"
+#include "../../src/background.h"
+#include "../../src/status.h"
+
+static char *saved_cwd;
+
+DEFINE_SUITE();
+
+SETUP_ONCE()
+{
+	fix_environ();
+
+	stub_colmgr();
+
+	/* Remember original path in global SETUP_ONCE instead of SETUP to make sure
+	 * nothing will change the path before we try to save it. */
+	saved_cwd = save_cwd();
+
+	bg_init();
+
+	tabs_init();
+}
+
+SETUP()
+{
+	strcpy(lwin.curr_dir, "/non-existing-dir");
+	strcpy(rwin.curr_dir, "/non-existing-dir");
+	curr_stats.save_msg = 0;
+}
+
+TEARDOWN()
+{
+	restore_cwd(saved_cwd);
+	saved_cwd = save_cwd();
+}
+
+/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
+/* vim: set cinoptions+=t0 filetype=c : */
