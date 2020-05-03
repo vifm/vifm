ca983c8686265cbbbd8d7aed1dd91ab83f2cf82f
xaizek <xaizek@posteo.net>
2020-05-03 19:36:41 +0300

    Remove extra empty line in get_char_width_esc test

--- tests/escape/get_char_width_esc.c
+++ tests/escape/get_char_width_esc.c
@@ -8,6 +8,5 @@ TEST(line_erasure_sequence_is_eaten)
 	assert_int_equal(3, w);
 }
 
-
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
 /* vim: set cinoptions+=t0 : */
