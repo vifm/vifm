vifm/vifm

commit d72129e47fa26dd3948c37bab749d881b5146bd3

Add a and A view mode keys
They switch to next and previous viewer of current file
correspondingly.

Thanks to j-xella.

Part of #330 on GitHub.

src/modes/view.c
@@ -53,6 +53,7 @@
 #include "../utils/regexp.h"
 #include "../utils/str.h"
 #include "../utils/string_array.h"
+#include "../utils/test_helpers.h"
 #include "../utils/utf8.h"
 #include "../utils/utils.h"
 #include "../filelist.h"
@@ -97,11 +98,16 @@ struct modview_info_t
 	int last_search_backward; /* Value -1 means no search was performed. */
 	int search_repeat;        /* Saved count prefix of search commands. */
 
+	/* Viewers. */
+	strlist_t viewers;       /* List of viewers of current file. */
+	char *ext_viewer;        /* When non-NULL, specifies custom preview command
+	                            (no implicit %c). */
+	const char *curr_viewer; /* Current viewer (pointer to strings of two previous
+	                            fields). */
+
 	/* The rest of the state. */
 	view_t *view;    /* File view association with the view. */
 	char *filename;  /* Full path to the file being viewed. */
-	char *viewer;    /* When non-NULL, specifies custom preview command (no
-	                    implicit %c). */
 	int detached;    /* Whether view mode was detached. */
 	ViewerKind kind; /* Kind of preview. */
 	int wrap;        /* Whether lines are wrapped. */
@@ -154,6 +160,7 @@ static void cmd_tab(key_info_t key_info, keys_info_t *keys_info);
 static void cmd_slash(key_info_t key_info, keys_info_t *keys_info);
 static void pick_vi(int explore);
 static void cmd_qmark(key_info_t key_info, keys_info_t *keys_info);
+static void cmd_A(key_info_t key_info, keys_info_t *keys_info);
 static void cmd_F(key_info_t key_info, keys_info_t *keys_info);
 static void cmd_G(key_info_t key_info, keys_info_t *keys_info);
 static void cmd_N(key_info_t key_info, keys_info_t *keys_info);
@@ -161,7 +168,10 @@ static void cmd_R(key_info_t key_info, keys_info_t *keys_info);
 static int load_view_data(modview_info_t *vi, const char action[],
 		const char file_to_view[], int silent);
 static int get_view_data(modview_info_t *vi, const char file_to_view[]);
+static void pick_current_viewer(modview_info_t *vi);
 static void replace_vi(modview_info_t *orig, modview_info_t *new);
+static void cmd_a(key_info_t key_info, keys_info_t *keys_info);
+static void switch_viewer(modview_info_t *vi, int offset);
 static void cmd_b(key_info_t key_info, keys_info_t *keys_info);
 static void cmd_d(key_info_t key_info, keys_info_t *keys_info);
 static void cmd_f(key_info_t key_info, keys_info_t *keys_info);
@@ -190,6 +200,8 @@ static int scroll_to_bottom(modview_info_t *vi);
 static void reload_view(modview_info_t *vi, int silent);
 static void cleanup(modview_info_t *vi);
 static modview_info_t * view_info_alloc(void);
+TSTATIC int modview_is_raw(modview_info_t *vi);
+TSTATIC const char * modview_current_viewer(modview_info_t *vi);
 
 /* Points to current (for quick view) or last used (for explore mode)
  * modview_info_t structure. */
@@ -255,6 +267,7 @@ static keys_add_info_t builtin_cmds[] = {
 	{WK_ALT WK_GT,     {{&cmd_G}, .descr = "scroll to the end"}},
 	{WK_ALT WK_SPACE,  {{&cmd_meta_space}, .descr = "scroll down one window"}},
 	{WK_PERCENT,       {{&cmd_percent},    .descr = "to [count]% position"}},
+	{WK_A,             {{&cmd_A}, .descr = "switch to previous viewer alternative"}},
 	{WK_F,             {{&cmd_F}, .descr = "toggle automatic forward scroll"}},
 	{WK_G,             {{&cmd_G}, .descr = "scroll to the end"}},
 	{WK_N,             {{&cmd_N}, .descr = "go to previous search match"}},
@@ -262,6 +275,7 @@ static keys_add_info_t builtin_cmds[] = {
 	{WK_R,             {{&cmd_R}, .descr = "reload view contents"}},
 	{WK_Z WK_Q,        {{&cmd_q}, .descr = "leave view mode"}},
 	{WK_Z WK_Z,        {{&cmd_q}, .descr = "leave view mode"}},
+	{WK_a,             {{&cmd_a}, .descr = "switch to next viewer alternative"}},
 	{WK_b,             {{&cmd_b}, .descr = "scroll page up"}},
 	{WK_d,             {{&cmd_d}, .descr = "scroll half-page down"}},
 	{WK_f,             {{&cmd_f}, .descr = "scroll page down"}},
@@ -330,10 +344,14 @@ modview_enter(view_t *view, int explore)
 	pick_vi(explore);
 
 	vi->view = view;
+	vi->viewers = ft_get_viewers(full_path);
 	vi->filename = strdup(full_path);
 
 	if(load_view_data(vi, "File exploring", full_path, NOSILENT) != 0)
 	{
+		free_string_array(vi->viewers.items, vi->viewers.nitems);
+		vi->viewers.items = NULL;
+		vi->viewers.nitems = 0;
 		update_string(&vi->filename, NULL);
 		return;
 	}
@@ -369,7 +387,7 @@ modview_detached_make(view_t *view, const char cmd[])
 	reset_view_info(vi);
 
 	vi->view = view;
-	vi->viewer = strdup(cmd);
+	vi->ext_viewer = strdup(cmd);
 	vi->filename = strdup(full_path);
 	vi->detached = 1;
 
@@ -553,13 +571,14 @@ static void
 free_view_info(modview_info_t *vi)
 {
 	free_string_array(vi->lines, vi->nlines);
+	free_string_array(vi->viewers.items, vi->viewers.nitems);
 	free(vi->widths);
 	if(vi->last_search_backward != -1)
 	{
 		regfree(&vi->re);
 	}
 	free(vi->filename);
-	free(vi->viewer);
+	free(vi->ext_viewer);
 }
 
 /* Updates line width and redraws the view. */
@@ -976,6 +995,13 @@ cmd_qmark(key_info_t key_info, keys_info_t *keys_info)
 	modcline_enter(CLS_VWBSEARCH, "", NULL);
 }
 
+/* Switches to the previous viewer. */
+static void
+cmd_A(key_info_t key_info, keys_info_t *keys_info)
+{
+	switch_viewer(vi, -1);
+}
+
 /* Toggles automatic forwarding of file. */
 static void
 cmd_F(key_info_t key_info, keys_info_t *keys_info)
@@ -1075,10 +1101,10 @@ load_view_data(modview_info_t *vi, const char action[],
 static int
 get_view_data(modview_info_t *vi, const char file_to_view[])
 {
-	FILE *fp;
-	const char *const viewer = qv_get_viewer(file_to_view);
+	pick_current_viewer(vi);
 
-	if(vi->raw || (vi->viewer == NULL && is_null_or_empty(viewer)))
+	FILE *fp;
+	if(vi->raw || vi->curr_viewer == NULL)
 	{
 		if(is_dir(file_to_view))
 		{
@@ -1101,8 +1127,7 @@ get_view_data(modview_info_t *vi, const char file_to_view[])
 	}
 	else
 	{
-		const char *const v = (vi->viewer != NULL) ? vi->viewer : viewer;
-		const ViewerKind kind = ft_viewer_kind(v);
+		const ViewerKind kind = ft_viewer_kind(vi->curr_viewer);
 		view_t *const curr = curr_view;
 		curr_view = curr_stats.preview.on ? curr_view
 		          : (vi->view != NULL) ? vi->view : curr_view;
@@ -1123,14 +1148,14 @@ get_view_data(modview_info_t *vi, const char file_to_view[])
 			 * of them need this). */
 			usleep(50000);
 		}
-		if(vi->viewer == NULL)
+		if(vi->curr_viewer != vi->ext_viewer)
 		{
-			fp = qv_execute_viewer(viewer);
+			fp = qv_execute_viewer(vi->curr_viewer);
 		}
 		else
 		{
 			/* Don't add implicit %c to a command with %q macro. */
-			char *const cmd = ma_expand(vi->viewer, NULL, NULL, 1);
+			char *const cmd = ma_expand(vi->ext_viewer, NULL, NULL, 1);
 			fp = read_cmd_output(cmd, 0);
 			free(cmd);
 		}
@@ -1166,6 +1191,25 @@ get_view_data(modview_info_t *vi, const char file_to_view[])
 	return 0;
 }
 
+/* Makes sure that vi->curr_viewer field has a sensible value. */
+static void
+pick_current_viewer(modview_info_t *vi)
+{
+	if(vi->curr_viewer != NULL)
+	{
+		return;
+	}
+
+	if(vi->ext_viewer != NULL)
+	{
+		vi->curr_viewer = vi->ext_viewer;
+	}
+	else if(vi->viewers.nitems != 0)
+	{
+		vi->curr_viewer = vi->viewers.items[0];
+	}
+}
+
 /* Replaces modview_info_t structure with another one preserving as much as
  * possible. */
 static void
@@ -1174,8 +1218,14 @@ replace_vi(modview_info_t *orig, modview_info_t *new)
 	new->filename = orig->filename;
 	orig->filename = NULL;
 
-	new->viewer = orig->viewer;
-	orig->viewer = NULL;
+	new->viewers = orig->viewers;
+	orig->viewers.items = NULL;
+	orig->viewers.nitems = 0;
+
+	new->curr_viewer = orig->curr_viewer;
+
+	new->ext_viewer = orig->ext_viewer;
+	orig->ext_viewer = NULL;
 
 	if(orig->last_search_backward != -1)
 	{
@@ -1194,6 +1244,42 @@ replace_vi(modview_info_t *orig, modview_info_t *new)
 
 	free_view_info(orig);
 	*orig = *new;
+}
+
+/* Switches to the next viewer. */
+static void
+cmd_a(key_info_t key_info, keys_info_t *keys_info)
+{
+	switch_viewer(vi, 1);
+}
+
+/* Switches to the next or previous viewer. */
+static void
+switch_viewer(modview_info_t *vi, int offset)
+{
+	assert((offset == 1 || offset == -1) && "Invalid offset.");
+
+	if(vi->raw || vi->viewers.nitems <= 1)
+	{
+		return;
+	}
+
+	int i;
+	for(i = 0; i < vi->viewers.nitems; ++i)
+	{
+		if(vi->viewers.items[i] == vi->curr_viewer)
+		{
+			break;
+		}
+	}
+	if(i == vi->viewers.nitems)
+	{
+		return;
+	}
+
+	i = (i + vi->viewers.nitems + offset)%vi->viewers.nitems;
+	vi->curr_viewer = vi->viewers.items[i];
+	reload_view(vi, NOSILENT);
 }
 
 static void
@@ -1688,7 +1774,9 @@ reload_view(modview_info_t *vi, int silent)
 	init_view_info(&new_vi);
 	/* These fields are used in get_view_data(). */
 	new_vi.view = vi->view;
-	new_vi.viewer = vi->viewer;
+	new_vi.curr_viewer = vi->curr_viewer;
+	new_vi.ext_viewer = vi->ext_viewer;
+	new_vi.viewers = vi->viewers;
 	new_vi.raw = vi->raw;
 
 	if(load_view_data(&new_vi, "File exploring reload", vi->filename, silent)
@@ -1716,15 +1804,15 @@ modview_hide_graphics(void)
 static void
 cleanup(modview_info_t *vi)
 {
-	const char *cmd = qv_get_viewer(vi->filename);
-	cmd = (cmd != NULL ? ma_get_clear_cmd(cmd) : NULL);
+	const char *viewer = vi->curr_viewer;
+	const char *cmd = (viewer != NULL ? ma_get_clear_cmd(viewer) : NULL);
 	qv_cleanup(vi->view, cmd);
 }
 
 const char *
 modview_detached_get_viewer(void)
 {
-	return (vi == NULL ? NULL : vi->viewer);
+	return (vi == NULL ? NULL : vi->ext_viewer);
 }
 
 /* Allocates and initializes view mode information.  Returns pointer to it. */
@@ -1748,6 +1836,18 @@ modview_info_free(modview_info_t *info)
 			vi = NULL;
 		}
 	}
+}
+
+TSTATIC int
+modview_is_raw(modview_info_t *vi)
+{
+	return vi->raw;
+}
+
+TSTATIC const char *
+modview_current_viewer(modview_info_t *vi)
+{
+	return vi->curr_viewer;
 }
 
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */

data/man/vifm.1
@@ -1,4 +1,4 @@
-.TH VIFM 1 "October 30, 2020" "vifm 0.11"
+.TH VIFM 1 "November 10, 2020" "vifm 0.11"
 .\" ---------------------------------------------------------------------------
 .SH NAME
 .\" ---------------------------------------------------------------------------
@@ -992,6 +992,13 @@ reload view preserving scroll position.
 toggle automatic forwarding.  Roughly equivalent to periodic file reload and
 scrolling to the bottom.  The behaviour is similar to `tail \-F` or F key in
 less.
+.TP
+.BI a
+switch to the next viewer.  Does nothing for preview constructed via %q macro.
+.TP
+.BI A
+switch to the previous viewer.  Does nothing for preview constructed via
+%q macro.
 .TP
 .BI i
 toggle raw mode (ignoring of defined viewers).

src/modes/view.h
@@ -19,6 +19,8 @@
 #ifndef VIFM__MODES__VIEW_H__
 #define VIFM__MODES__VIEW_H__
 
+#include "../utils/test_helpers.h"
+
 struct view_t;
 
 /* Holds state of a single view mode window. */
@@ -88,6 +90,11 @@ const char * modview_detached_get_viewer(void);
 
 /* Frees view info.  The parameter can be NULL. */
 void modview_info_free(modview_info_t *vi);
+
+TSTATIC_DEFS(
+	int modview_is_raw(modview_info_t *vi);
+	const char * modview_current_viewer(modview_info_t *vi);
+)
 
 #endif /* VIFM__MODES__VIEW_H__ */
 

src/tags.c
@@ -751,6 +751,7 @@ const char *tags[] = {
 	"vifm-q_<",
 	"vifm-q_>",
 	"vifm-q_?",
+	"vifm-q_A",
 	"vifm-q_ALT-<",
 	"vifm-q_ALT->",
 	"vifm-q_ALT-Space",
@@ -777,6 +778,7 @@ const char *tags[] = {
 	"vifm-q_Space",
 	"vifm-q_Tab",
 	"vifm-q_ZZ",
+	"vifm-q_a",
 	"vifm-q_b",
 	"vifm-q_d",
 	"vifm-q_e",

tests/misc/view_mode.c
@@ -1,0 +1,89 @@
+#include <stic.h>
+
+#include <test-utils.h>
+
+#include "../../src/engine/cmds.h"
+#include "../../src/engine/keys.h"
+#include "../../src/engine/mode.h"
+#include "../../src/modes/modes.h"
+#include "../../src/modes/view.h"
+#include "../../src/modes/wk.h"
+#include "../../src/ui/ui.h"
+#include "../../src/utils/matchers.h"
+#include "../../src/filelist.h"
+#include "../../src/filetype.h"
+#include "../../src/status.h"
+
+SETUP_ONCE()
+{
+	curr_stats.preview.on = 0;
+}
+
+SETUP()
+{
+	view_setup(&lwin);
+	curr_view = &lwin;
+	other_view = &rwin;
+
+	init_modes();
+
+	conf_setup();
+
+	char *error;
+	matchers_t *ms = matchers_alloc("*", 0, 1, "", &error);
+	assert_non_null(ms);
+	ft_set_viewers(ms, "echo 1, echo 2, echo 3");
+
+	make_abs_path(lwin.curr_dir, sizeof(lwin.curr_dir), TEST_DATA_PATH, "read",
+			NULL);
+	populate_dir_list(&lwin, 0);
+	(void)vle_keys_exec_timed_out(WK_e);
+	assert_true(vle_mode_is(VIEW_MODE));
+}
+
+TEARDOWN()
+{
+	(void)vle_keys_exec_timed_out(WK_q);
+
+	conf_teardown();
+
+	view_teardown(&lwin);
+	curr_view = NULL;
+	other_view = NULL;
+
+	vle_keys_reset();
+	vle_cmds_reset();
+	ft_reset(0);
+}
+
+TEST(toggling_raw_mode)
+{
+	assert_false(modview_is_raw(lwin.vi));
+	(void)vle_keys_exec_timed_out(WK_i);
+	assert_true(modview_is_raw(lwin.vi));
+	(void)vle_keys_exec_timed_out(WK_i);
+	assert_false(modview_is_raw(lwin.vi));
+}
+
+TEST(switching_between_viewers)
+{
+	assert_string_equal("echo 1", modview_current_viewer(lwin.vi));
+	(void)vle_keys_exec_timed_out(WK_a);
+	assert_string_equal("echo 2", modview_current_viewer(lwin.vi));
+	(void)vle_keys_exec_timed_out(WK_a);
+	assert_string_equal("echo 3", modview_current_viewer(lwin.vi));
+	(void)vle_keys_exec_timed_out(WK_a);
+	assert_string_equal("echo 1", modview_current_viewer(lwin.vi));
+	(void)vle_keys_exec_timed_out(WK_A);
+	assert_string_equal("echo 3", modview_current_viewer(lwin.vi));
+
+	(void)vle_keys_exec_timed_out(WK_i);
+	assert_true(modview_is_raw(lwin.vi));
+	(void)vle_keys_exec_timed_out(WK_a);
+	assert_string_equal("echo 3", modview_current_viewer(lwin.vi));
+	(void)vle_keys_exec_timed_out(WK_A);
+	assert_string_equal("echo 3", modview_current_viewer(lwin.vi));
+}
+
+/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
+/* vim: set cinoptions+=t0 : */

commit 6808d53d2a3b5e8820f1b75bdf60e3bbae32cca5

Fix previewing of directories in view mode
Got broken in d72129e47fa26dd3948c37bab749d881b5146bd3:

    Add a and A view mode keys

    They switch to next and previous viewer of current file
    correspondingly.

Thanks to Yusuf Aktepe.

Fixes #618 on GitHub.

src/modes/view.c
@@ -343,9 +343,10 @@ modview_enter(view_t *view, int explore)
 
 	pick_vi(explore);
 
+	vi->filename = is_dir(full_path) ? format_str("%s/", full_path)
+	                                 : strdup(full_path);
+	vi->viewers = ft_get_viewers(vi->filename);
 	vi->view = view;
-	vi->viewers = ft_get_viewers(full_path);
-	vi->filename = strdup(full_path);
 
 	if(load_view_data(vi, "File exploring", full_path, NOSILENT) != 0)
 	{