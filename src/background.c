vifm/vifm

commit 95f91946c25e2f0b8d3ab8ac6c2557aeb8bf3182

Don't handle SIGCHLD
It makes it harder to use system(), popen() or start processes using
fork as well as waiting for them using waitpid().  From now on children
are ripped synchronously by polling.

SIGCHLD handler was always there, but it mostly made the life harder.
In particular it doesn't play well with bg_job_t as signal handler might
want to interrupt thread after acquiring status lock of the job and then
try to acquire it again from the handler to produce a deadlock.

src/background.c
@@ -27,7 +27,7 @@
 #include <sys/stat.h> /* O_RDONLY */
 #include <sys/types.h> /* pid_t ssize_t */
 #ifndef _WIN32
-#include <sys/wait.h> /* WEXITSTATUS() WIFEXITED() */
+#include <sys/wait.h> /* waitpid() */
 #endif
 #include <signal.h> /* kill() */
 #include <unistd.h> /* execve() fork() setpgid() setsid() */
@@ -133,9 +133,7 @@ static bg_job_t * add_background_job(pid_t pid, const char cmd[],
 static void * background_task_bootstrap(void *arg);
 static void set_current_job(bg_job_t *job);
 static void make_current_job_key(void);
-#ifdef _WIN32
-static int query_win_code(bg_job_t *job);
-#endif
+static int update_job_status(bg_job_t *job);
 static void mark_job_finished(bg_job_t *job, int exit_code);
 static int bg_op_cancel(bg_op_t *bg_op);
 
@@ -155,33 +153,12 @@ void
 bg_init(void)
 {
 	pthread_t id;
-	const int err = pthread_create(&id, NULL, &error_thread, NULL);
+	int err = pthread_create(&id, NULL, &error_thread, NULL);
 	assert(err == 0);
 	(void)err;
 
 	/* Initialize state for the main thread. */
 	set_current_job(NULL);
-}
-
-void
-bg_process_finished_cb(pid_t pid, int exit_code)
-{
-	bg_job_t *job;
-
-	/* Mark any finished jobs. */
-	job = bg_jobs;
-	while(job != NULL)
-	{
-		if(job->pid == pid)
-		{
-			pthread_spin_lock(&job->status_lock);
-			job->running = 0;
-			job->exit_code = exit_code;
-			pthread_spin_unlock(&job->status_lock);
-			break;
-		}
-		job = job->next;
-	}
 }
 
 void
@@ -194,11 +171,6 @@ bg_check(void)
 	if(head == NULL)
 	{
 		set_jobcount_var(0);
-		return;
-	}
-
-	if(bg_jobs_freeze() != 0)
-	{
 		return;
 	}
 
@@ -248,8 +220,6 @@ bg_check(void)
 	assert(bg_jobs == NULL && "Job list shouldn't be used by anyone.");
 	bg_jobs = head;
 
-	bg_jobs_unfreeze();
-
 	set_jobcount_var(active_jobs);
 }
 
@@ -294,9 +264,7 @@ job_check(bg_job_t *job)
 	}
 	while(new_errors != NULL);
 
-#ifdef _WIN32
-	(void)query_win_code(job);
-#endif
+	(void)update_job_status(job);
 }
 
 /* Frees resources allocated by the job as well as the bg_job_t structure
@@ -356,17 +324,13 @@ bg_and_wait_for_errors(char cmd[], const struct cancellation_t *cancellation)
 		return -1;
 	}
 
-	(void)set_sigchld(1);
-
 	if((pid = fork()) == -1)
 	{
-		(void)set_sigchld(0);
 		return -1;
 	}
 
 	if(pid == 0)
 	{
-		(void)set_sigchld(0);
 		run_from_fork(error_pipe, 1, 0, cmd, SHELL_BY_APP);
 	}
 	else
@@ -401,15 +365,9 @@ bg_and_wait_for_errors(char cmd[], const struct cancellation_t *cancellation)
 		}
 		else
 		{
-			/* Don't use "const int" variables with WEXITSTATUS() as they cause
-			 * compilation errors in case __USE_BSD is defined.  Anonymous type with
-			 * "const int" is composed via compound literal expression. */
-			int status = get_proc_exit_status(pid);
-			result = (status != -1 && WIFEXITED(status)) ? WEXITSTATUS(status) : -1;
+			result = status_to_exit_code(get_proc_exit_status(pid));
 		}
 	}
-
-	(void)set_sigchld(0);
 
 	return result;
 #else
@@ -1248,13 +1206,6 @@ make_current_job_key(void)
 int
 bg_has_active_jobs(int important_only)
 {
-	if(bg_jobs_freeze() != 0)
-	{
-		/* Failed to lock jobs list and using safe choice: pretend there are active
-		 * tasks. */
-		return 1;
-	}
-
 	bg_job_t *job;
 	int running = 0;
 	for(job = bg_jobs; job != NULL && !running; job = job->next)
@@ -1266,26 +1217,7 @@ bg_has_active_jobs(int important_only)
 		}
 	}
 
-	bg_jobs_unfreeze();
-
 	return running;
-}
-
-int
-bg_jobs_freeze(void)
-{
-	/* SIGCHLD needs to be blocked anytime the jobs list is accessed from anywhere
-	 * except the received_sigchld(). */
-	return set_sigchld(1);
-}
-
-void
-bg_jobs_unfreeze(void)
-{
-	/* Unblock SIGCHLD signal. */
-	/* FIXME: maybe store previous state of SIGCHLD and don't unblock if it was
-	 *        blocked. */
-	(void)set_sigchld(0);
 }
 
 int
@@ -1336,11 +1268,10 @@ bg_job_cancelled(bg_job_t *job)
 int
 bg_job_is_running(bg_job_t *job)
 {
-	int running;
 	pthread_spin_lock(&job->status_lock);
-	running = job->running;
+	int running = job->running;
 	pthread_spin_unlock(&job->status_lock);
-	return running;
+	return (running && update_job_status(job));
 }
 
 int
@@ -1349,44 +1280,42 @@ bg_job_wait(bg_job_t *job)
 	assert(job->type == BJT_COMMAND &&
 			"Only external commands can be waited for.");
 
-	(void)set_sigchld(1);
-
-	int error = 0;
-	if(bg_job_is_running(job))
+	if(!bg_job_is_running(job))
 	{
-#ifndef _WIN32
-		int status = get_proc_exit_status(job->pid);
-		if(status == -1)
-		{
-			error = 1;
-		}
-		else
-		{
-			int exit_code = (WIFEXITED(status) ? WEXITSTATUS(status) : -1);
-			mark_job_finished(job, exit_code);
-		}
-#else
-		if(WaitForSingleObject(job->hprocess, INFINITE) != WAIT_OBJECT_0)
-		{
-			error = 1;
-		}
-		else
-		{
-			error = query_win_code(job);
-		}
-#endif
+		return 0;
 	}
 
-	(void)set_sigchld(0);
-	return error;
+#ifndef _WIN32
+	int status = get_proc_exit_status(job->pid);
+	if(status == -1)
+	{
+		return 1;
+	}
+	mark_job_finished(job, status_to_exit_code(status));
+	return 0;
+#else
+	if(WaitForSingleObject(job->hprocess, INFINITE) != WAIT_OBJECT_0)
+	{
+		return 1;
+	}
+	return update_job_status(job);
+#endif
 }
 
-#ifdef _WIN32
 /* Retrieves exit code of a process associated with the job.  Returns zero on
- * success (fields of the argument updated), otherwise non-zero is returned. */
+ * success (job has just finished), otherwise non-zero is returned. */
 static int
-query_win_code(bg_job_t *job)
+update_job_status(bg_job_t *job)
 {
+#ifndef _WIN32
+	int status;
+	if(job->pid != -1 && waitpid(job->pid, &status, WNOHANG) == job->pid)
+	{
+		mark_job_finished(job, status_to_exit_code(status));
+		return 0;
+	}
+	return 1;
+#else
 	DWORD retcode;
 	if(GetExitCodeProcess(job->hprocess, &retcode))
 	{
@@ -1397,8 +1326,8 @@ query_win_code(bg_job_t *job)
 		}
 	}
 	return 1;
-}
 #endif
+}
 
 /* Marks job as finished with the specified exit code. */
 static void

src/background.h
@@ -106,7 +106,7 @@ bg_job_t;
 /* Background task entry point function signature. */
 typedef void (*bg_task_func)(bg_op_t *bg_op, void *arg);
 
-/* List of background jobs.  Use bg_jobs_freeze() before accessing it. */
+/* List of background jobs. */
 extern bg_job_t *bg_jobs;
 
 /* Prepare background unit for the work. */
@@ -136,10 +136,6 @@ int bg_and_wait_for_errors(char cmd[],
  * non-*nix like systems) or (pid_t)-1 on error. */
 pid_t bg_run_and_capture(char cmd[], int user_sh, FILE **out, FILE **err);
 
-/* Callback-like function that marks background job specified by its process id,
- * which is finished with the exit_code. */
-void bg_process_finished_cb(pid_t pid, int exit_code);
-
 /* Checks status of background jobs (their streams and state).  Removes finished
  * ones from the list, displays any pending error messages, corrects job bar if
  * needed. */
@@ -154,14 +150,6 @@ int bg_execute(const char descr[], const char op_descr[], int total,
  * jobs or tasks (important_only is zero) running in background.  External
  * applications whose state is tracked are always ignored by this function. */
 int bg_has_active_jobs(int important_only);
-
-/* Performs preparations necessary for safe access of the bg_jobs list.  Effect
- * of calling this function must be reverted by calling bg_jobs_unfreeze().
- * Returns zero on success, otherwise non-zero is returned. */
-int bg_jobs_freeze(void);
-
-/* Undoes changes made by bg_jobs_freeze(). */
-void bg_jobs_unfreeze(void);
 
 /* Cancels the job.  Returns non-zero if job wasn't cancelled before, but is
  * after this call, otherwise zero is returned. */

src/int/fuse.c
@@ -617,12 +617,9 @@ run_fuse_command(char cmd[], const cancellation_t *cancellation, int *cancelled)
 		return 1;
 	}
 
-	(void)set_sigchld(1);
-
 	pid = fork();
 	if(pid == (pid_t)-1)
 	{
-		(void)set_sigchld(0);
 		LOG_SERROR_MSG(errno, "Forking has failed.");
 		return -1;
 	}
@@ -630,8 +627,6 @@ run_fuse_command(char cmd[], const cancellation_t *cancellation, int *cancelled)
 	if(pid == (pid_t)0)
 	{
 		extern char **environ;
-
-		(void)set_sigchld(0);
 
 		prepare_for_exec();
 		(void)execve(get_execv_path(cfg.shell),
@@ -655,8 +650,6 @@ run_fuse_command(char cmd[], const cancellation_t *cancellation, int *cancelled)
 	{
 		*cancelled = 1;
 	}
-
-	(void)set_sigchld(0);
 
 	return status;
 #else

src/menus/jobs_menu.c
@@ -56,8 +56,6 @@ show_jobs_menu(view_t *view)
 
 	bg_check();
 
-	bg_jobs_freeze();
-
 	p = bg_jobs;
 
 	i = 0;
@@ -93,8 +91,6 @@ show_jobs_menu(view_t *view)
 
 		p = p->next;
 	}
-
-	bg_jobs_unfreeze();
 
 	jobs_m.len = i;
 
@@ -144,7 +140,6 @@ cancel_job(menu_data_t *m, bg_job_t *job)
 
 	/* We have to make sure the job pointer is still valid and the job is
 	 * running. */
-	bg_jobs_freeze();
 	for(p = bg_jobs; p != NULL; p = p->next)
 	{
 		if(p == job && bg_job_is_running(job))
@@ -158,7 +153,6 @@ cancel_job(menu_data_t *m, bg_job_t *job)
 			break;
 		}
 	}
-	bg_jobs_unfreeze();
 
 	return (p != NULL);
 }
@@ -174,7 +168,6 @@ show_job_errors(view_t *view, menu_data_t *m, bg_job_t *job)
 
 	/* We have to make sure the job pointer is still valid and the job is
 	 * running. */
-	bg_jobs_freeze();
 	for(p = bg_jobs; p != NULL; p = p->next)
 	{
 		if(p == job)
@@ -185,7 +178,6 @@ show_job_errors(view_t *view, menu_data_t *m, bg_job_t *job)
 			break;
 		}
 	}
-	bg_jobs_unfreeze();
 
 	if(p == NULL)
 	{

src/signals.c
@@ -42,9 +42,6 @@ static void _gnuc_noreturn shutdown_nicely(int sig, const char descr[]);
 
 #ifndef _WIN32
 
-#include <sys/types.h> /* pid_t */
-#include <sys/wait.h> /* WEXITSTATUS() WIFEXITED() waitpid() */
-
 #include <stdlib.h> /* EXIT_FAILURE _Exit() */
 
 #include "utils/macros.h"
@@ -76,27 +73,6 @@ received_sigcont(void)
 }
 
 static void
-received_sigchld(void)
-{
-	int status;
-	pid_t pid;
-
-	/* This needs to be a loop in case of multiple blocked signals. */
-	while((pid = waitpid(-1, &status, WNOHANG)) > 0)
-	{
-		if(WIFEXITED(status))
-		{
-			bg_process_finished_cb(pid, WEXITSTATUS(status));
-		}
-		else if(WIFSIGNALED(status))
-		{
-			/* Child was terminated by a signal. */
-			bg_process_finished_cb(pid, -1);
-		}
-	}
-}
-
-static void
 handle_signal(int sig)
 {
 	/* Try to not change errno value in the main program. */
@@ -106,9 +82,6 @@ handle_signal(int sig)
 	{
 		case SIGINT:
 			ui_cancellation_request();
-			break;
-		case SIGCHLD:
-			received_sigchld();
 			break;
 		case SIGWINCH:
 			received_sigwinch();
@@ -189,7 +162,6 @@ setup_signals(void)
 	 * wanted us to have disabled (e.g., the app will catch Ctrl-C sent to another
 	 * process). */
 
-	sigaction(SIGCHLD, &handle_signal_action, NULL);
 	sigaction(SIGHUP, &handle_signal_action, NULL);
 	sigaction(SIGINT, &handle_signal_action, NULL);
 	sigaction(SIGQUIT, &handle_signal_action, NULL);

src/utils/utils.h
@@ -196,10 +196,6 @@ struct cancellation_t;
 void wait_for_data_from(pid_t pid, FILE *f, int fd,
 		const struct cancellation_t *cancellation);
 
-/* Blocks/unblocks SIGCHLD signal.  Returns zero on success, otherwise non-zero
- * is returned. */
-int set_sigchld(int block);
-
 /* Blocks all signals of current thread that can be blocked. */
 void block_all_thread_signals(void);
 

src/utils/utils_nix.c
@@ -34,7 +34,7 @@
 #include <sys/stat.h> /* O_* S_* */
 #include <sys/statvfs.h> /* statvfs statvfs() */
 #include <sys/time.h> /* timeval futimens() utimes() */
-#include <sys/wait.h> /* waitpid */
+#include <sys/wait.h> /* WEXITSTATUS() WIFEXITED() waitpid() */
 #include <fcntl.h> /* open() close() */
 #include <grp.h> /* getgrnam() getgrgid_r() */
 #include <pthread.h> /* pthread_sigmask() */
@@ -45,8 +45,8 @@
 #include <assert.h> /* assert() */
 #include <ctype.h> /* isdigit() */
 #include <errno.h> /* EINTR ENOTSUP errno */
-#include <signal.h> /* SIG* SIG_* sigset_t kill() sigaddset() sigemptyset()
-                       sigfillset() signal() sigprocmask() */
+#include <signal.h> /* SIG* SIG_* sigset_t kill() sigemptyset() sigfillset()
+                       signal() */
 #include <stddef.h> /* NULL size_t */
 #include <stdio.h> /* FILE stderr fclose() fdopen() fprintf() snprintf() */
 #include <stdlib.h> /* atoi() free() */
@@ -126,23 +126,16 @@ run_in_shell_no_cls(char command[], ShellRequester by)
 	new.sa_flags = SA_RESTART;
 	sigaction(SIGTSTP, &new, &old);
 
-	/* We need to block SIGCHLD signal.  One can't just set it to SIG_DFL, because
-	 * it will possibly cause missing of SIGCHLD from a background process
-	 * (job). */
-	(void)set_sigchld(1);
-
 	pid = fork();
 	if(pid == -1)
 	{
 		sigaction(SIGTSTP, &old, NULL);
-		(void)set_sigchld(0);
 		return -1;
 	}
 	if(pid == 0)
 	{
 		signal(SIGTSTP, SIG_DFL);
 		signal(SIGINT, SIG_DFL);
-		(void)set_sigchld(0);
 
 		prepare_for_exec();
 		char *sh_flag = (by == SHELL_BY_USER ? cfg.shell_cmd_flag : "-c");
@@ -154,7 +147,6 @@ run_in_shell_no_cls(char command[], ShellRequester by)
 	result = get_proc_exit_status(pid);
 
 	sigaction(SIGTSTP, &old, NULL);
-	(void)set_sigchld(0);
 
 	return result;
 }
@@ -186,17 +178,6 @@ wait_for_data_from(pid_t pid, FILE *f, int fd,
 		select_result = select(fd + 1, &read_ready, NULL, NULL, &ts);
 	}
 	while(select_result == 0 || (select_result == -1 && errno == EINTR));
-}
-
-int
-set_sigchld(int block)
-{
-	const int action = block ? SIG_BLOCK : SIG_UNBLOCK;
-	sigset_t sigchld_mask;
-
-	return sigemptyset(&sigchld_mask) == -1
-	    || sigaddset(&sigchld_mask, SIGCHLD) == -1
-	    || sigprocmask(action, &sigchld_mask, NULL) == -1;
 }
 
 void
@@ -734,6 +715,12 @@ get_gid(const char group[], gid_t *gid)
 		*gid = g->gr_gid;
 	}
 	return 0;
+}
+
+int
+status_to_exit_code(int status)
+{
+	return (status != -1 && WIFEXITED(status) ? WEXITSTATUS(status) : -1);
 }
 
 int

src/utils/utils_nix.h
@@ -72,6 +72,10 @@ int get_uid(const char user[], uid_t *uid);
  * zero on success and non-zero otherwise. */
 int get_gid(const char group[], gid_t *gid);
 
+/* Converts status to exit code.  Input can be -1, meaning that status is
+ * unknown.  Returns the exit code or -1 for -1 status. */
+int status_to_exit_code(int status);
+
 int S_ISEXE(mode_t mode);
 
 #endif /* VIFM__UTILS__UTILS_NIX_H__ */

commit 35ab7808d965aaf12e5546e68f4566d743e166fc

Fix zombies not being ripped
Caused by 95f91946c25e2f0b8d3ab8ac6c2557aeb8bf3182:

    Don't handle SIGCHLD

    It makes it harder to use system(), popen() or start processes using
    fork as well as waiting for them using waitpid().  From now on
    children are ripped synchronously by polling.

    SIGCHLD handler was always there, but it mostly made the life harder.
    In particular it doesn't play well with bg_job_t as signal handler
    might want to interrupt thread after acquiring status lock of the
    job and then try to acquire it again from the handler to produce a
    deadlock.

Thanks to PRESFIL.

Fixes #639 on GitHub.

src/background.c
@@ -121,6 +121,8 @@ static void free_drained_jobs(bg_job_t **jobs);
 static void import_error_jobs(bg_job_t **jobs);
 static void make_ready_list(const bg_job_t *jobs, selector_t *selector);
 #ifndef _WIN32
+static void rip_children(void);
+static void rip_child(pid_t pid, int status);
 static void report_error_msg(const char title[], const char text[]);
 #endif
 static bg_job_t * launch_external(const char cmd[], int capture_output,
@@ -164,11 +166,13 @@ bg_init(void)
 void
 bg_check(void)
 {
-	bg_job_t *head = bg_jobs;
+#ifndef _WIN32
+	rip_children();
+#endif
 
 	/* Quit if there is no jobs or list is unavailable (e.g. used by another
 	 * invocation of this function). */
-	if(head == NULL)
+	if(bg_jobs == NULL)
 	{
 		set_jobcount_var(0);
 		return;
@@ -176,7 +180,7 @@ bg_check(void)
 
 	int active_jobs = 0;
 
-	head = bg_jobs;
+	bg_job_t *head = bg_jobs;
 	bg_jobs = NULL;
 
 	bg_job_t *p = head;
@@ -552,6 +556,39 @@ make_ready_list(const bg_job_t *jobs, selector_t *selector)
 }
 
 #ifndef _WIN32
+
+/* Rips children updating status of jobs in the process. */
+static void
+rip_children(void)
+{
+	int status;
+	pid_t pid;
+
+	/* This needs to be a loop in case of multiple blocked signals. */
+	while((pid = waitpid(-1, &status, WNOHANG)) > 0)
+	{
+		if(WIFEXITED(status) || WIFSIGNALED(status))
+		{
+			rip_child(pid, status);
+		}
+	}
+}
+
+/* Looks up a child in job list and rips it if found. */
+static void
+rip_child(pid_t pid, int status)
+{
+	bg_job_t *job;
+	for(job = bg_jobs; job != NULL; job = job->next)
+	{
+		if(job->pid == pid)
+		{
+			mark_job_finished(job, status_to_exit_code(status));
+			break;
+		}
+	}
+}
+
 /* Either displays error message to the user for foreground operations or saves
  * it for displaying on the next invocation of bg_check(). */
 static void
@@ -569,6 +606,7 @@ report_error_msg(const char title[], const char text[])
 		append_error_msg(job, text);
 	}
 }
+
 #endif
 
 /* Appends message to error-related fields of the job. */