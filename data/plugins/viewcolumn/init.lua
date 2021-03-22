932aad2125d90154df690f08eaca8ed64a0d09c8
filterfalse <filterfalse@gmail.com>
2021-03-22 18:06:14 +0200

    Add user-defined columns with time stats age

--- data/plugins/viewcolumn/init.lua
+++ data/plugins/viewcolumn/init.lua
@@ -1,14 +1,17 @@
 --[[
 
-Provides three user-defined view column types:
- * LsSize -- ls-like size column (at most 4 characters in width)
- * LsTime -- ls-like (also MC-like) time where time is shown if year matches
-             current, otherwise year is displayed instead of time
- * MCSize -- MC-like size column (at most 7 characters in width)
+Provides six user-defined view column types:
+ * LsSize   -- ls-like size column (at most 4 characters in width)
+ * LsTime   -- ls-like (also MC-like) time where time is shown if year matches
+               current, otherwise year is displayed instead of time
+ * MCSize   -- MC-like size column (at most 7 characters in width)
+ * AgeAtime -- integer age based on the atime stat (11 characters in width)
+ * AgeCtime -- integer age based on the ctime stat (11 characters in width)
+ * AgeMtime -- integer age based on the mtime stat (11 characters in width)
 
 Usage example:
 
-    :set viewcolumns+=8{MCSize}
+    :set viewcolumns=-{name},8{MCSize}
 
 --]]
 
@@ -86,6 +89,44 @@ local added = vifm.addcolumntype {
 }
 if not added then
     vifm.sb.error("Failed to add MCSize view column")
+end
+
+local time_units = {
+    {name = 'year',   seconds = 365 * 24 * 60 * 60},
+    {name = 'month',  seconds =  30 * 24 * 60 * 60},
+    {name = 'week',   seconds =   7 * 24 * 60 * 60},
+    {name = 'day',    seconds =       24 * 60 * 60},
+    {name = 'hour',   seconds =            60 * 60},
+    {name = 'minute', seconds =                 60},
+    {name = 'second', seconds =                  1},
+}
+
+local function get_age(seconds)
+    local diff = os.difftime(os.time(), seconds)
+    local future_sign = diff >= 0 and '' or '-'
+    diff = math.abs(diff)
+    for _, time_unit in ipairs(time_units) do
+        if diff >= time_unit.seconds then
+            local int = math.floor(diff / time_unit.seconds)
+            return string.format('%3s %-7s',
+                future_sign .. tostring(int),
+                time_unit.name .. (int > 1 and 's' or ' '))
+        end
+    end
+    return '  0 seconds'
+end
+
+for _, stat_name in ipairs({'atime', 'ctime', 'mtime'}) do
+    local column_name = 'Age' .. stat_name:sub(1, 1):upper() .. stat_name:sub(2)
+    local added = vifm.addcolumntype {
+        name = column_name,
+        handler = function(info)
+            return get_age(info.entry[stat_name])
+        end
+    }
+    if not added then
+        vifm.sb.error("Failed to add " .. column_name .. " view column")
+    end
 end
 
 return {}
