b10a72b4e39ed7d47f77b52e59bcaec1e5b0a57a
xaizek <xaizek@posteo.net>
2021-09-11 14:22:27 +0300

    Fix lua_pop() in vlua_state_alloc()
    Left a extra value on stack.

--- src/lua/vlua_state.c
+++ src/lua/vlua_state.c
@@ -50,7 +50,7 @@ vlua_state_alloc(void)
 	luaL_requiref(vlua->lua, LUA_STRLIBNAME, &luaopen_string, 1);
 	luaL_requiref(vlua->lua, LUA_MATHLIBNAME, &luaopen_math, 1);
 	luaL_requiref(vlua->lua, LUA_OSLIBNAME, &luaopen_os, 1);
-	lua_pop(vlua->lua, 5);
+	lua_pop(vlua->lua, 6);
 
 	return vlua;
 }
