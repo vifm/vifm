ca980ede4685fd2fcc2e5e82a004ce298e5bc4c3
xaizek <xaizek@posteo.net>
2022-06-12 16:25:54 +0300

    Put more logic into middle_border_is_visible()
    We have the function, might as well use it in all appropriate places.

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -600,7 +600,7 @@ horizontal_layout(int screen_x, int screen_y)
 		splitter_pos = screen_y/2 - 1;
 	else
 		splitter_pos = curr_stats.splitter_pos;
-	int splitter_height = (strlen(cfg.hborder_filler) ? 1 : 0);
+	int splitter_height = (middle_border_is_visible() ? 1 : 0);
 	if(splitter_pos < 2)
 		splitter_pos = 2;
 	if(splitter_pos > get_working_area_height() - 1 - splitter_height)
@@ -956,7 +956,19 @@ clear_border(WINDOW *border)
 static int
 middle_border_is_visible(void)
 {
-	return (curr_stats.number_of_windows == 2);
+	if(curr_stats.number_of_windows != 2)
+	{
+		return 0;
+	}
+
+	if(curr_stats.split == VSPLIT)
+	{
+		/* Always visible for vertical split. */
+		return 1;
+	}
+
+	/* Optionally visible for horizontal split. */
+	return (strlen(cfg.hborder_filler) != 0);
 }
 
 /* Updates (redraws or reloads) views. */
