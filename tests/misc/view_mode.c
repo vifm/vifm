vifm/vifm

commit 6808d53d2a3b5e8820f1b75bdf60e3bbae32cca5

Fix previewing of directories in view mode
Got broken in d72129e47fa26dd3948c37bab749d881b5146bd3:

    Add a and A view mode keys

    They switch to next and previous viewer of current file
    correspondingly.

Thanks to Yusuf Aktepe.

Fixes #618 on GitHub.

src/modes/view.c
@@ -343,9 +343,10 @@ modview_enter(view_t *view, int explore)
 
 	pick_vi(explore);
 
+	vi->filename = is_dir(full_path) ? format_str("%s/", full_path)
+	                                 : strdup(full_path);
+	vi->viewers = ft_get_viewers(vi->filename);
 	vi->view = view;
-	vi->viewers = ft_get_viewers(full_path);
-	vi->filename = strdup(full_path);
 
 	if(load_view_data(vi, "File exploring", full_path, NOSILENT) != 0)
 	{

commit 14f198051256cdb97e1c837813fd8edf0f7a15f7

Fix directory preview resetting on detach
Got broken by commit 6808d53d2a3b5e8820f1b75bdf60e3bbae32cca5:

    Fix previewing of directories in view mode

    Got broken in d72129e47fa26dd3948c37bab749d881b5146bd3:

        Add a and A view mode keys

        They switch to next and previous viewer of current file
        correspondingly.

Thanks to Luka Markušić (a.k.a. mark2185).

Fixes #769 on GitHub.

THANKS
@@ -145,6 +145,7 @@ lcj
 Lomov Vladimir (lomov_vl)
 loongw
 Losiara
+Luka Markušić (mark2185)
 lyuts
 M Kelly
 m-fonseca

src/modes/view.c
@@ -203,6 +203,7 @@ static void reload_view(modview_info_t *vi, int silent);
 static void cleanup(modview_info_t *vi);
 static modview_info_t * view_info_alloc(void);
 TSTATIC int modview_is_raw(modview_info_t *vi);
+TSTATIC int modview_is_detached(modview_info_t *vi);
 TSTATIC const char * modview_current_viewer(modview_info_t *vi);
 TSTATIC int modview_current_line(modview_info_t *vi);
 TSTATIC strlist_t modview_lines(modview_info_t *vi);
@@ -413,7 +414,7 @@ try_resurrect_detached(const char full_path[], int explore)
 	                   && vi->detached
 	                   && vi->view == (explore ? curr_view : other_view)
 	                   && vi->filename != NULL
-	                   && stroscmp(vi->filename, full_path) == 0;
+	                   && paths_are_equal(vi->filename, full_path);
 	if(!same_file)
 	{
 		return 1;
@@ -1655,12 +1656,7 @@ is_trying_the_same_file(void)
 		return 0;
 	}
 
-	if(stroscmp(vi->filename, full_path) != 0)
-	{
-		return 0;
-	}
-
-	return 1;
+	return paths_are_equal(vi->filename, full_path);
 }
 
 /* Gets full path to the file that will be explored (the current file of the
@@ -1839,6 +1835,12 @@ TSTATIC int
 modview_is_raw(modview_info_t *vi)
 {
 	return vi->raw;
+}
+
+TSTATIC int
+modview_is_detached(modview_info_t *vi)
+{
+	return vi->detached;
 }
 
 TSTATIC const char *

src/modes/view.h
@@ -96,6 +96,7 @@ void modview_info_free(modview_info_t *vi);
 
 TSTATIC_DEFS(
 	int modview_is_raw(modview_info_t *vi);
+	int modview_is_detached(modview_info_t *vi);
 	const char * modview_current_viewer(modview_info_t *vi);
 	int modview_current_line(modview_info_t *vi);
 	struct strlist_t;

tests/misc/view_mode.c
@@ -11,6 +11,7 @@
 #include "../../src/modes/view.h"
 #include "../../src/modes/wk.h"
 #include "../../src/ui/statusbar.h"
+#include "../../src/ui/quickview.h"
 #include "../../src/ui/ui.h"
 #include "../../src/utils/matchers.h"
 #include "../../src/utils/str.h"
@@ -32,6 +33,9 @@ SETUP()
 {
 	view_setup(&lwin);
 	lwin.window_rows = 1;
+	view_setup(&rwin);
+	rwin.window_rows = 1;
+	rwin.window_cols = 1;
 
 	curr_view = &lwin;
 	other_view = &rwin;
@@ -43,11 +47,15 @@ SETUP()
 
 TEARDOWN()
 {
-	(void)vle_keys_exec_timed_out(WK_q);
+	if(vle_mode_is(VIEW_MODE))
+	{
+		modview_leave();
+	}
 
 	conf_teardown();
 
 	view_teardown(&lwin);
+	view_teardown(&rwin);
 	curr_view = NULL;
 	other_view = NULL;
 
@@ -282,6 +290,43 @@ TEST(cmd_v)
 
 	vlua_finish(curr_stats.vlua);
 	curr_stats.vlua = NULL;
+}
+
+TEST(views_with_dirs_can_be_reattached)
+{
+	opt_handlers_setup();
+
+	make_abs_path(lwin.curr_dir, sizeof(lwin.curr_dir), TEST_DATA_PATH, "", NULL);
+	populate_dir_list(&lwin, 0);
+	/* The next call is to reset state of modview. */
+	assert_false(modview_detached_draw());
+	assert_success(qv_ensure_is_shown());
+
+	/* Enter the view. */
+	(void)vle_keys_exec_timed_out(WK_C_w WK_C_w);
+	assert_true(vle_mode_is(VIEW_MODE));
+
+	/* Scroll down a bit. */
+	(void)vle_keys_exec_timed_out(L"2j");
+	assert_int_equal(2, modview_current_line(curr_stats.preview.explore));
+
+	/* Detach the view. */
+	(void)vle_keys_exec_timed_out(WK_C_w WK_C_w);
+	assert_true(vle_mode_is(NORMAL_MODE));
+
+	/* Reattach it. */
+	(void)vle_keys_exec_timed_out(WK_C_w WK_C_w);
+	assert_true(vle_mode_is(VIEW_MODE));
+
+	/* It was resurrected. */
+	assert_true(modview_is_detached(curr_stats.preview.explore));
+	/* Its internal state probably wasn't reset if it allows scrolling. */
+	(void)vle_keys_exec_timed_out(L"k");
+	assert_int_equal(1, modview_current_line(curr_stats.preview.explore));
+	(void)vle_keys_exec_timed_out(L"k");
+	assert_int_equal(0, modview_current_line(curr_stats.preview.explore));
+
+	opt_handlers_teardown();
 }
 
 static int