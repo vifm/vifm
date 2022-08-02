7e39d4ee4698ab23030f37d4a321ddca2ddde58d
xaizek <xaizek@posteo.net>
2022-08-02 15:13:48 +0300

    Add an extra test for user mapping onto a multikey

--- tests/keys/remap_builtin.c
+++ tests/keys/remap_builtin.c
@@ -16,5 +16,11 @@ TEST(remap_builtin_waitpoint)
 	assert_success(vle_keys_exec(L"qa"));
 }
 
+TEST(remap_multi_has_no_wait)
+{
+	assert_success(vle_keys_user_add(L"m", L"j", NORMAL_MODE, KEYS_FLAG_NONE));
+	assert_false(IS_KEYS_RET_CODE(vle_keys_exec(L"m")));
+}
+
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0: */
 /* vim: set cinoptions+=t0 filetype=c : */
