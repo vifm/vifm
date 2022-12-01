vifm/vifm

commit f3690f52a48c4945d83f2820464bbadcff51690a

Add 'locateprg' option to specify command format
%a can be used to specify placement of :locate command arguments, if
not used, it will be implicitly added after a space to the expanded
value of the 'locateprg' option.

src/menus/locate_menu.c
@@ -18,9 +18,14 @@
  */
 
 #include <stdio.h> /* snprintf() */
+#include <stdlib.h> /* free() */
 #include <string.h> /* strdup() */
 
+#include "../cfg/config.h"
+#include "../utils/macros.h"
 #include "../utils/path.h"
+#include "../utils/str.h"
+#include "../macros.h"
 #include "../ui.h"
 #include "menus.h"
 
@@ -29,19 +34,25 @@
 int
 show_locate_menu(FileView *view, const char args[])
 {
-	char cmd_buf[256];
+	char *cmd;
+	int save_msg;
+	custom_macro_t macros[] =
+	{
+		{ .letter = 'a', .value = args, .uses_left = 1 },
+	};
 
 	static menu_info m;
 	init_menu_info(&m, LOCATE, strdup("No files found"));
 	m.args = (args[0] == '-') ? strdup(args) : escape_filename(args, 0);
-
-	snprintf(cmd_buf, sizeof(cmd_buf), "locate %s", m.args);
-
-	m.title = strdup(cmd_buf);
+	m.title = format_str(" Locate %s ", m.args);
 
 	status_bar_message("locate...");
 
-	return capture_output_to_menu(view, cmd_buf, &m);
+	cmd = expand_custom_macros(cfg.locate_prg, ARRAY_LEN(macros), macros);
+	save_msg = capture_output_to_menu(view, cmd, &m);
+	free(cmd);
+
+	return save_msg;
 }
 
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */

data/vim/syntax/vifm.vim
@@ -1,6 +1,6 @@
 " vifm syntax file
 " Maintainer:  xaizek <xaizek@lavabit.com>
-" Last Change: June 16, 2013
+" Last Change: June 17, 2013
 " Based On:    Vim syntax file by Dr. Charles E. Campbell, Jr.
 
 if exists('b:current_syntax')
@@ -61,11 +61,11 @@ syntax case match
 " Options
 syntax keyword vifmOption contained aproposprg autochpos classify columns co
 		\ confirm cf cpoptions cpo dotdirs fastrun followlinks fusehome gdefault
-		\ history hi hlsearch hls iec ignorecase ic incsearch is laststatus lines ls
-		\ lsview rulerformat ruf runexec scrollbind scb scrolloff so sort sortorder
-		\ shell sh shortmess shm slowfs smartcase scs sortnumbers statusline stl
-		\ tabstop timefmt timeoutlen trash trashdir ts undolevels ul vicmd
-		\ viewcolumns vifminfo vimhelp vixcmd wildmenu wmnu wrap wrapscan ws
+		\ history hi hlsearch hls iec ignorecase ic incsearch is laststatus lines
+		\ locateprg ls lsview rulerformat ruf runexec scrollbind scb scrolloff so
+		\ sort sortorder shell sh shortmess shm slowfs smartcase scs sortnumbers
+		\ statusline stl tabstop timefmt timeoutlen trash trashdir ts undolevels ul
+		\ vicmd viewcolumns vifminfo vimhelp vixcmd wildmenu wmnu wrap wrapscan ws
 
 " Disabled boolean options
 syntax keyword vifmOption contained noautochpos noconfirm nocf nofastrun

src/cfg/config.c
@@ -167,6 +167,7 @@ init_config(void)
 	cfg.filter_inverted_by_default = 1;
 
 	cfg.apropos_prg = strdup("apropos %a");
+	cfg.locate_prg = strdup("locate %a");
 
 #ifndef _WIN32
 	snprintf(cfg.log_file, sizeof(cfg.log_file), "/var/log/vifm-startup-log");

src/cfg/config.h
@@ -116,6 +116,7 @@ typedef struct
 	int trunc_normal_sb_msgs; /* Truncate normal status bar messages if needed. */
 	int filter_inverted_by_default; /* Default inversion value for :filter. */
 	char *apropos_prg; /* apropos tool calling pattern. */
+	char *locate_prg; /* locate tool calling pattern. */
 }config_t;
 
 extern config_t cfg;

src/cfg/info.c
@@ -694,6 +694,7 @@ update_info_file(const char filename[])
 		fprintf(fp, "=%sincsearch\n", cfg.inc_search ? "" : "no");
 		fprintf(fp, "=%slaststatus\n", cfg.last_status ? "" : "no");
 		fprintf(fp, "=lines=%d\n", cfg.lines);
+		fprintf(fp, "=locateprg=%s\n", escape_spaces(cfg.locate_prg));
 		fprintf(fp, "=rulerformat=%s\n", escape_spaces(cfg.ruler_format));
 		fprintf(fp, "=%srunexec\n", cfg.auto_execute ? "" : "no");
 		fprintf(fp, "=%sscrollbind\n", cfg.scroll_bind ? "" : "no");

src/opt_handlers.c
@@ -94,6 +94,7 @@ static void ignorecase_handler(OPT_OP op, optval_t val);
 static void incsearch_handler(OPT_OP op, optval_t val);
 static void laststatus_handler(OPT_OP op, optval_t val);
 static void lines_handler(OPT_OP op, optval_t val);
+static void locate_handler(OPT_OP op, optval_t val);
 static void scroll_line_down(FileView *view);
 static void rulerformat_handler(OPT_OP op, optval_t val);
 static void runexec_handler(OPT_OP op, optval_t val);
@@ -255,6 +256,8 @@ static struct
 		{ .ref.bool_val = &cfg.last_status }                                                                   },
 	{ "lines",       "",     OPT_INT,     0,                          NULL,            &lines_handler,
 		{ .ref.int_val = &cfg.lines }                                                                          },
+	{ "locateprg",   "",     OPT_STR,     0,                          NULL,            &locate_handler,
+		{ .ref.str_val = &cfg.locate_prg }                                                                     },
 	{ "rulerformat", "ruf",  OPT_STR,     0,                          NULL,            &rulerformat_handler,
 		{ .ref.str_val = &cfg.ruler_format }                                                                   },
 	{ "runexec",     "",     OPT_BOOL,    0,                          NULL,            &runexec_handler,
@@ -825,6 +828,12 @@ lines_handler(OPT_OP op, optval_t val)
 	/* Need to update value of option in case it was corrected above. */
 	val.int_val = cfg.lines;
 	set_option("lines", val);
+}
+
+static void
+locate_handler(OPT_OP op, optval_t val)
+{
+	(void)replace_string(&cfg.locate_prg, val.str_val);
 }
 
 static void

commit 26a4bd2c15d2b2a384e33879df259502ce7d0246

Fix :locate never escaping its arguments
Escaping should be done unless the first one starts with a dash.

Got broken in f3690f52a48c4945d83f2820464bbadcff51690a:

    Add 'locateprg' option to specify command format

    %a can be used to specify placement of :locate command arguments, if
    not used, it will be implicitly added after a space to the expanded
    value of the 'locateprg' option.

A regression in 0.7.6.

src/menus/locate_menu.c
@@ -42,24 +42,25 @@ show_locate_menu(view_t *view, const char args[])
 	char *cmd;
 	char *margs;
 	int save_msg;
-	custom_macro_t macros[] = {
-		[M_a] = { .letter = 'a', .value = args, .uses_left = 1, .group = -1 },
-
-		[M_u] = { .letter = 'u', .value = "",   .uses_left = 1, .group = -1 },
-		[M_U] = { .letter = 'U', .value = "",   .uses_left = 1, .group = -1 },
-	};
 
 	static menu_data_t m;
 	margs = (args[0] == '-') ? strdup(args) : shell_like_escape(args, 0);
 	menus_init_data(&m, view, format_str("Locate %s", margs),
 			strdup("No files found"));
-	free(margs);
+
+	custom_macro_t macros[] = {
+		[M_a] = { .letter = 'a', .value = margs, .uses_left = 1, .group = -1 },
+
+		[M_u] = { .letter = 'u', .value = "",    .uses_left = 1, .group = -1 },
+		[M_U] = { .letter = 'U', .value = "",    .uses_left = 1, .group = -1 },
+	};
 
 	m.stashable = 1;
 	m.execute_handler = &execute_locate_cb;
 	m.key_handler = &menus_def_khandler;
 
 	cmd = ma_expand_custom(cfg.locate_prg, ARRAY_LEN(macros), macros, MA_NOOPT);
+	free(margs);
 
 	MacroFlags flags = MF_NONE;
 	if(macros[M_u].explicit_use)

tests/misc/menus_misc.c
@@ -5,6 +5,7 @@
 
 #include <test-utils.h>
 
+#include "../../src/compat/fs_limits.h"
 #include "../../src/cfg/config.h"
 #include "../../src/engine/keys.h"
 #include "../../src/modes/menu.h"
@@ -113,6 +114,32 @@ TEST(menu_is_turned_into_cv)
 	assert_string_equal("!echo existing-files/a%M", lwin.custom.title);
 
 	undo_teardown();
+}
+
+TEST(locate_menu_can_escape_args, IF(not_windows))
+{
+	char script_path[PATH_MAX + 1];
+	make_abs_path(script_path, sizeof(script_path), SANDBOX_PATH, "script", NULL);
+
+	char locate_prg[PATH_MAX + 1];
+	snprintf(locate_prg, sizeof(locate_prg), "%s", script_path);
+	update_string(&cfg.locate_prg, locate_prg);
+
+	create_executable(SANDBOX_PATH "/script");
+	make_file(SANDBOX_PATH "/script",
+			"#!/bin/sh\n"
+			"for arg; do echo \"$arg\"; done\n");
+
+	assert_success(exec_commands("locate a  b", &lwin, CIT_COMMAND));
+	assert_int_equal(1, menu_get_current()->len);
+	assert_string_equal("a  b", menu_get_current()->items[0]);
+
+	assert_success(exec_commands("locate -a  b", &lwin, CIT_COMMAND));
+	assert_int_equal(2, menu_get_current()->len);
+	assert_string_equal("-a", menu_get_current()->items[0]);
+	assert_string_equal("b", menu_get_current()->items[1]);
+
+	assert_success(remove(script_path));
 }
 
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */