8f8bf83c58431a840ba49cf880cab33036f55dd9
xaizek <xaizek@posteo.net>
2023-02-15 18:11:30 +0200

    Extract ops.c:ops_runs_in_bg() function
    Checks whether operation is a background one.
    
    This is more readable then having this check reappear in several places.

--- src/ops.c
+++ src/ops.c
@@ -144,6 +144,7 @@ static int ui_cancellation_hook(void *arg);
 #ifndef _WIN32
 static OpsResult run_operation_command(ops_t *ops, char cmd[], int cancellable);
 #endif
+static int ops_runs_in_bg(const ops_t *ops);
 static int bg_cancellation_hook(void *arg);
 static OpsResult result_from_code(int exit_code);
 
@@ -298,7 +299,7 @@ OpsResult
 perform_operation(OPS op, ops_t *ops, void *data, const char src[],
 		const char dst[])
 {
-	if(ops != NULL && ops->bg)
+	if(ops_runs_in_bg(ops))
 	{
 		/* Not reporting events from background jobs. */
 		return op_funcs[op](ops, data, src, dst);
@@ -1063,7 +1064,7 @@ exec_io_op(ops_t *ops, IoRes (*func)(io_args_t *), io_args_t *args,
 
 	if(cancellable)
 	{
-		if(ops != NULL && ops->bg)
+		if(ops_runs_in_bg(ops))
 		{
 			args->cancellation.arg = ops->bg_op;
 			args->cancellation.hook = &bg_cancellation_hook;
@@ -1270,7 +1271,7 @@ run_operation_command(ops_t *ops, char cmd[], int cancellable)
 		return result_from_code(bg_and_wait_for_errors(cmd, &no_cancellation));
 	}
 
-	if(ops != NULL && ops->bg)
+	if(ops_runs_in_bg(ops))
 	{
 		const cancellation_t bg_cancellation_info = {
 			.arg = ops->bg_op,
@@ -1291,6 +1292,14 @@ run_operation_command(ops_t *ops, char cmd[], int cancellable)
 }
 
 #endif
+
+/* Checks whether operation is a background one.  The parameter can be NULL.
+ * Returns non-zero for background operations. */
+static int
+ops_runs_in_bg(const ops_t *ops)
+{
+	return (ops != NULL && ops->bg);
+}
 
 /* Implementation of cancellation hook for background tasks. */
 static int
