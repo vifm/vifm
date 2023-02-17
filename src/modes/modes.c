vifm/vifm

commit 5497f13d71548631ce4ace8146d166d71adad6b7

Fix :keepsel not preserving statusbar messages
They were lost because save_msg flag wasn't checked before displaying
message about selected files.

src/modes/modes.c
@@ -187,7 +187,8 @@ modes_statusbar_update(void)
 	{
 		/* Status bar is used for special purposes. */
 	}
-	else if(curr_view->selected_files || vle_mode_is(VISUAL_MODE))
+	else if(!curr_stats.save_msg &&
+			(curr_view->selected_files || vle_mode_is(VISUAL_MODE)))
 	{
 		print_selected_msg();
 	}

src/ui/ui.c
@@ -771,7 +771,7 @@ update_start(UpdateType update_kind)
 
 	ui_stat_update(curr_view, 0);
 
-	if(!ui_sb_multiline())
+	if(curr_stats.save_msg == 0 && !ui_sb_multiline())
 	{
 		if(curr_view->selected_files)
 		{
@@ -795,6 +795,10 @@ update_start(UpdateType update_kind)
 	if(curr_stats.save_msg == 0)
 	{
 		ui_sb_clear();
+	}
+	else
+	{
+		ui_sb_msg(NULL);
 	}
 
 	if(vle_mode_is(VIEW_MODE) ||

commit b55977eadc0a479451b57573a6555ab92379e2ec

Fix input bar not being updated in visual mode
A regression caused by 5497f13d71548631ce4ace8146d166d71adad6b7:

    Fix :keepsel not preserving statusbar messages

    They were lost because save_msg flag wasn't checked before
    displaying message about selected files.

If some action (e.g. a search) shows a message the whole statusbar is
not updated.  Update just the input bar in visual mode in that case.

src/modes/modes.c
@@ -188,9 +188,16 @@ modes_post(void)
 void
 modes_statusbar_update(void)
 {
-	if(ANY(vle_mode_is, MORE_MODE, CMDLINE_MODE, NAV_MODE) ||
-			curr_stats.save_msg != 0)
+	if(ANY(vle_mode_is, MORE_MODE, CMDLINE_MODE, NAV_MODE))
 	{
+		return;
+	}
+	else if(curr_stats.save_msg != 0)
+	{
+		if(vle_mode_is(VISUAL_MODE))
+		{
+			update_vmode_input();
+		}
 		return;
 	}
 