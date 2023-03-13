vifm/vifm

commit 2336aed02d754ed39ef6b3b35d1004593442b6b5

Don't invoke line change on entering cmdline mode
This allows postponing initialization of mode-specific data in
prepare_cmdline_mode() and do it later.

I don't see a case where it's necessary now and it's actually removes
extra redrawing, so let's try it this way.  Potentially handing input
change can be useful to trigger something interactive.

src/modes/cmdline.c
@@ -797,7 +797,7 @@ prepare_cmdline_mode(const wchar_t prompt[], const wchar_t initial[],
 	input_stat.curs_pos += input_stat.prompt_wid;
 
 	update_cmdline_size();
-	update_cmdline_text(&input_stat);
+	draw_cmdline_text(&input_stat);
 
 	curr_stats.save_msg = 1;
 

commit 6818f54607627334de980759db13b05faaca6b42

Fix interactive search not done after c in :hist/
Caused by 2336aed02d754ed39ef6b3b35d1004593442b6b5:

    Don't invoke line change on entering cmdline mode

    This allows postponing initialization of mode-specific data in
    prepare_cmdline_mode() and do it later.

    I don't see a case where it's necessary now and it's actually removes
    extra redrawing, so let's try it this way.  Potentially handing input
    change can be useful to trigger something interactive.

Need to invoke line change handler for non-empty input to handle it.

src/modes/cmdline.c
@@ -605,7 +605,12 @@ modcline_enter(CmdLineSubmode sub_mode, const char initial[])
 			"Use modcline_in_menu() for CLS_MENU_* submodes.");
 	assert(sub_mode != CLS_PROMPT &&
 			"Use modcline_prompt() for CLS_PROMPT submode.");
-	(void)enter_submode(sub_mode, initial, /*reenter=*/0);
+	if(enter_submode(sub_mode, initial, /*reenter=*/0) == 0 &&
+			!is_null_or_empty(initial))
+	{
+		/* Trigger handling of input if its initial value isn't empty. */
+		update_cmdline_text(&input_stat);
+	}
 }
 
 void

tests/menus/history.c
@@ -13,6 +13,7 @@
 #include "../../src/ui/statusbar.h"
 #include "../../src/ui/ui.h"
 #include "../../src/cmd_core.h"
+#include "../../src/filelist.h"
 #include "../../src/status.h"
 
 static line_stats_t *stats;
@@ -214,6 +215,27 @@ TEST(bsearch_hist_sets_search_direction_on_editing)
 	(void)vle_keys_exec(WK_c);
 	assert_true(curr_stats.last_search_backward);
 	(void)vle_keys_exec_timed_out(WK_ESC);
+}
+
+TEST(editing_search_performs_interactive_search)
+{
+	cfg.inc_search = 1;
+	opt_handlers_setup();
+
+	make_abs_path(lwin.curr_dir, sizeof(lwin.curr_dir), TEST_DATA_PATH, "", NULL);
+	populate_dir_list(&lwin, 0);
+	assert_int_equal(0, lwin.list_pos);
+
+	hists_search_save("var");
+	assert_success(show_fsearchhistory_menu(&lwin));
+
+	(void)vle_keys_exec(WK_c);
+	assert_int_equal(9, lwin.list_pos);
+	(void)vle_keys_exec_timed_out(WK_ESC);
+	assert_int_equal(0, lwin.list_pos);
+
+	opt_handlers_teardown();
+	cfg.inc_search = 0;
 }
 
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */