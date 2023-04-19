c3771732858c66e8d262a360924a312127d69835
xaizek <xaizek@posteo.net>
2023-04-19 14:47:09 +0300

    Don't try to clear graphics in menu-like modes
    The graphics should have been hidden on entering the mode.

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -2832,8 +2832,11 @@ ui_qv_cleanup_if_needed(void)
 void
 ui_hide_graphics(void)
 {
-	ui_qv_cleanup_if_needed();
-	modview_hide_graphics();
+	if(!modes_is_menu_like())
+	{
+		ui_qv_cleanup_if_needed();
+		modview_hide_graphics();
+	}
 }
 
 void
