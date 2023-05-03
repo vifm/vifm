3d5e127323a47531a07e44a3e8a9d0c7266a8198
xaizek <xaizek@posteo.net>
2023-05-03 21:08:47 +0300

    Test :history command

--- tests/commands/misc.c
+++ tests/commands/misc.c
@@ -779,6 +779,131 @@ TEST(registers_command)
 	regs_reset();
 }
 
+TEST(history_command)
+{
+	modes_init();
+
+	curr_stats.load_stage = -1;
+
+	histories_init(/*size=*/5);
+	hists_commands_save("cmd");
+	hists_menucmd_save("mcmd");
+	hists_exprreg_save("ereg");
+	hists_search_save("search");
+	hists_prompt_save("prompt");
+	hists_filter_save("filter");
+	init_view_list(&lwin);
+	flist_hist_setup(&lwin, "/", NULL, /*rel_pos=*/0, /*timestamp=*/1);
+
+	/* Symbols. */
+
+	assert_success(cmds_dispatch1("history :", &lwin, CIT_COMMAND));
+	assert_string_equal("Command Line History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	assert_success(cmds_dispatch1("history /", &lwin, CIT_COMMAND));
+	assert_string_equal("Search History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	assert_success(cmds_dispatch1("history @", &lwin, CIT_COMMAND));
+	assert_string_equal("Prompt History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	assert_success(cmds_dispatch1("history ?", &lwin, CIT_COMMAND));
+	assert_string_equal("Search History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	assert_success(cmds_dispatch1("history =", &lwin, CIT_COMMAND));
+	assert_string_equal("Filter History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	assert_success(cmds_dispatch1("history .", &lwin, CIT_COMMAND));
+	assert_string_equal("Directory History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	/* Keywords. */
+
+	assert_success(cmds_dispatch1("history cmd", &lwin, CIT_COMMAND));
+	assert_string_equal("Command Line History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	assert_success(cmds_dispatch1("history search", &lwin, CIT_COMMAND));
+	assert_string_equal("Search History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	assert_success(cmds_dispatch1("history fsearch", &lwin, CIT_COMMAND));
+	assert_string_equal("Search History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	assert_success(cmds_dispatch1("history input", &lwin, CIT_COMMAND));
+	assert_string_equal("Prompt History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	assert_success(cmds_dispatch1("history bsearch", &lwin, CIT_COMMAND));
+	assert_string_equal("Search History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	assert_success(cmds_dispatch1("history filter", &lwin, CIT_COMMAND));
+	assert_string_equal("Filter History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	assert_success(cmds_dispatch1("history dir", &lwin, CIT_COMMAND));
+	assert_string_equal("Directory History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	assert_success(cmds_dispatch1("history exprreg", &lwin, CIT_COMMAND));
+	assert_string_equal("Expression Register History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	assert_success(cmds_dispatch1("history mcmd", &lwin, CIT_COMMAND));
+	assert_string_equal("Menu Command History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	/* Shortcuts. */
+
+	assert_success(cmds_dispatch1("history c", &lwin, CIT_COMMAND));
+	assert_string_equal("Command Line History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	assert_success(cmds_dispatch1("history s", &lwin, CIT_COMMAND));
+	assert_string_equal("Search History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	assert_success(cmds_dispatch1("history f", &lwin, CIT_COMMAND));
+	assert_string_equal("Search History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	assert_success(cmds_dispatch1("history i", &lwin, CIT_COMMAND));
+	assert_string_equal("Prompt History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	assert_success(cmds_dispatch1("history b", &lwin, CIT_COMMAND));
+	assert_string_equal("Search History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	assert_success(cmds_dispatch1("history fi", &lwin, CIT_COMMAND));
+	assert_string_equal("Filter History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	assert_success(cmds_dispatch1("history d", &lwin, CIT_COMMAND));
+	assert_string_equal("Directory History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	assert_success(cmds_dispatch1("history e", &lwin, CIT_COMMAND));
+	assert_string_equal("Expression Register History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	assert_success(cmds_dispatch1("history mc", &lwin, CIT_COMMAND));
+	assert_string_equal("Menu Command History", menu_get_current()->title);
+	assert_success(cmds_dispatch1("q", &lwin, CIT_MENU_COMMAND));
+
+	/* Invalid input. */
+
+	assert_failure(cmds_dispatch1("history x", &lwin, CIT_COMMAND));
+
+	curr_stats.load_stage = 0;
+}
+
 static void
 strings_list_is(const strlist_t expected, const strlist_t actual)
 {
