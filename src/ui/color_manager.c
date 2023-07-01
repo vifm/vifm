304eeed1edba5ddae4f1b092bd78df8cfef5fb51
xaizek <xaizek@posteo.net>
2023-07-01 20:25:15 +0300

    Fix slowdown on consuming lots of color pairs
    Primarily visible when previewing images via chafa or something like it:
    linear lookup among tens of thousands of pairs was way too slow.
    
    Construct a map "(fg, bg) -> index" in a mostly sorted array which can
    have unsorted tail to avoid sorting it all the time.  Assumed that
    insertion-sort round would also be quite slow, haven't tried it.

--- src/ui/color_manager.c
+++ src/ui/color_manager.c
@@ -21,16 +21,34 @@
 #include <assert.h> /* assert() */
 #include <stddef.h> /* NULL */
 
+#include "../compat/reallocarray.h"
 #include "../utils/macros.h"
+#include "../utils/utils.h"
 #include "colors.h"
 
 /* Number of color pairs preallocated by curses library. */
 #define PREALLOCATED_COUNT 1
 
+/* Maximum allowed number of unsorted color pairs. */
+#define MAX_UNSORTED_PAIRS  512
+/* Initial size of the pair cache. */
+#define START_CACHE_SIZE    128
+/* Size factor for growing pair cache. */
+#define CACHE_GROWTH_FACTOR 2
+
+/* Pair cache entry that maps foreground-background pair to its index. */
+typedef struct
+{
+	int fg;   /* Foreground color. */
+	int bg;   /* Background color. */
+	int pair; /* Index. */
+}
+pair_cache_t;
+
 static int find_pair(int fg, int bg);
-static int color_pair_matches(int pair, int fg, int bg);
 static int allocate_pair(int fg, int bg);
 static int compress_pair_space(void);
+static int pair_cache_cmp(const void *a, const void *b);
 
 /* Number of color pairs available. */
 static int avail_pairs;
@@ -46,6 +64,39 @@ static int initialized;
 
 /* Default foreground and background colors. */
 static int def_fg, def_bg;
+
+/* Pair cache to avoid expensive linear lookup of thousands of elements. */
+static pair_cache_t *pair_cache;
+/* Current size of the cache. */
+static int pair_cache_size;
+/* Current maximum size of the cache. */
+static int pair_cache_capacity;
+/* Size of the sorted prefix of the cache.  The unsorted tail needs to be looked
+ * up linearly. */
+static int pair_cache_sorted;
+
+static pair_cache_t *
+add_to_cache(void)
+{
+	if(pair_cache_size == pair_cache_capacity)
+	{
+		/* Start small, then grow cache size fast. */
+		int new_capacity = (pair_cache_capacity == 0)
+		                 ? START_CACHE_SIZE
+		                 : pair_cache_capacity*CACHE_GROWTH_FACTOR;
+
+		void *ptr = reallocarray(pair_cache, new_capacity, sizeof(*pair_cache));
+		if(ptr == NULL)
+		{
+			return NULL;
+		}
+
+		pair_cache_capacity = new_capacity;
+		pair_cache = ptr;
+	}
+
+	return &pair_cache[pair_cache_size++];
+}
 
 void
 colmgr_init(const colmgr_conf_t *conf_init)
@@ -77,6 +128,9 @@ colmgr_reset(void)
 
 	used_pairs = PREALLOCATED_COUNT;
 	avail_pairs = conf.max_color_pairs - used_pairs;
+
+	pair_cache_size = 0;
+	pair_cache_sorted = 0;
 }
 
 int
@@ -118,26 +172,29 @@ find_pair(int fg, int bg)
 		bg = def_bg;
 	}
 
-	int i;
-	for(i = PREALLOCATED_COUNT; i < used_pairs; ++i)
+	/* The check is to not call bsearch() on NULL. */
+	if(pair_cache_sorted > 0)
 	{
-		if(color_pair_matches(i, fg, bg))
+		pair_cache_t key = { .fg = fg, .bg = bg };
+		pair_cache_t *needle = bsearch(&key, pair_cache, pair_cache_sorted,
+				sizeof(*pair_cache), &pair_cache_cmp);
+		if(needle != NULL)
 		{
-			return i;
+			return needle->pair;
+		}
+	}
+
+	int i;
+	for(i = pair_cache_sorted; i < pair_cache_size; ++i)
+	{
+		pair_cache_t *entry = &pair_cache[i];
+		if(entry->fg == fg && entry->bg == bg)
+		{
+			return entry->pair;
 		}
 	}
 
 	return -1;
-}
-
-/* Checks whether color pair number pair has specified foreground (fg) and
- * background (bg) colors. */
-static int
-color_pair_matches(int pair, int fg, int bg)
-{
-	int pair_fg, pair_bg;
-	conf.pair_content(pair, &pair_fg, &pair_bg);
-	return (pair_fg == fg && pair_bg == bg);
 }
 
 /* Allocates new color pair.  Returns new pair index, or -1 on failure. */
@@ -156,6 +213,25 @@ allocate_pair(int fg, int bg)
 	if(conf.init_pair(used_pairs, fg, bg) != 0)
 	{
 		return -1;
+	}
+
+	pair_cache_t *cache_entry = add_to_cache();
+	if(cache_entry == NULL)
+	{
+		return -1;
+	}
+
+	cache_entry->fg = fg;
+	cache_entry->bg = bg;
+	cache_entry->pair = used_pairs;
+
+	/* Color pairs are not resorted on every allocation to avoid performance
+	 * hit. */
+	if(pair_cache_size - pair_cache_sorted > MAX_UNSORTED_PAIRS)
+	{
+		safe_qsort(pair_cache, pair_cache_size, sizeof(*pair_cache),
+				&pair_cache_cmp);
+		pair_cache_sorted = pair_cache_size;
 	}
 
 	--avail_pairs;
@@ -214,7 +290,36 @@ compress_pair_space(void)
 	used_pairs = j;
 	avail_pairs = conf.max_color_pairs - used_pairs;
 
+	pair_cache_size = 0;
+	for(i = PREALLOCATED_COUNT; i < used_pairs; ++i)
+	{
+		int fg, bg;
+		if(conf.pair_content(i, &fg, &bg) == 0)
+		{
+			pair_cache_t *cache_entry = add_to_cache();
+			if(cache_entry != NULL)
+			{
+				cache_entry->fg = fg;
+				cache_entry->bg = bg;
+				cache_entry->pair = i;
+			}
+		}
+	}
+
+	safe_qsort(pair_cache, pair_cache_size, sizeof(*pair_cache), &pair_cache_cmp);
+	pair_cache_sorted = pair_cache_size;
+
 	return 0;
+}
+
+/* Comparator for pair_cache_t.  Returns values <0, =0 and >0 to signify that
+ * first argument is less than, equal or greater than the second one. */
+static int
+pair_cache_cmp(const void *a, const void *b)
+{
+	const pair_cache_t *s = a;
+	const pair_cache_t *t = b;
+	return (s->fg != t->fg ? s->fg - t->fg : s->bg - t->bg);
 }
 
 int
