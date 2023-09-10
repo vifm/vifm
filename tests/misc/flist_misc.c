dbcca29d6bcada7f8d67f18dbfc24db8a537039a
xaizek <xaizek@posteo.net>
2023-09-10 15:21:34 +0300

    Try to fix flaky fentry_get_recalculates_size #2
    It still happened with 10ms wait time after commit
    35a5603e4b0d1d8181be6ac802bfad84adb53a40:
    
        Try to fix flaky fentry_get_recalculates_size test
    
        Try to wait for file system changes to be visible, they seem
        to be postponed on Windows (at least in Wine, but Windows can
        do that too).

--- tests/misc/flist_misc.c
+++ tests/misc/flist_misc.c
@@ -38,6 +38,10 @@
 			} \
 			usleep(1000/*us*/); \
 		} \
+		if(duration == duration_ms) \
+		{ \
+			assert_fail("Timed out waiting for: " # expr); \
+		} \
 	} \
 	while(0)
 
@@ -368,20 +372,20 @@ TEST(fentry_get_recalculates_size)
 	copy_file(TEST_DATA_PATH "/various-sizes/block-size-file",
 			SANDBOX_PATH "/dir/file");
 	WAIT_FOR((dir.mtime += 1000, fentry_get_size(&lwin, &dir) == 8192),
-			10/*ms*/);
+			50/*ms*/);
 	assert_ulong_equal(8192, fentry_get_size(&lwin, &dir));
 	assert_ulong_equal(0, fentry_get_size(&lwin, &subdir));
 
 	copy_file(TEST_DATA_PATH "/various-sizes/block-size-file",
 			SANDBOX_PATH "/dir/subdir/file");
 	WAIT_FOR((subdir.mtime += 1000, fentry_get_size(&lwin, &subdir) == 8192),
-			10/*ms*/);
+			50/*ms*/);
 	assert_ulong_equal(8192, fentry_get_size(&lwin, &subdir));
 	assert_ulong_equal(16384, fentry_get_size(&lwin, &dir));
 
 	assert_success(remove(SANDBOX_PATH "/dir/subdir/file"));
 	WAIT_FOR((subdir.mtime += 1000, fentry_get_size(&lwin, &subdir) == 0),
-			10/*ms*/);
+			50/*ms*/);
 	assert_ulong_equal(0, fentry_get_size(&lwin, &subdir));
 	assert_ulong_equal(8192, fentry_get_size(&lwin, &dir));
 
