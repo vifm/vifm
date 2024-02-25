vifm/vifm

commit b94eeed3018b32f72295e88408238f7245903d7e

Optimize compare.c:put_file_id() a bit
Avoid doing trie_set() if there is no need for it.

src/compare.c
@@ -952,16 +952,25 @@ put_file_id(trie_t *trie, const char path[], const char fingerprint[], int id,
 		CompareType ct)
 {
 	compare_record_t *const record = malloc(sizeof(*record));
-	void *data = NULL;
-	(void)trie_get(trie, fingerprint, &data);
 
 	record->id = id;
-	record->next = data;
+	record->next = NULL;
 
 	/* Comparison by contents is the only one when we need to resolve fingerprint
 	 * conflicts. */
 	record->path = (ct == CT_CONTENTS ? strdup(path) : NULL);
 
+	/* Just add new entry to the list if something is already there. */
+	void *data = NULL;
+	(void)trie_get(trie, fingerprint, &data);
+	compare_record_t *prev = data;
+	if(prev != NULL)
+	{
+		prev->next = record;
+		return;
+	}
+
+	/* Otherwise we're the head of the list. */
 	if(trie_set(trie, fingerprint, record) < 0)
 	{
 		free(record->path);

commit d0deea1673c3867ec96f34cd0aca67ca47130f4e

Fix :compare reporting identical files as changed
When there were more than a couple of files with identical size and
4096-byte prefix.

A regression in 0.13-beta caused by commit
b94eeed3018b32f72295e88408238f7245903d7e:

    Optimize compare.c:put_file_id() a bit

    Avoid doing trie_set() if there is no need for it.

List of conflicting files was never longer than 2 because every append
operation dropped tail of the list (no memory leak because the list
isn't used for memory-management).

Thanks to Alexandr Keyp (a.k.a. IAmKapuze).

Fixes #984 on GitHub.

THANKS
@@ -20,6 +20,7 @@ Alborz Jafari
 Alejandro Pulver (alepulver)
 Aleksandr Vysotskiy (loki1368)
 aleksejrs
+Alexandr Keyp (IAmKapuze)
 Alexandru Geana (alegen)
 Allan Neal (akneal)
 Allison McNulty (allora)

src/compare.c
@@ -1228,6 +1228,7 @@ put_file_id(trie_t *trie, const char path[], const char fingerprint[], int id,
 	compare_record_t *prev = data;
 	if(prev != NULL)
 	{
+		record->next = prev->next;
 		prev->next = record;
 		return;
 	}

tests/misc/compare.c
@@ -4,7 +4,7 @@
 #include <unistd.h> /* rmdir() */
 
 #include <stdio.h> /* FILE fopen() fwrite() fclose() remove() */
-#include <string.h> /* strcpy() */
+#include <string.h> /* memset() strcpy() */
 
 #include <test-utils.h>
 
@@ -445,6 +445,51 @@ TEST(content_difference_is_detected)
 
 	remove_file(SANDBOX_PATH "/a/two-lines");
 	remove_file(SANDBOX_PATH "/b/two-lines");
+	remove_dir(SANDBOX_PATH "/a");
+	remove_dir(SANDBOX_PATH "/b");
+}
+
+TEST(files_with_identical_fingerprints_are_handled_well)
+{
+	char contents[32*1024];
+	memset(contents, ' ', sizeof(contents));
+	contents[sizeof(contents) - 1] = '\0';
+
+	/* Files are pair-wise identical (1-1, 2-2, 3-3), but all of them have the
+	 * same fingerprint because of the same size and prefix (4096 first bytes, but
+	 * trying to allow for changes here). */
+	create_dir(SANDBOX_PATH "/a");
+	create_dir(SANDBOX_PATH "/b");
+	contents[sizeof(contents)/2] = '1';
+	make_file(SANDBOX_PATH "/a/1", contents);
+	make_file(SANDBOX_PATH "/b/1", contents);
+	contents[sizeof(contents)/2] = '2';
+	make_file(SANDBOX_PATH "/a/2", contents);
+	make_file(SANDBOX_PATH "/b/2", contents);
+	contents[sizeof(contents)/2] = '3';
+	make_file(SANDBOX_PATH "/a/3", contents);
+	make_file(SANDBOX_PATH "/b/3", contents);
+
+	strcpy(lwin.curr_dir, SANDBOX_PATH "/a");
+	strcpy(rwin.curr_dir, SANDBOX_PATH "/b");
+	compare_two_panes(CT_CONTENTS, LT_ALL, CF_SHOW | CF_GROUP_PATHS);
+
+	assert_int_equal(3, lwin.list_rows);
+	assert_int_equal(3, rwin.list_rows);
+
+	assert_int_equal(1, lwin.dir_entry[0].id);
+	assert_int_equal(1, rwin.dir_entry[0].id);
+	assert_int_equal(2, lwin.dir_entry[1].id);
+	assert_int_equal(2, rwin.dir_entry[1].id);
+	assert_int_equal(3, lwin.dir_entry[2].id);
+	assert_int_equal(3, rwin.dir_entry[2].id);
+
+	remove_file(SANDBOX_PATH "/a/1");
+	remove_file(SANDBOX_PATH "/b/1");
+	remove_file(SANDBOX_PATH "/a/2");
+	remove_file(SANDBOX_PATH "/b/2");
+	remove_file(SANDBOX_PATH "/a/3");
+	remove_file(SANDBOX_PATH "/b/3");
 	remove_dir(SANDBOX_PATH "/a");
 	remove_dir(SANDBOX_PATH "/b");
 }