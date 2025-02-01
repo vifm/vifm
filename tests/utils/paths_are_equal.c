vifm/vifm

commit 7f32f8a45b32b36a9d927d82985ab28ebf5264a7

Handle relative paths better in paths_are_equal()
"a" and "./a" were canonicalized to themselves.  Fixing this in
canonicalize_path() to always add "./" breaks its uses for things like
patterns in auto-commands.  Always stripping leading "./" can
potentially result in running a command with "-path" instead of
"./-path".  Although not sure the latter is possible, decided not to
risk.

src/utils/path.c
@@ -44,6 +44,8 @@
 #include "str.h"
 #include "utils.h"
 
+static void make_canonic_path(const char directory[], char buf[],
+		size_t buf_size, int strict_rel_paths);
 static int skip_dotdir_if_any(const char *path[], int has_parent);
 static const char * find_home_tail(const char path[]);
 static char * try_replace_tilde(const char path[]);
@@ -104,14 +106,25 @@ paths_are_equal(const char s[], const char t[])
 	char s_can[strlen(s) + 8];
 	char t_can[strlen(t) + 8];
 
-	canonicalize_path(s, s_can, sizeof(s_can));
-	canonicalize_path(t, t_can, sizeof(t_can));
+	make_canonic_path(s, s_can, sizeof(s_can), /*strict_rel_paths=*/1);
+	make_canonic_path(t, t_can, sizeof(t_can), /*strict_rel_paths=*/1);
 
 	return stroscmp(s_can, t_can) == 0;
 }
 
 void
 canonicalize_path(const char directory[], char buf[], size_t buf_size)
+{
+	make_canonic_path(directory, buf, buf_size, /*strict_rel_paths=*/0);
+}
+
+/* Removes excess slashes, "../" and "./" from the path.  buf will always
+ * contain trailing forward slash (empty result will become "./").  Non-zero
+ * strict_rel_paths results in relative paths getting "./" prefix if they
+ * didn't have it, which doesn't happen otherwise. */
+static void
+make_canonic_path(const char directory[], char buf[], size_t buf_size,
+		int strict_rel_paths)
 {
 	/* Source string pointer. */
 	const char *p = directory;
@@ -133,7 +146,13 @@ canonicalize_path(const char directory[], char buf[], size_t buf_size)
 		}
 		buf = q + 1;
 	}
+	else /* note this else */
 #endif
+	if(strict_rel_paths)
+	{
+		strcpy(buf, "./");
+		q += 2;;
+	}
 
 	while(*p != '\0' && (size_t)((q + 1) - buf) < buf_size - 1U)
 	{

tests/utils/paths_are_equal.c
@@ -1,0 +1,13 @@
+#include <stic.h>
+
+#include "../../src/utils/path.h"
+
+TEST(dotdirs_in_relative_paths_normalized)
+{
+	assert_true(paths_are_equal("a", "./a"));
+	assert_true(paths_are_equal("./a", "././a"));
+	assert_true(paths_are_equal("a", "./././a"));
+}
+
+/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
+/* vim: set cinoptions+=t0 filetype=c : */

commit 631b3cb6950e3c56d5a23449bb43b330fa55e912

Fix abs paths comparing equal to similar rel paths
Regression in 0.14-beta introduced by 7f32f8a45b:

    Handle relative paths better in paths_are_equal()

    "a" and "./a" were canonicalized to themselves.  Fixing this in
    canonicalize_path() to always add "./" breaks its uses for things
    like patterns in auto-commands.  Always stripping leading "./" can
    potentially result in running a command with "-path" instead of
    "./-path".  Although not sure the latter is possible, decided not to
    risk.

src/utils/path.c
@@ -148,10 +148,10 @@ make_canonic_path(const char directory[], char buf[], size_t buf_size,
 	}
 	else /* note this else */
 #endif
-	if(strict_rel_paths)
+	if(strict_rel_paths && !is_path_absolute(directory))
 	{
 		strcpy(buf, "./");
-		q += 2;;
+		q += 2;
 	}
 
 	while(*p != '\0' && (size_t)((q + 1) - buf) < buf_size - 1U)

tests/utils/paths_are_equal.c
@@ -4,6 +4,15 @@
 
 TEST(dotdirs_in_relative_paths_normalized)
 {
+	assert_false(paths_are_equal("/a", "./a"));
+	assert_false(paths_are_equal("/a", "a"));
+
+	assert_false(paths_are_equal("/a/b/c", "./a/b/c"));
+	assert_false(paths_are_equal("/a/b/c", "a/b/c"));
+
+	assert_true(paths_are_equal("/a", "////a"));
+	assert_true(paths_are_equal("/a/b/./c", "/a//b/c/d/.."));
+
 	assert_true(paths_are_equal("a", "./a"));
 	assert_true(paths_are_equal("./a", "././a"));
 	assert_true(paths_are_equal("a", "./././a"));