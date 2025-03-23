vifm/vifm

commit f22d57d619c34c570a0c859bb8b2589d55932f67

Fix %i without " &" breaking some applications
Because it was closing their standard output and error.  There was a
lame implementation and some issues with applications started within
Vifm being closed on Vifm's exit.  With current way of running
background commands, this doesn't seem to happen and that code isn't
needed at all.

Thanks to Daniel J. Perry (a.k.a. BioBox).

Fixes #823 on GitHub.

src/running.c
@@ -127,7 +127,6 @@ static int try_run_with_filetype(view_t *view, const assoc_records_t assocs,
 static void output_to_statusbar(const char cmd[], view_t *view,
 		MacroFlags flags);
 static int output_to_preview(view_t *view, const char cmd[], MacroFlags flags);
-static void output_to_nowhere(const char cmd[], view_t *view, MacroFlags flags);
 static void run_in_split(const view_t *view, const char cmd[], int vert_split);
 static void path_handler(const char line[], void *arg);
 static void line_handler(const char line[], void *arg);
@@ -1247,24 +1246,31 @@ rn_ext(view_t *view, const char cmd[], const char title[], MacroFlags flags,
 	}
 	else if(ma_flags_present(flags, MF_IGNORE))
 	{
-		*save_msg = 0;
-		if(bg)
+		/* TODO: consider merging with rn_start_bg_command(). */
+
+		const int supply_input = ma_flags_present(flags, MF_PIPE_FILE_LIST)
+		                      || ma_flags_present(flags, MF_PIPE_FILE_LIST_Z);
+		FILE *input = NULL;
+		FILE **input_ptr = (supply_input ? &input : NULL);
+		int error = 0;
+
+		setup_shellout_env();
+		if(bg_run_external(cmd, 1, SHELL_BY_USER, input_ptr) != 0)
 		{
-			int error;
-
-			setup_shellout_env();
-			error = (bg_run_external(cmd, 1, SHELL_BY_USER, NULL) != 0);
-			cleanup_shellout_env();
-
-			if(error)
-			{
-				ui_sb_errf("Failed to start in bg: %s", cmd);
-				*save_msg = 1;
-			}
+			error = 1;
 		}
-		else
+		cleanup_shellout_env();
+
+		if(input != NULL)
 		{
-			output_to_nowhere(cmd, view, flags);
+			const int null_sep = ma_flags_present(flags, MF_PIPE_FILE_LIST_Z);
+			write_marked_paths(input, view, null_sep);
+			fclose(input);
+		}
+
+		if(error)
+		{
+			show_error_msgf("Trouble running command", "Unable to run: %s", cmd);
 		}
 		return -1;
 	}
@@ -1361,39 +1367,6 @@ output_to_preview(view_t *view, const char cmd[], MacroFlags flags)
 	return 0;
 }
 
-/* Executes the cmd ignoring its output. */
-static void
-output_to_nowhere(const char cmd[], view_t *view, MacroFlags flags)
-{
-	FILE *file, *err;
-	FILE *input_tmp = make_in_file(view, flags);
-
-	setup_shellout_env();
-	int error = 0;
-	if(bg_run_and_capture((char *)cmd, 1, input_tmp, &file, &err) == (pid_t)-1)
-	{
-		error = 1;
-	}
-	cleanup_shellout_env();
-
-	if(input_tmp != NULL)
-	{
-		fclose(input_tmp);
-	}
-
-	if(error)
-	{
-		show_error_msgf("Trouble running command", "Unable to run: %s", cmd);
-		return;
-	}
-
-	/* FIXME: better way of doing this would be to redirect these streams to
-	 *        /dev/null rather than closing them, but not sure about Windows (NUL
-	 *        device might work). */
-	fclose(file);
-	fclose(err);
-}
-
 /* Runs the cmd in a split window of terminal multiplexer.  Runs shell, if cmd
  * is NULL. */
 static void
@@ -1449,6 +1422,8 @@ run_in_split(const view_t *view, const char cmd[], int vert_split)
 void
 rn_start_bg_command(view_t *view, const char cmd[], MacroFlags flags)
 {
+	/* TODO: similar code in rn_ext(). */
+
 	const int supply_input = ma_flags_present(flags, MF_PIPE_FILE_LIST)
 	                      || ma_flags_present(flags, MF_PIPE_FILE_LIST_Z);
 	FILE *input = NULL;

THANKS
@@ -34,6 +34,7 @@ Behrooz
 Ben Boeckel (mathstuf)
 Ben Lu (ayroblu)
 Bernhard Grotz
+Daniel J. Perry (BioBox)
 blurm
 Branislav Gerazov
 bratekarate

commit 9151eb19473d46a95b7fd8dc2308a71daf290a27

Keep access to the terminal for `%i` without ` &`
Instead of treating `%i` and `%i &` the same.

This restores the behavior prior to v0.12.1-beta (can be considered a
regression as this breaks several use cases).

Caused by commit f22d57d619c34c570a0c859bb8b2589d55932f67:

    Fix %i without " &" breaking some applications

    Because it was closing their standard output and error.  There was a
    lame implementation and some issues with applications started within
    Vifm being closed on Vifm's exit.  With current way of running
    background commands, this doesn't seem to happen and that code isn't
    needed at all.

Moving `%i` commands to a separate session was not intended.

Thanks to an anonymous at Vifm Q2A site.

See
https://q2a.vifm.info/1965/splitting-ghostty-terminal-doesnt-current-working-directory?show=2010#c2010

data/man/vifm.1
@@ -1,4 +1,4 @@
-.TH VIFM 1 "08 March 2025" "vifm 0.14"
+.TH VIFM 1 "23 March 2025" "vifm 0.14"
 .\" ---------------------------------------------------------------------------
 .SH NAME
 .\" ---------------------------------------------------------------------------
@@ -3642,7 +3642,10 @@ otherwise.
 .TP
 .BI %i
 Run in background and suppress error dialogs, but collect
-errors internally for viewing via :jobs menu.
+errors internally for viewing via :jobs menu.  This macro
+doesn't detach the command from terminal session (`/dev/tty`) on
+its own, add " &" to leave the command without access to the
+terminal.
 .TP
 .BI %Pl
 Pipe list of files to standard input of a command.

src/running.c
@@ -1254,8 +1254,8 @@ rn_ext(view_t *view, const char cmd[], const char title[], MacroFlags flags,
 		int error = 0;
 
 		setup_shellout_env();
-		if(bg_run_external(cmd, /*keep_in_fg=*/0, /*skip_errors=*/1, SHELL_BY_USER,
-					input_ptr) != 0)
+		if(bg_run_external(cmd, /*keep_in_fg=*/!bg, /*skip_errors=*/1,
+					SHELL_BY_USER, input_ptr) != 0)
 		{
 			error = 1;
 		}