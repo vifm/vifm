vifm/vifm

commit 9b4e6721f3997ef750d521f1af68ecdde3f241ba

Apply file highlighting to "ext" and "fileext"
Those are view columns.

Thanks to aleksejrs.

Closes #867 on GitHub.

src/ui/fileview.c
@@ -1150,6 +1150,8 @@ column_line_print(const char buf[], size_t offset, AlignType align,
 	                 || info->id == SK_BY_INAME
 	                 || info->id == SK_BY_ROOT
 	                 || info->id == SK_BY_FILEROOT
+	                 || info->id == SK_BY_EXTENSION
+	                 || info->id == SK_BY_FILEEXT
 	                 || vlua_viewcolumn_is_primary(curr_stats.vlua, info->id);
 	const cchar_t line_attrs = prepare_col_color(view, primary, 0, cdt);
 

commit 9833c508ae276e3a37353931227c02817b72e39e

Fix a crash on drawing a tree-view
Happened when {ext} or {fileext} followed {name}, {iname}, {root} or
{fileroot}.

This was caused by commit 9b4e6721f3:

    Apply file highlighting to "ext" and "fileext"

    Those are view columns.

The reason is that column_line_print() assumed that all primary columns
can have a tree drawn on their left and the above commit made {ext} and
{fileext} count as such columns which combined with the fact that
*cdt->prefix_len was never reset caused this weird bug.

The bug however wasn't visible until commit
8e460ca64d75544b4bd880d7b75924fb886e53ce:

    Make ui/column_view forget about ui/fileview.h

    It's not supposed to know those details.

which added more fields to `struct format_info_t` which gets affected by
a buffer overrun by `strcpy(print_buf, buf);`.

Thanks to aw-cloud.

Fixes #1066 on GitHub.

THANKS
@@ -35,6 +35,7 @@ Artur Shaik (artur-shaik)
 astrell
 AtomToast
 Aurelio Sanabria (Sufrostico)
+aw-cloud
 Azin Sharaf
 b0x4it
 Badalisc

src/ui/fileview.c
@@ -1256,6 +1256,9 @@ column_line_print(const char buf[], int offset, AlignType align,
 		wprinta(view->win, print_buf + match_from, &line_attrs,
 				A_REVERSE | A_UNDERLINE);
 	}
+
+	/* Use a given prefix value at most once. */
+	*cdt->prefix_len = 0;
 }
 
 /* Draws current line number at specified column. */

tests/misc/ui.c
@@ -8,12 +8,16 @@
 #include "../../src/cfg/config.h"
 #include "../../src/ui/color_scheme.h"
 #include "../../src/ui/colored_line.h"
+#include "../../src/ui/column_view.h"
 #include "../../src/ui/fileview.h"
 #include "../../src/ui/tabs.h"
 #include "../../src/ui/statusline.h"
 #include "../../src/ui/ui.h"
 #include "../../src/utils/str.h"
+#include "../../src/cmd_core.h"
 #include "../../src/filelist.h"
+
+#include "utils.h"
 
 static void check_tab_title(const tab_info_t *tab_info, const char text[]);
 static char * identity(const char path[]);
@@ -564,6 +568,33 @@ TEST(mouse_map_tlsview)
 	assert_int_equal(FVM_NONE, fview_map_coordinates(&lwin, 4, 5));
 	assert_int_equal(FVM_NONE, fview_map_coordinates(&lwin, 5, 5));
 	assert_int_equal(FVM_NONE, fview_map_coordinates(&lwin, 6, 5));
+}
+
+TEST(prefix_len_is_reset_by_column_line_print)
+{
+	curr_view = &lwin;
+	fview_setup();
+	cmds_init();
+	opt_handlers_setup();
+
+	assert_success(load_tree(&lwin, TEST_DATA_PATH "/tree", NULL));
+	assert_true(lwin.list_rows > 6);
+	lwin.list_pos = 6;
+	lwin.columns = columns_create();
+
+	assert_success(cmds_dispatch("set viewcolumns={name},{ext}", &lwin,
+				CIT_COMMAND));
+	curr_stats.load_stage = 2;
+	/* If this doesn't cause a crash or a memory issue, then there should be no
+	 * bug related to tree prefix length computed for the {name} column being used
+	 * for drawing {ext}. */
+	fview_cursor_redraw(&lwin);
+	curr_stats.load_stage = 0;
+
+	curr_view = NULL;
+	vle_cmds_reset();
+	opt_handlers_teardown();
+	columns_teardown();
 }
 
 static void