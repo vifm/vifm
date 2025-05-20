vifm/vifm

commit 09e976d017599e6be3639b395efed4891f07ddde

Fix rooted trashes and read-only file-systems
Fix treating a failure to create a trash directory on a read-only
file-system in a Unix environment as a success.

The issue was introduced in commit
dc63ea0c6e26a36431afa971bb9eb624cffc7ec8 which did not differentiate
between two uses of try_create_trash_dir(), that commit was:

    Ignore read-only trash directory error

The issue became apparent on MacOS after commit
4ed5d79ed1a0f3c11fa2f3ad2c1b8b4831f872d1:

     Fix 'trashdir' with "%r" on BSD-like systems

Thanks to feekApp.

Fixes #1079 on GitHub.

src/trash.c
@@ -92,7 +92,8 @@ get_list_of_trashes_traverser_state;
 
 static int validate_spec(const char spec[]);
 static int create_trash_dir(const char trash_dir[], int user_specific);
-static int try_create_trash_dir(const char trash_dir[], int user_specific);
+static int try_create_trash_dir(const char trash_dir[], int user_specific,
+		int interactive);
 static void empty_trash_dirs(void);
 static void empty_trash_dir(const char trash_dir[], int can_delete);
 static void empty_trash_in_bg(bg_op_t *bg_op, void *arg);
@@ -220,7 +221,7 @@ create_trash_dir(const char trash_dir[], int user_specific)
 {
 	LOG_FUNC_ENTER;
 
-	if(try_create_trash_dir(trash_dir, user_specific) != 0)
+	if(try_create_trash_dir(trash_dir, user_specific, /*interactive=*/1) != 0)
 	{
 		show_error_msgf("Error Setting Trash Directory",
 				"Could not set trash directory to %s: %s", trash_dir, strerror(errno));
@@ -233,7 +234,7 @@ create_trash_dir(const char trash_dir[], int user_specific)
 /* Tries to create trash directory.  Returns zero on success, otherwise non-zero
  * value is returned. */
 static int
-try_create_trash_dir(const char trash_dir[], int user_specific)
+try_create_trash_dir(const char trash_dir[], int user_specific, int interactive)
 {
 	LOG_FUNC_ENTER;
 
@@ -242,11 +243,12 @@ try_create_trash_dir(const char trash_dir[], int user_specific)
 		if(make_path(trash_dir, 0777) != 0)
 		{
 #ifndef _WIN32
-			/* Do not treat it as an error if trash is not writable because
-			 * file-system is mounted read-only.  User should be aware of it. */
-			return (errno != EROFS);
-#else
-			return 1;
+			if(interactive)
+			{
+				/* Do not treat it as an error if the trash is not writable because
+				 * file-system is mounted read-only.  User should be aware of it. */
+				return (errno != EROFS);
+			}
 #endif
 		}
 	}
@@ -684,7 +686,7 @@ static int
 pick_trash_dir_traverser(const char base_path[], const char trash_dir[],
 		int user_specific, void *arg)
 {
-	if(try_create_trash_dir(trash_dir, user_specific) == 0)
+	if(try_create_trash_dir(trash_dir, user_specific, /*interactive=*/0) == 0)
 	{
 		char **const result = arg;
 		*result = strdup(trash_dir);

THANKS
@@ -100,6 +100,7 @@ emorozov
 Evgeniy (iron-udjin)
 Factorial Prime
 Fang (peromage)
+feekApp
 Flaviu Tamas (flaviut)
 Florian Baumann (derflob)
 flux242

commit 5456cda278e0860341e34660052edefd7a2e3bd0

Fix rooted trashes and read-write file-systems
The commit 09e976d017599e6be3639b395efed4891f07ddde ("Fix rooted trashes
and read-only file-systems") broke the check for everything but macOS...

Would be great to find a way of unit-testing this at least partially.

src/trash.c
@@ -243,13 +243,14 @@ try_create_trash_dir(const char trash_dir[], int user_specific, int interactive)
 		if(make_path(trash_dir, 0777) != 0)
 		{
 #ifndef _WIN32
-			if(interactive)
+			if(interactive && errno == EROFS)
 			{
 				/* Do not treat it as an error if the trash is not writable because
 				 * file-system is mounted read-only.  User should be aware of it. */
-				return (errno != EROFS);
+				return 0;
 			}
 #endif
+			return 1;
 		}
 	}
 