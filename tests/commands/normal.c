f2558e3ced153a1f7d0fa5b6378ed6009fc3a78d
xaizek <xaizek@posteo.net>
2025-07-01 12:22:50 +0300

    Extract tests/commands/normal.c
    Out of tests/commands/misc.c in anticipation of adding more tests of
    :normal command.

---
+++ tests/commands/normal.c
@@ -0,0 +1,70 @@
+#include <stic.h>
+
+#include <string.h> /* strdup() */
+
+#include <test-utils.h>
+
+#include "../../src/engine/keys.h"
+#include "../../src/modes/modes.h"
+#include "../../src/ui/ui.h"
+#include "../../src/utils/dynarray.h"
+#include "../../src/cmd_core.h"
+
+SETUP()
+{
+	view_setup(&lwin);
+	view_setup(&rwin);
+
+	curr_view = &lwin;
+	other_view = &rwin;
+
+	cmds_init();
+	modes_init();
+	opt_handlers_setup();
+}
+
+TEARDOWN()
+{
+	view_teardown(&lwin);
+	view_teardown(&rwin);
+
+	curr_view = NULL;
+	other_view = NULL;
+
+	vle_cmds_reset();
+	vle_keys_reset();
+	opt_handlers_teardown();
+}
+
+TEST(normal_command_does_not_reset_selection)
+{
+	lwin.list_rows = 2;
+	lwin.list_pos = 0;
+	lwin.dir_entry = dynarray_cextend(NULL,
+			lwin.list_rows*sizeof(*lwin.dir_entry));
+	lwin.dir_entry[0].name = strdup("a");
+	lwin.dir_entry[0].origin = &lwin.curr_dir[0];
+	lwin.dir_entry[0].selected = 1;
+	lwin.dir_entry[1].name = strdup("b");
+	lwin.dir_entry[1].origin = &lwin.curr_dir[0];
+	lwin.dir_entry[1].selected = 0;
+	lwin.selected_files = 1;
+
+	assert_success(cmds_dispatch(":normal! t", &lwin, CIT_COMMAND));
+	assert_int_equal(0, lwin.selected_files);
+	assert_false(lwin.dir_entry[0].selected);
+	assert_false(lwin.dir_entry[1].selected);
+
+	assert_success(cmds_dispatch(":normal! vG\r", &lwin, CIT_COMMAND));
+	assert_int_equal(2, lwin.selected_files);
+	assert_true(lwin.dir_entry[0].selected);
+	assert_true(lwin.dir_entry[1].selected);
+
+	assert_success(cmds_dispatch(":normal! t", &lwin, CIT_COMMAND));
+	assert_int_equal(1, lwin.selected_files);
+	assert_true(lwin.dir_entry[0].selected);
+	assert_false(lwin.dir_entry[1].selected);
+}
+
+/* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
+/* vim: set cinoptions+=t0 filetype=c : */
