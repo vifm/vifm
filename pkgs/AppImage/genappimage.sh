5da808f3a051fcca168dcf1ee3eb003382f2737f
xaizek <xaizek@posteo.net>
2025-09-28 15:39:55 +0300

    Download ncurses for AppImage build using HTTPS
    It's going to be HTTPS either way thanks to HSTS, but make it explicit
    and the redirect.

--- pkgs/AppImage/genappimage.sh
+++ pkgs/AppImage/genappimage.sh
@@ -38,7 +38,7 @@ cd "$BUILD_DIR"
 mkdir -p "$BUILD_DIR/AppDir/usr"
 
 # Obtain and compile libncursesw6 so that we get 256 color support
-wget http://ftp.gnu.org/gnu/ncurses/ncurses-6.5.tar.gz
+wget https://ftp.gnu.org/gnu/ncurses/ncurses-6.5.tar.gz
 tar -xf ncurses-6.5.tar.gz
 NCURSES_DIR="$PWD/ncurses-6.5"
 pushd "$NCURSES_DIR"
