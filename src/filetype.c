vifm/vifm

commit e40063483ee87e083cb684198a8ac590169054f1

Keep file matchers grouped together
Previously, each matcher was registered separately to the same list of
programs/viewers.  Now a single record associating matchers group to the
same list of programs/viewers is made.

src/filetype.c
@@ -53,6 +53,8 @@ static int assoc_records_contains(assoc_records_t *assocs, const char command[],
 		const char description[]);
 static void safe_free(char **adr);
 static int is_assoc_record_empty(const assoc_record_t *record);
+static int mg_match(const matchers_group_t *mg, const char str[]);
+static void mg_free(matchers_group_t *mg);
 
 const assoc_record_t NONE_PSEUDO_PROG = {
 	.command = "",
@@ -116,7 +118,7 @@ ft_get_viewers(const char file[])
 	{
 		assoc_t *const assoc = &fileviewers.list[i];
 
-		if(!matchers_match(assoc->matchers, file))
+		if(!mg_match(&assoc->mg, file))
 		{
 			continue;
 		}
@@ -149,7 +151,7 @@ find_existing_cmd(const assoc_list_t *record_list, const char file[])
 		assoc_record_t prog;
 		assoc_t *const assoc = &record_list->list[i];
 
-		if(!matchers_match(assoc->matchers, file))
+		if(!mg_match(&assoc->mg, file))
 		{
 			continue;
 		}
@@ -190,16 +192,15 @@ ft_get_all_programs(const char file[])
 }
 
 void
-ft_set_programs(matchers_t *matchers, const char programs[], int for_x,
-		int in_x)
+ft_set_programs(matchers_group_t mg, const char programs[], int for_x, int in_x)
 {
 	const assoc_t assoc = {
-		.matchers = matchers,
+		.mg = mg,
 		.records = parse_command_list(programs, 1),
 	};
 
 	/* On error, add_assoc() frees assoc, so just exit then. */
-	if(add_assoc(for_x ? &xfiletypes : &filetypes, assoc) == 0)
+	if(add_assoc(for_x ? &xfiletypes : &filetypes, assoc))
 	{
 		if(!for_x || in_x)
 		{
@@ -261,7 +262,7 @@ clone_all_matching_records(const char file[], const assoc_list_t *record_list)
 	for(i = 0; i < record_list->count; ++i)
 	{
 		assoc_t *const assoc = &record_list->list[i];
-		if(matchers_match(assoc->matchers, file))
+		if(mg_match(&assoc->mg, file))
 		{
 			ft_assoc_record_add_all(&result, &assoc->records);
 		}
@@ -271,10 +272,10 @@ clone_all_matching_records(const char file[], const assoc_list_t *record_list)
 }
 
 void
-ft_set_viewers(matchers_t *matchers, const char viewers[])
+ft_set_viewers(matchers_group_t mg, const char viewers[])
 {
 	const assoc_t assoc = {
-		.matchers = matchers,
+		.mg = mg,
 		.records = parse_command_list(viewers, 0),
 	};
 	/* On error, add_assoc() frees assoc, so just exit then. */
@@ -282,8 +283,8 @@ ft_set_viewers(matchers_t *matchers, const char viewers[])
 }
 
 /* Adds association to the list of associations, takes ownership of it
- * regardless of the outcome.  Returns non-zero on out of memory error,
- * otherwise zero is returned. */
+ * regardless of the outcome.  Returns non-zero if the element was added,
+ * otherwise it's freed. */
 static int
 add_assoc(assoc_list_t *assoc_list, assoc_t assoc)
 {
@@ -304,13 +305,13 @@ add_assoc(assoc_list_t *assoc_list, assoc_t assoc)
 	{
 		free_assoc(&assoc);
 		show_error_msg("Memory Error", "Unable to allocate enough memory");
-		return 1;
+		return 0;
 	}
 
 	assoc_list->list = p;
 	assoc_list->list[assoc_list->count] = assoc;
 	assoc_list->count++;
-	return 0;
+	return 1;
 }
 
 /* Compares two associations for equality.  Returns non-zero if they are equal,
@@ -323,13 +324,25 @@ is_assoc_equal(const assoc_t *a, const assoc_t *b)
 		return 0;
 	}
 
-	if(strcmp(matchers_get_expr(a->matchers),
-				matchers_get_expr(b->matchers)) != 0)
+	if(a->mg.count != b->mg.count)
 	{
 		return 0;
 	}
 
 	int i;
+
+	/* In principle, we're only interested whether we have the same set of
+	 * matchers or not regardless of their order, but since the goal is to avoid
+	 * essentially identical duplicates, just do pair-wise comparison. */
+	for(i = 0; i < a->mg.count; ++i)
+	{
+		if(strcmp(matchers_get_expr(a->mg.list[i]),
+					matchers_get_expr(b->mg.list[i])) != 0)
+		{
+			return 0;
+		}
+	}
+
 	for(i = 0; i < a->records.count; ++i)
 	{
 		if(strcmp(a->records.list[i].command, b->records.list[i].command) != 0)
@@ -337,6 +350,7 @@ is_assoc_equal(const assoc_t *a, const assoc_t *b)
 			return 0;
 		}
 	}
+
 	return 1;
 }
 
@@ -389,11 +403,14 @@ static void
 add_defaults(int in_x)
 {
 	char *error;
-	matchers_t *const m = matchers_alloc("{*/}", 0, 1, "", &error);
-	assert(m != NULL && "Failed to allocate builtin matcher!");
+	matchers_group_t mg;
+	if(ft_mg_from_string("{*/}", &mg, &error) != 0)
+	{
+		assert(0 && "Failed to allocate builtin matcher!");
+	}
 
 	new_records_type = ART_BUILTIN;
-	ft_set_programs(m, "{Enter directory}" VIFM_PSEUDO_CMD, 0, in_x);
+	ft_set_programs(mg, "{Enter directory}" VIFM_PSEUDO_CMD, /*for_x=*/0, in_x);
 	new_records_type = ART_CUSTOM;
 }
 
@@ -419,7 +436,7 @@ reset_list_head(assoc_list_t *assoc_list)
 static void
 free_assoc(assoc_t *assoc)
 {
-	matchers_free(assoc->matchers);
+	mg_free(&assoc->mg);
 	ft_assoc_records_free(&assoc->records);
 }
 
@@ -472,10 +489,20 @@ ft_assoc_exists(const assoc_list_t *assocs, const char pattern[],
 		int j;
 
 		const assoc_t assoc = assocs->list[i];
-		if(strcmp(matchers_get_expr(assoc.matchers), pattern) != 0)
+
+		char *mg_str = ft_mg_to_string(&assoc.mg);
+		if(mg_str == NULL)
 		{
+			show_error_msg("Memory Error", "Unable to allocate enough memory");
+			return 0;
+		}
+
+		if(strcmp(mg_str, pattern) != 0)
+		{
+			free(mg_str);
 			continue;
 		}
+		free(mg_str);
 
 		for(j = 0; j < assoc.records.count; ++j)
 		{
@@ -605,6 +632,114 @@ static int
 is_assoc_record_empty(const assoc_record_t *record)
 {
 	return record->command == NULL && record->description == NULL;
+}
+
+/* Checks whether given string matches.  Returns non-zero if so, otherwise zero
+ * is returned. */
+static int
+mg_match(const matchers_group_t *mg, const char str[])
+{
+	int i;
+	for(i = 0; i < mg->count; ++i)
+	{
+		if(matchers_match(mg->list[i], str))
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int
+ft_mg_from_string(const char str[], matchers_group_t *mg, char **error)
+{
+	*error = NULL;
+
+	int nexprs;
+	char **exprs = matchers_list(str, &nexprs);
+	if(exprs == NULL)
+	{
+		update_string(error, "Failed to parse list of matchers.");
+		return 1;
+	}
+
+	matchers_group_t result = {
+		.list = reallocarray(NULL, nexprs, sizeof(*result.list)),
+		.count = 0,
+	};
+	if(result.list == NULL)
+	{
+		free_string_array(exprs, nexprs);
+		update_string(error, "Failed to allocate matchers.");
+		return 1;
+	}
+
+	int i;
+	for(i = 0; i < nexprs; ++i, ++result.count)
+	{
+		char *matcher_error;
+		result.list[i] = matchers_alloc(exprs[i], /*cs_by_def=*/0,
+				/*glob_by_def=*/1, /*on_empty_re=*/"", &matcher_error);
+		if(result.list[i] == NULL)
+		{
+			put_string(error, format_str("Wrong pattern (%s): %s", exprs[i],
+						matcher_error));
+			free(matcher_error);
+			break;
+		}
+	}
+
+	free_string_array(exprs, nexprs);
+
+	if(i < nexprs)
+	{
+		mg_free(&result);
+		return 1;
+	}
+
+	*mg = result;
+	return 0;
+}
+
+/* Frees matchers in a group of matchers. */
+static void
+mg_free(matchers_group_t *mg)
+{
+	int i;
+	for(i = 0; i < mg->count; ++i)
+	{
+		matchers_free(mg->list[i]);
+	}
+	free(mg->list);
+}
+
+char *
+ft_mg_to_string(const matchers_group_t *mg)
+{
+	char *str = NULL;
+	size_t len = 0;
+
+	int i;
+	for(i = 0; i < mg->count; ++i)
+	{
+		if(i != 0 && strappendch(&str, &len, ',') != 0)
+		{
+			break;
+		}
+
+		if(strappend(&str, &len, matchers_get_expr(mg->list[i])) != 0)
+		{
+			break;
+		}
+	}
+
+	if(i < mg->count)
+	{
+		free(str);
+		return NULL;
+	}
+
+	return str;
 }
 
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */

src/cfg/info.c
@@ -47,7 +47,6 @@
 #include "../utils/log.h"
 #include "../utils/macros.h"
 #include "../utils/matcher.h"
-#include "../utils/matchers.h"
 #include "../utils/parson.h"
 #include "../utils/path.h"
 #include "../utils/str.h"
@@ -985,16 +984,19 @@ load_assocs(JSON_Object *root, const char node[], int for_x)
 		if(get_str(entry, "matchers", &matchers) && get_str(entry, "cmd", &cmd))
 		{
 			char *error;
-			matchers_t *const ms = matchers_alloc(matchers, 0, 1, "", &error);
-			if(ms == NULL)
+			matchers_group_t mg;
+			if(ft_mg_from_string(matchers, &mg, &error) != 0)
 			{
-				LOG_ERROR_MSG("Error with matchers of an assoc `%s`: %s", matchers,
-						error);
-				free(error);
+				if(error != NULL)
+				{
+					LOG_ERROR_MSG("Error with matchers of an assoc `%s`: %s", matchers,
+							error);
+					free(error);
+				}
 			}
 			else
 			{
-				ft_set_programs(ms, cmd, for_x, in_x);
+				ft_set_programs(mg, cmd, for_x, in_x);
 			}
 		}
 	}
@@ -1015,16 +1017,19 @@ load_viewers(JSON_Object *root)
 		if(get_str(viewer, "matchers", &matchers) && get_str(viewer, "cmd", &cmd))
 		{
 			char *error;
-			matchers_t *const ms = matchers_alloc(matchers, 0, 1, "", &error);
-			if(ms == NULL)
+			matchers_group_t mg;
+			if(ft_mg_from_string(matchers, &mg, &error) != 0)
 			{
-				LOG_ERROR_MSG("Error with matchers of a viewer `%s`: %s", matchers,
-						error);
-				free(error);
+				if(error != NULL)
+				{
+					LOG_ERROR_MSG("Error with matchers of a viewer `%s`: %s", matchers,
+							error);
+					free(error);
+				}
 			}
 			else
 			{
-				ft_set_viewers(ms, cmd);
+				ft_set_viewers(mg, cmd);
 			}
 		}
 	}
@@ -2421,7 +2426,14 @@ store_assocs(JSON_Object *root, const char node[], assoc_list_t *assocs)
 			char *doubled_commas_cmd = double_char(ft_record.command, ',');
 
 			JSON_Object *entry = append_object(entries);
-			set_str(entry, "matchers", matchers_get_expr(assoc.matchers));
+
+			/* Just make an incomplete object on error, parsing will discard it. */
+			char *mg_str = ft_mg_to_string(&assoc.mg);
+			if(mg_str != NULL)
+			{
+				set_str(entry, "matchers", mg_str);
+				free(mg_str);
+			}
 
 			if(ft_record.description[0] == '\0')
 			{

src/cmd_handlers.c
@@ -2503,9 +2503,6 @@ fileviewer_cmd(const cmd_info_t *cmd_info)
 static int
 add_assoc(const cmd_info_t *cmd_info, int viewer, int for_x)
 {
-	char **matchers;
-	int nmatchers;
-	int i;
 	const int in_x = (curr_stats.exec_env_type == EET_EMULATOR_WITH_X);
 	const char *const records = vle_cmds_next_arg(cmd_info->args);
 
@@ -2516,35 +2513,29 @@ add_assoc(const cmd_info_t *cmd_info, int viewer, int for_x)
 		     : (show_fileprograms_menu(curr_view, cmd_info->argv[0]) != 0);
 	}
 
-	matchers = matchers_list(cmd_info->argv[0], &nmatchers);
-	if(matchers == NULL)
+	char *error;
+	matchers_group_t mg;
+	if(ft_mg_from_string(cmd_info->argv[0], &mg, &error) != 0)
 	{
-		return CMDS_ERR_NO_MEM;
+		if(error == NULL)
+		{
+			return CMDS_ERR_NO_MEM;
+		}
+
+		ui_sb_err(error);
+		free(error);
+		return CMDS_ERR_CUSTOM;
 	}
 
-	for(i = 0; i < nmatchers; ++i)
+	if(viewer)
 	{
-		char *error;
-		matchers_t *const ms = matchers_alloc(matchers[i], 0, 1, "", &error);
-		if(ms == NULL)
-		{
-			ui_sb_errf("Wrong pattern (%s): %s", matchers[i], error);
-			free(error);
-			free_string_array(matchers, nmatchers);
-			return CMDS_ERR_CUSTOM;
-		}
-
-		if(viewer)
-		{
-			ft_set_viewers(ms, records);
-		}
-		else
-		{
-			ft_set_programs(ms, records, for_x, in_x);
-		}
+		ft_set_viewers(mg, records);
+	}
+	else
+	{
+		ft_set_programs(mg, records, for_x, in_x);
 	}
 
-	free_string_array(matchers, nmatchers);
 	return 0;
 }
 

src/filetype.h
@@ -58,11 +58,19 @@ typedef struct
 }
 assoc_records_t;
 
+/* List of matchers which matches if any matcher does. */
+typedef struct
+{
+	struct matchers_t **list; /* Disjunction of matching mechanisms. */
+	int count;                /* Size of the list. */
+}
+matchers_group_t;
+
 /* Single file association entry. */
 typedef struct
 {
-	struct matchers_t *matchers; /* Matching mechanism. */
-	assoc_records_t records;     /* Associated programs. */
+	matchers_group_t mg;     /* Matching mechanism. */
+	assoc_records_t records; /* Associated programs. */
 }
 assoc_t;
 
@@ -111,11 +119,11 @@ const char * ft_get_program(const char file[]);
  * Caller should free the result by calling ft_assoc_records_free() on it. */
 assoc_records_t ft_get_all_programs(const char file[]);
 
-/* Associates list of comma separated patterns with each item in the list of
- * comma separated programs either for X or non-X associations and depending on
- * current execution environment.  Takes over ownership of the matchers. */
-void ft_set_programs(struct matchers_t *matchers, const char programs[],
-		int for_x, int in_x);
+/* Associates a group of matchers with each item in the list of comma separated
+ * programs either for X or non-X associations and depending on current
+ * execution environment.  Takes over ownership of the matchers group. */
+void ft_set_programs(matchers_group_t mg, const char programs[], int for_x,
+		int in_x);
 
 /* Viewers. */
 
@@ -132,9 +140,9 @@ struct strlist_t ft_get_viewers(const char file[]);
  * Caller should free the result by calling ft_assoc_records_free() on it. */
 assoc_records_t ft_get_all_viewers(const char file[]);
 
-/* Associates list of comma separated patterns with each item in the list of
- * comma separated viewers. */
-void ft_set_viewers(struct matchers_t *matchers, const char viewers[]);
+/* Associates a group of matchers with each item in the list of comma separated
+ * viewers.  Takes ownership over the matchers group. */
+void ft_set_viewers(matchers_group_t mg, const char viewers[]);
 
 /* Guesses kind of viewer from the invocation command.  The parameter can be
  * empty or NULL in which case textual kind is implied.  Returns the kind. */
@@ -155,6 +163,16 @@ void ft_assoc_record_add_all(assoc_records_t *assocs,
 
 /* After this call the structure contains NULL values. */
 void ft_assoc_records_free(assoc_records_t *records);
+
+/* Matchers group. */
+
+/* Parses comma-separated list of matchers.  Returns zero on success and sets
+ * *error to NULL, otherwise *error is set to an error message. */
+int ft_mg_from_string(const char str[], matchers_group_t *mg, char **error);
+
+/* Turns group of matchers into a comma-separated list.  Returns NULL on
+ * errors. */
+char * ft_mg_to_string(const matchers_group_t *mg);
 
 #endif /* VIFM__FILETYPE_H__ */
 

tests/filetype/test.c
@@ -1,31 +1,28 @@
 #include "test.h"
 
 #include "../../src/int/file_magic.h"
-#include "../../src/utils/matchers.h"
 #include "../../src/filetype.h"
 
 void
 set_programs(const char pattern[], const char programs[], int for_x, int in_x)
 {
 	char *error;
-	matchers_t *ms;
+	matchers_group_t mg;
+	assert_success(ft_mg_from_string(pattern, &mg, &error));
+	assert_string_equal(NULL, error);
 
-	assert_non_null(ms = matchers_alloc(pattern, 0, 1, "", &error));
-	assert_null(error);
-
-	ft_set_programs(ms, programs, for_x, in_x);
+	ft_set_programs(mg, programs, for_x, in_x);
 }
 
 void
 set_viewers(const char pattern[], const char viewers[])
 {
 	char *error;
-	matchers_t *ms;
+	matchers_group_t mg;
+	assert_success(ft_mg_from_string(pattern, &mg, &error));
+	assert_string_equal(NULL, error);
 
-	assert_non_null(ms = matchers_alloc(pattern, 0, 1, "", &error));
-	assert_null(error);
-
-	ft_set_viewers(ms, viewers);
+	ft_set_viewers(mg, viewers);
 }
 
 int

tests/menus/filetypes.c
@@ -14,7 +14,6 @@
 #include "../../src/modes/wk.h"
 #include "../../src/ui/ui.h"
 #include "../../src/utils/fs.h"
-#include "../../src/utils/matchers.h"
 #include "../../src/cmd_core.h"
 #include "../../src/filelist.h"
 #include "../../src/filetype.h"
@@ -113,10 +112,11 @@ TEST(unknown_key_is_ignored)
 TEST(pseudo_entry_is_always_present_for_directories)
 {
 	char *error;
-	matchers_t *ms = matchers_alloc("{bla-*/}", 0, 1, "", &error);
-	assert_non_null(ms);
+	matchers_group_t mg;
+	assert_success(ft_mg_from_string("{bla-*/}", &mg, &error));
+	assert_string_equal(NULL, error);
 
-	ft_set_programs(ms, "abc-run %c", 0, 0);
+	ft_set_programs(mg, "abc-run %c", 0, 0);
 
 	assert_success(cmds_dispatch("filetype bla-dir/", &lwin, CIT_COMMAND));
 
@@ -137,10 +137,11 @@ TEST(no_menu_if_no_handlers)
 TEST(filetypes_menu)
 {
 	char *error;
-	matchers_t *ms = matchers_alloc("{a,b,c}", 0, 1, "", &error);
-	assert_non_null(ms);
+	matchers_group_t mg;
+	assert_success(ft_mg_from_string("{a,b,c}", &mg, &error));
+	assert_string_equal(NULL, error);
 
-	ft_set_programs(ms, "abc-run %c", 0, 0);
+	ft_set_programs(mg, "abc-run %c", 0, 0);
 
 	assert_success(cmds_dispatch("filetype b", &lwin, CIT_COMMAND));
 
@@ -153,10 +154,11 @@ TEST(filetypes_menu)
 TEST(fileviewers_menu)
 {
 	char *error;
-	matchers_t *ms = matchers_alloc("{a,b,c}", 0, 1, "", &error);
-	assert_non_null(ms);
+	matchers_group_t mg;
+	assert_success(ft_mg_from_string("{a,b,c}", &mg, &error));
+	assert_string_equal(NULL, error);
 
-	ft_set_viewers(ms, "abc-view %c");
+	ft_set_viewers(mg, "abc-view %c");
 
 	assert_success(cmds_dispatch("fileviewer c", &lwin, CIT_COMMAND));
 

tests/misc/quickview.c
@@ -17,7 +17,6 @@
 #include "../../src/ui/ui.h"
 #include "../../src/utils/file_streams.h"
 #include "../../src/utils/fs.h"
-#include "../../src/utils/matchers.h"
 #include "../../src/utils/str.h"
 #include "../../src/utils/string_array.h"
 #include "../../src/filelist.h"
@@ -54,15 +53,14 @@ TEST(no_extra_line_without_extra_padding)
 
 TEST(preview_can_match_agains_full_paths)
 {
-	char *error;
-	matchers_t *ms;
-
 	ft_init(NULL);
 
-	assert_non_null(ms = matchers_alloc("{{*/*}}", 0, 1, "", &error));
-	assert_null(error);
+	char *error;
+	matchers_group_t mg;
+	assert_success(ft_mg_from_string("{{*/*}}", &mg, &error));
+	assert_string_equal(NULL, error);
 
-	ft_set_viewers(ms, "the-viewer");
+	ft_set_viewers(mg, "the-viewer");
 
 	assert_string_equal("the-viewer",
 			qv_get_viewer(TEST_DATA_PATH "/read/two-lines"));
@@ -72,15 +70,14 @@ TEST(preview_can_match_agains_full_paths)
 
 TEST(preview_prg_overrules_fileviewer)
 {
-	char *error;
-	matchers_t *ms;
-
 	ft_init(NULL);
 
-	assert_non_null(ms = matchers_alloc("file", 0, 1, "", &error));
-	assert_null(error);
+	char *error;
+	matchers_group_t mg;
+	assert_success(ft_mg_from_string("file", &mg, &error));
+	assert_string_equal(NULL, error);
 
-	ft_set_viewers(ms, "the-viewer");
+	ft_set_viewers(mg, "the-viewer");
 
 	assert_string_equal("the-viewer", qv_get_viewer("file"));
 	update_string(&curr_view->preview_prg, "override");
@@ -160,9 +157,11 @@ TEST(quick_view_picks_parent_directory_if_there_is_a_match)
 	char path[PATH_MAX + 1];
 
 	char *error;
-	matchers_t *ms = matchers_alloc("../", 0, 1, "", &error);
-	assert_non_null(ms);
-	ft_set_viewers(ms, "do something");
+	matchers_group_t mg;
+	assert_success(ft_mg_from_string("../", &mg, &error));
+	assert_string_equal(NULL, error);
+
+	ft_set_viewers(mg, "do something");
 
 	qv_get_path_to_explore(&entry, path, sizeof(path));
 	assert_string_equal("/path/..", path);

tests/misc/running.c
@@ -17,7 +17,6 @@
 #include "../../src/utils/dynarray.h"
 #include "../../src/utils/fs.h"
 #include "../../src/utils/macros.h"
-#include "../../src/utils/matchers.h"
 #include "../../src/utils/path.h"
 #include "../../src/utils/str.h"
 #include "../../src/filelist.h"
@@ -97,17 +96,18 @@ TEARDOWN()
 
 TEST(full_path_regexps_are_handled_for_selection)
 {
-	matchers_t *ms;
-	char pattern[PATH_MAX + 16];
-	char *error;
-
 	/* Mind that there is no chdir(), this additionally checks that origins are
 	 * being used by the code. */
 
+	char pattern[PATH_MAX + 16];
 	snprintf(pattern, sizeof(pattern), "//%s/*//", lwin.curr_dir);
-	ms = matchers_alloc(pattern, 0, 1, "", &error);
-	assert_non_null(ms);
-	ft_set_programs(ms, "echo %f >> " SANDBOX_PATH "/run", 0, 1);
+
+	char *error;
+	matchers_group_t mg;
+	assert_success(ft_mg_from_string(pattern, &mg, &error));
+	assert_string_equal(NULL, error);
+
+	ft_set_programs(mg, "echo %f >> " SANDBOX_PATH "/run", 0, 1);
 
 	rn_open(&lwin, FHE_NO_RUN);
 
@@ -117,20 +117,21 @@ TEST(full_path_regexps_are_handled_for_selection)
 
 TEST(full_path_regexps_are_handled_for_selection2)
 {
-	matchers_t *ms;
-	char pattern[PATH_MAX + 16];
-	char *error;
-
 	/* Mind that there is no chdir(), this additionally checks that origins are
 	 * being used by the code. */
 
+	char pattern[PATH_MAX + 16];
 	snprintf(pattern, sizeof(pattern), "//%s/*//", lwin.curr_dir);
-	ms = matchers_alloc(pattern, 0, 1, "", &error);
-	assert_non_null(ms);
+
+	char *error;
+	matchers_group_t mg;
+	assert_success(ft_mg_from_string(pattern, &mg, &error));
+	assert_string_equal(NULL, error);
+
 #ifndef _WIN32
-	ft_set_programs(ms, "echo > /dev/null %c &", 0, 1);
+	ft_set_programs(mg, "echo > /dev/null %c &", 0, 1);
 #else
-	ft_set_programs(ms, "echo > NUL %c &", 0, 1);
+	ft_set_programs(mg, "echo > NUL %c &", 0, 1);
 #endif
 
 	rn_open(&lwin, FHE_NO_RUN);
@@ -148,9 +149,11 @@ TEST(can_open_via_plugin)
 			"vifm.addhandler{ name = 'open', handler = open }");
 
 	char *error;
-	matchers_t *ms = matchers_alloc("*", 0, 1, "", &error);
-	assert_non_null(ms);
-	ft_set_programs(ms, "#vifmtest#open", /*for_x=*/0, /*in_x=*/1);
+	matchers_group_t mg;
+	assert_success(ft_mg_from_string("*", &mg, &error));
+	assert_string_equal(NULL, error);
+
+	ft_set_programs(mg, "#vifmtest#open", /*for_x=*/0, /*in_x=*/1);
 
 	rn_open(&lwin, FHE_NO_RUN);
 
@@ -428,12 +431,13 @@ TEST(macro_can_be_added_implicitly, IF(not_windows))
 	--lwin.selected_files;
 
 	char *error;
-	matchers_t *ms = matchers_alloc("{a}", 0, 1, "", &error);
-	assert_non_null(ms);
+	matchers_group_t mg;
+	assert_success(ft_mg_from_string("{a}", &mg, &error));
+	assert_string_equal(NULL, error);
 
 	char cmd[PATH_MAX + 1];
 	snprintf(cmd, sizeof(cmd), "%s a", script_path);
-	ft_set_programs(ms, cmd, 0, 0);
+	ft_set_programs(mg, cmd, 0, 0);
 
 	rn_open(&lwin, FHE_NO_RUN);
 
@@ -453,12 +457,13 @@ TEST(handler_can_be_matched_by_a_prefix, IF(not_windows))
 	--lwin.selected_files;
 
 	char *error;
-	matchers_t *ms = matchers_alloc("{a}", 0, 1, "", &error);
-	assert_non_null(ms);
+	matchers_group_t mg;
+	assert_success(ft_mg_from_string("{a}", &mg, &error));
+	assert_string_equal(NULL, error);
 
 	char cmd[PATH_MAX + 1];
 	snprintf(cmd, sizeof(cmd), "{wrong}no-such-cmd a, {right}%s a", script_path);
-	ft_set_programs(ms, cmd, 0, 0);
+	ft_set_programs(mg, cmd, 0, 0);
 
 	rn_open_with_match(&lwin, script_path, 0);
 
@@ -475,19 +480,20 @@ TEST(selection_multi_run, IF(not_windows))
 {
 	start_use_script();
 
-	char *error;
-	matchers_t *ms;
 	char cmd[PATH_MAX + 1];
 
-	ms = matchers_alloc("{a}", 0, 1, "", &error);
-	assert_non_null(ms);
-	snprintf(cmd, sizeof(cmd), "%s a %%c &", script_path);
-	ft_set_programs(ms, cmd, /*for_x=*/0, /*in_x=*/0);
+	char *error;
+	matchers_group_t mg;
 
-	ms = matchers_alloc("{b}", 0, 1, "", &error);
-	assert_non_null(ms);
+	assert_success(ft_mg_from_string("{a}", &mg, &error));
+	assert_string_equal(NULL, error);
+	snprintf(cmd, sizeof(cmd), "%s a %%c &", script_path);
+	ft_set_programs(mg, cmd, /*for_x=*/0, /*in_x=*/0);
+
+	assert_success(ft_mg_from_string("{b}", &mg, &error));
+	assert_string_equal(NULL, error);
 	snprintf(cmd, sizeof(cmd), "%s b %%\"c &", script_path);
-	ft_set_programs(ms, cmd, /*for_x=*/0, /*in_x=*/0);
+	ft_set_programs(mg, cmd, /*for_x=*/0, /*in_x=*/0);
 
 	rn_open(&lwin, FHE_NO_RUN);
 
@@ -703,46 +709,50 @@ static void
 assoc_a(char macro)
 {
 	char *error;
-	matchers_t *ms = matchers_alloc("{a}", 0, 1, "", &error);
-	assert_non_null(ms);
+	matchers_group_t mg;
+	assert_success(ft_mg_from_string("{a}", &mg, &error));
+	assert_string_equal(NULL, error);
 
 	char cmd[PATH_MAX + 1];
 	snprintf(cmd, sizeof(cmd), "%s a %%%c", script_path, macro);
-	ft_set_programs(ms, cmd, 0, 0);
+	ft_set_programs(mg, cmd, 0, 0);
 }
 
 static void
 assoc_b(char macro)
 {
 	char *error;
-	matchers_t *ms = matchers_alloc("{b}", 0, 1, "", &error);
-	assert_non_null(ms);
+	matchers_group_t mg;
+	assert_success(ft_mg_from_string("{b}", &mg, &error));
+	assert_string_equal(NULL, error);
 
 	char cmd[PATH_MAX + 1];
 	snprintf(cmd, sizeof(cmd), "%s b %%%c", script_path, macro);
-	ft_set_programs(ms, cmd, 0, 0);
+	ft_set_programs(mg, cmd, 0, 0);
 }
 
 static void
 assoc_common(void)
 {
 	char *error;
-	matchers_t *ms = matchers_alloc("{a,b}", 0, 1, "", &error);
-	assert_non_null(ms);
+	matchers_group_t mg;
+	assert_success(ft_mg_from_string("{a,b}", &mg, &error));
+	assert_string_equal(NULL, error);
 
 	char cmd[PATH_MAX + 1];
 	snprintf(cmd, sizeof(cmd), "%s common %%f", script_path);
-	ft_set_programs(ms, cmd, 0, 0);
+	ft_set_programs(mg, cmd, 0, 0);
 }
 
 static void
 assoc(const char pattern[], const char cmd[])
 {
 	char *error;
-	matchers_t *ms = matchers_alloc(pattern, 0, 1, "", &error);
-	assert_non_null(ms);
+	matchers_group_t mg;
+	assert_success(ft_mg_from_string(pattern, &mg, &error));
+	assert_string_equal(NULL, error);
 
-	ft_set_programs(ms, cmd, 0, 0);
+	ft_set_programs(mg, cmd, 0, 0);
 }
 
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */

commit 7550d5d45cdd41bb5b8be8037e926abb3880cdc5

Fix a memory leak in filetype.c:ft_assoc_exists()
Introduced in a recent commit e40063483ee87e083cb684198a8ac590169054f1:

    Keep file matchers grouped together

    Previously, each matcher was registered separately to the same list
    of programs/viewers.  Now a single record associating matchers group
    to the same list of programs/viewers is made.

Found via Coverity.

src/filetype.c
@@ -869,6 +869,7 @@ ft_assoc_exists(const assoc_list_t *assocs, const char pattern[],
 		char *mg_str = ft_mg_to_string(&assoc.mg);
 		if(mg_str == NULL)
 		{
+			free(undoubled);
 			show_error_msg("Memory Error", "Unable to allocate enough memory");
 			return 0;
 		}